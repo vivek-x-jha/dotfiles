ae08a161666638e86c5b534c737b35ae5b9d4b91
Vivek Jha <vivek.x.jha@gmail.com>
2024-06-22 02:46:04 -0700

    Updated iterm2 configurations script

--- iterm2/configure_iterm2.zsh
+++ iterm2/configure_iterm2.zsh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+
 # Specify the preferences directory
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$XDG_CONFIG_HOME/iterm2"
 
@@ -5,13 +7,9 @@ defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$XDG_CONFIG_HOME
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
 
 # Surpress Shell Last Login Message
-touch "$HOME/.hushlogin"
-
-# Install Source Code Pro
-brew tap caskroom/fonts && brew cask install font-source-code-pro
+ln -sF .hushlogin ~/.hushlogin
 
 # After downloading homebrew versions of bash and zsh, use those
 # Need to add them to /etc/shells
 echo "$HOMEBREW_PREFIX/bin/bash" | sudo tee -a /etc/shells
 echo "$HOMEBREW_PREFIX/bin/zsh" | sudo tee -a /etc/shells
-
