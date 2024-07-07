7f9cb482158092a774536de1102b88e0d7f43c78
Vivek Jha <vivek.x.jha@gmail.com>
2024-07-06 23:45:56 -0700

    Created tmux list sessions function in nicer table

--- zsh/aliases/tmux-aliases.zsh
+++ zsh/aliases/tmux-aliases.zsh
@@ -3,5 +3,5 @@
 alias t='tmux attach || tmux new-session'
 alias ta='tmux attach -t'
 alias tn='tmux new-session'
-alias tl='tmux list-sessions'
 alias tks='tmux kill-server'
+alias tl='tmux-list-sessions'
