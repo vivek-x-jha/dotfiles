80a24113e6f8e283c8555f1ea0e29959838b998e
Vivek Jha <vivek.x.jha@gmail.com>
2024-07-20 02:40:34 -0700

    refactored as .dotfiles

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -8,22 +8,21 @@ symlink() {
   [ -d $HOME/Dropbox   ] || ln -sF Library/CloudStorage/Dropbox
   [ -d $HOME/Developer ] || ln -sF Dropbox/developer Developer
 
-  ln -sf dotfiles/conda/.anaconda
-  ln -sf dotfiles/bash/.bashrc
-  ln -sf dotfiles/conda/.conda
-  ln -sf dotfiles/conda/.condarc
-  ln -sf dotfiles/conda/.continuum
-  ln -sf dotfiles/cricut/.cricut-design-space
-  ln -sf dotfiles/git/.gitconfig
-  ln -sf dotfiles/ipython/.ipython
-  ln -sf dotfiles/keras/.keras
-  ln -sf dotfiles/matplotlib/.matplotlib
-  ln -sf dotfiles/npm/.npm
-  ln -sf dotfiles/redhat/.redhat
-  ln -sf dotfiles/thinkorswim/.thinkorswim
-  ln -sf dotfiles/vscode/.vscode
-  ln -sf dotfiles/zsh/.zshenv 
-  ln -sf dotfiles/zsh/.zprofile
+  ln -sf .dotfiles/conda/.anaconda
+  ln -sf .dotfiles/bash/.bashrc
+  ln -sf .dotfiles/conda/.conda
+  ln -sf .dotfiles/conda/.condarc
+  ln -sf .dotfiles/conda/.continuum
+  ln -sf .dotfiles/cricut/.cricut-design-space
+  ln -sf .dotfiles/ipython/.ipython
+  ln -sf .dotfiles/keras/.keras
+  ln -sf .dotfiles/matplotlib/.matplotlib
+  ln -sf .dotfiles/npm/.npm
+  ln -sf .dotfiles/redhat/.redhat
+  ln -sf .dotfiles/thinkorswim/.thinkorswim
+  ln -sf .dotfiles/vscode/.vscode
+  ln -sf .dotfiles/zsh/.zshenv 
+  ln -sf .dotfiles/zsh/.zprofile
 
   # Configuation Symlinks
   cd ${XDG_CONFIG_HOME:-$HOME/.config}
@@ -46,7 +45,7 @@ symlink() {
 
   for pkg in "${config_pkgs[@]}"; do
     rm -rf "$pkg"
-    ln -sF "../dotfiles/$pkg"
+    ln -sF "../.dotfiles/$pkg"
   done
 
 }
