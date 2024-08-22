a30f189791101a4822f210c7596dcaac0be04231
Vivek Jha <vivek.x.jha@gmail.com>
2024-08-22 14:22:10 -0700

    Refactored bootstrap symlink to be inner function for init_filesystem

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,57 +10,25 @@
 
 is_installed() {
   local cmd="$1"
-  if command -v "$cmd" &> /dev/null; then
-    echo "[+ $cmd: INSTALLED]"
-  else
+  if ! command -v "$cmd" &> /dev/null; then
     echo "[? $cmd: NOT INSTALLED]"
     return 1
   fi
 }
 
-makedir() {
-  local dir="$1"
-  [ -d "$dir" ] || mkdir -p "$dir"
-}
-
-backup() {
-  local file="$1"
-  [ -e "$file" ] && mv -f "$file" "$file.bak"
-}
-
-symlink() {
-  local src="$1"
-  local cwd="$2"
-  local tgt="$3"
-  
-  # Test: Valid Current Working Dir
-  [ -d "$cwd" ] || return 1
-  cd "$cwd"
-
-  # Test: Valid Source File/Folder
-  [ -e "$src" ] || return 1
-
-  # Link Source to Target - remove original if directory
-  [ -d "$tgt" ] && rm -rf "$tgt"
-  ln -sf "$src" "$tgt"
-
-  echo "[+ Link: $cwd/$tgt -> $src]"
-}
-
 install_homebrew() {
   local install_type="$1" # all, formulas, casks
-  local logger="$2"
 
   local arch=$(uname -m)
   local brewfile='https://raw.githubusercontent.com/vivek-x-jha/dotfiles/main/.Brewfile'
 
-  # Test: Xcode is installed
+  # Test: Xcode installed
   if ! is_installed xcode-select; then
     echo 'Homebrew requires Xcode to run: xcode-select --install'
     exit 1
   fi
 
-  # Test: Architecture is arm64 or x86_64
+  # Test: Architecture arm64 or x86_64
   if [[ "$arch" == "arm64" ]]; then
     binary_path='/opt/homebrew/bin'
   elif [[ "$arch" == "x86_64" ]]; then
@@ -70,6 +38,8 @@ install_homebrew() {
     echo 'Requires arm64 or x86_64'
     exit 1
   fi
+
+  echo "[+ brew: Binary Path @ $binary_path]"
 
   # Test: Homebrew installed and in PATH
   if ! is_installed brew; then
@@ -97,16 +67,35 @@ install_homebrew() {
   brew list
 }
 
-init_filesystem() {
+create_filesystem() {
   local CLOUD="$1"
   local DOT="$2"
   local XDG_CONFIG="$3"
 
-  # Requires brew and git
+  symlink() {
+    local src="$1"
+    local cwd="$2"
+    local tgt="$3"
+    
+    # Test: Valid Current Working Dir
+    [ -d "$cwd" ] || return 1
+    cd "$cwd"
+
+    # Test: Valid Source File/Folder
+    [ -e "$src" ] || return 1
+
+    # Link Source to Target - remove original if directory
+    [ -d "$tgt" ] && rm -rf "$tgt"
+    ln -sf "$src" "$tgt"
+
+    echo "[+ Link: $cwd/$tgt -> $src]"
+  }
+
+  # Test: Brew and Git installed
   is_installed brew || install_homebrew
   is_installed git  || brew install git
   
-  # Create XDG & Media Directories
+  # Create XDG & Media directories
   local directories=(
     .cache
     .config
@@ -118,10 +107,10 @@ init_filesystem() {
     Movies
     Pictures
   )
-  for dir in "$directories[@]"; do makedir "$HOME/$dir"; done
+  for dir in "$directories[@]"; do [ -d "$HOME/$dir" ] || mkdir -p "$HOME/$dir"; done
 
-  # Create Dotfiles folder
-  backup "$DOT"
+  # Create Dotfiles directory
+  [ -e "$DOT" ] && mv -f "$DOT" "$DOT.bak"
   git clone https://github.com/vivek-x-jha/dotfiles.git "$DOT"
   
   # Link Dotfiles
@@ -157,7 +146,8 @@ init_filesystem() {
   symlink zsh-syntax-highlighting.zsh  "$(brew --prefix)/share/zsh-syntax-highlighting" syntax-highlighting.zsh
 }
 
-init_macos() {
+configure_macos() {
+  local DOT="$1"
   # https://github.com/mathiasbynens/dotfiles/blob/main/.macos
 
   # Supress iTerm login message
@@ -167,7 +157,7 @@ init_macos() {
   bat cache --build
 
   # Enable touchid for sudo
-  sudo cp -f "$HOME/.dotfiles/.sudo_local" /etc/pam.d/sudo_local
+  sudo cp -f "$DOT/.sudo_local" /etc/pam.d/sudo_local
 
   # Save screenshots to ~/Pictures/screenshots
   defaults write com.apple.screencapture location -string "$HOME/Pictures/screenshots"
@@ -189,13 +179,13 @@ main() {
   # TODO Create custom input for git.user, email, signing_key
   echo "󰓒 INSTALLATION START 󰓒"
 
-  install_homebrew all "$HOME/.bootstrap.log"
+  install_homebrew all
   echo "󰗡 [1/3] Homebrew & Packages Installed 󰗡"
 
-  init_filesystem Dropbox "$HOME/.dotfiles" "$HOME/.config"
+  create_filesystem Dropbox "$HOME/.dotfiles" "$HOME/.config"
   echo "󰗡 [2/3] Filesystem & Symlinks Created 󰗡"
   
-  init_macos
+  configure_macos "$HOME/.dotfiles"
   echo "󰗡 [3/3] MacOS Defaults Configured 󰗡"
 
   echo "󰓒 INSTALLATION COMPLETE 󰓒"
