096673344d2b0026e9fa63f710bb7b00a5f39bc5
Vivek Jha <vivek.x.jha@gmail.com>
2024-09-08 22:48:57 -0700

    Initial refacor of neovim config using nvchad starter

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  
+  {
+   "williamboman/mason.nvim",
+   opts = {
+    ensure_installed = {
+      "lua-language-server",
+      "stylua",
+      "html-lsp",
+      "css-lsp",
+      "prettier",
+      "pyre", -- Added python language server here
+    },
+   },
+  },
+  
+  {
+   "nvim-treesitter/nvim-treesitter",  
+    opts = {
+    ensure_installed = {
+      "vim",
+      "lua",
+      "vimdoc",
+      "html",
+      "css",
+      "python", -- Added python syntax highlighting here
+    },
+   },
+  },
+}
