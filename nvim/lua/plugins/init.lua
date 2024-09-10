252cc35271e28838547142e05d6eebee3f66aa57
Vivek Jha <vivek.x.jha@gmail.com>
2024-09-10 11:21:16 -0700

    Nvim UI mirrors old nvchad version

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,10 +1,20 @@
 return {
+
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "å*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end
   },
 
   {
@@ -14,32 +24,32 @@ return {
       require "configs.lspconfig"
     end,
   },
-  
+
   {
-   "williamboman/mason.nvim",
-   opts = {
-    ensure_installed = {
-      "lua-language-server",
-      "stylua",
-      "html-lsp",
-      "css-lsp",
-      "prettier",
-      "pyre", -- Added python language server here
-    },
-   },
-  },
-  
-  {
-   "nvim-treesitter/nvim-treesitter",  
+    "williamboman/mason.nvim",
     opts = {
-    ensure_installed = {
-      "vim",
-      "lua",
-      "vimdoc",
-      "html",
-      "css",
-      "python", -- Added python syntax highlighting here
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "pyre", -- Added python language server here
+      },
     },
-   },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "python", -- Added python syntax highlighting here
+      },
+    },
   },
 }
