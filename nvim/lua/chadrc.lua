3c4f9214f34d5ad6e167a908183092b4830e2531
Vivek Jha <vivek.x.jha@gmail.com>
2024-09-10 11:21:16 -0700

    Nvim UI mirrors old nvchad version

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -7,11 +7,102 @@ local M = {}
 
 M.base46 = {
 	theme = "catppuccin",
-
+  transparency = true,
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
+  changed_themes = {
+    catppuccin = {
+      base_16 = {
+        base00 = "#212030", -- editor background
+        base01 = "#CCCCCC", -- BLACK
+        base02 = "#5c617d", -- select background
+        base03 = "#5c617d", -- Bright BLACK
+        base04 = "#5c617d",
+        base05 = "#f4f3f2", -- default fg, `=` syntax, nvtree default fg
+        base06 = "#cccccc",
+        base07 = "#F4F3F2",
+        base08 = "#FFC7C7",
+        base09 = "#F3B175",
+        base0A = "#FDF7CD",
+        base0B = "#CEFFC9",
+        base0C = "#8AE7C5",
+        base0D = "#C4EFFA",
+        base0E = "#ECCEF0",
+        base0F = "#cccccc", -- punctuation, parantheses, brackets
+      },
+      base_30 = {
+        white = "#f4f3f2",
+        darker_black = "#212030", -- nvtree background
+        black = "#212030", --  statusline icon background and editor active tab background and telescope background
+        black2 = "#252435", -- highlights nvtree current line and inactive tabs bg, and cheat sheet sections
+        one_bg = "#313244", -- status line background
+        one_bg2 = "#5c617d", -- highlights current scope and nvtree divider
+        one_bg3 = "#212030", -- highlights toggle button and telescope borders
+        grey = "#5c617d", -- line number fg
+        grey_fg = "#5c617d", -- comments
+        grey_fg2 = "#555464",
+        light_grey = "#605f6f", -- inactive tabs fg
+        red = "#FFC7C7",
+        baby_pink = "#f096b7",
+        pink = "#ECCEF0",
+        line = "#5c617d", -- for lines like vertsplit
+        green = "#CEFFC9",
+        vibrant_green = "#d2fd9d",
+        nord_blue = "#80d7fe",
+        blue = "#C4EFFA",
+        yellow = "#FDF7CD",
+        sun = "#ffe9b6",
+        purple = "#ECCEF0",
+        dark_purple = "#ECCEF0",
+        teal = "#47e7b1",
+        orange = "#F3B175",
+        cyan = "#8AE7C5",
+        statusline_bg = "#F4F3F2",
+        lightbg = "#2f2e3e",
+        pmenu_bg = "#F4F3F2",
+        folder_bg = "#C4EFFA",
+        lavender = "#C9CCFB",
+      },
+    },
+  },
+  telescope = { style = "bordered" },
+  statusline = {
+    theme = "minimal",
+    separator_style = "round"
+  },
+  nvdash = {
+    load_on_startup = true,
+    buttons = {
+      { "  Find File", "<Space> + f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "<Space> + f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "<Space> + f w", "Telescope live_grep" },
+      { "  Bookmarks", "<Space> + m a", "Telescope marks" },
+      { "  Themes", "<Space> + t h", "Telescope themes" },
+      { "  Mappings", "<Space> + c h", "NvCheatsheet" },
+    },
+  },
+  noice = {
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        ["config.lsp.hover.enabled" ] = false,
+        ["config.lsp.signature.enabled" ] = false,
+      },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+      bottom_search = true, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
+  },
 }
 
 return M
--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -5,11 +5,11 @@ local options = {
     -- html = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
