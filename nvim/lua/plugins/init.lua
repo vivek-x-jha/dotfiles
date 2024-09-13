0c4dfb43410752500678c5777c72d648773e6f28
Vivek Jha <vivek.x.jha@gmail.com>
2024-09-13 11:06:37 -0700

    Fixed nvim-notify warning about background color hex code when transparent

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -41,7 +41,7 @@ return {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
+      -- "rcarriga/nvim-notify",
     },
     config = function()
       require("noice").setup({
@@ -58,12 +58,18 @@ return {
         },
         -- you can enable a preset for easier configuration
         presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
           command_palette = true, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
         },
+      })
+    end,
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("notify").setup({
+        background_colour = "#000000",
       })
     end,
   },
