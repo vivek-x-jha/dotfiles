38fda269e014d1a56b2e8b1660a83f413e771ecf
Vivek Jha <vivek.x.jha@gmail.com>
2024-09-13 13:38:12 -0700

    Updated noice and notify nvim plugins with color and animation - efactored configs into separate files

---
+++ nvim/lua/configs/noice.lua
@@ -0,0 +1,16 @@
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+    signature = { enabled = false }
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+  },
+})
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -32,45 +32,17 @@ return {
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {
-      -- add any options here
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      -- "rcarriga/nvim-notify",
-    },
+    dependencies = { "MunifTanjim/nui.nvim" }, -- if lazy-loaded, add proper `module="..."` entries
     config = function()
-      require("noice").setup({
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-          },
-          signature = {
-            enabled = false,
-          }
-        },
-        -- you can enable a preset for easier configuration
-        presets = {
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-        },
-      })
+      require "configs.noice"
     end,
   },
 
   {
     "rcarriga/nvim-notify",
+    module = "notify",
     config = function()
-      require("notify").setup({
-        background_colour = "#000000",
-      })
+      require "configs.notify"
     end,
   },
 
