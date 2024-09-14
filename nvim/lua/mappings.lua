0468124953412815c2600c331e15a805c733c73c
Vivek Jha <vivek.x.jha@gmail.com>
2024-09-14 11:18:33 -0700

    Commented on mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,18 +1,13 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("n", ";", ":", { desc = "CMD enter command mode" }) -- Command Line w/ `;`
+map({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr>")           -- Save inCtrl + S always saves
 
-map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-
--- Vim Tmux Naviagtor
-map("n", "<C-h>", ":TmuxNavigateLeft<CR>")
-map("n", "<C-j>", ":TmuxNavigateDown<CR>")
-map("n", "<C-k>", ":TmuxNavigateUp<CR>")
-map("n", "<C-l>", ":TmuxNavigateRight<CR>")
-map("n", "<C-\\>",":TmuxNavigateLastActive<CR>")
-map("n", "<C-Space>", ":TmuxNavigateNext<CR>")
+map("n", "<C-h>", ":TmuxNavigateLeft<CR>")              -- Move left buffer/pane
+map("n", "<C-j>", ":TmuxNavigateDown<CR>")              -- Move down buffer/pane
+map("n", "<C-k>", ":TmuxNavigateUp<CR>")                -- Move up buffer/pane
+map("n", "<C-l>", ":TmuxNavigateRight<CR>")             -- Move right buffer/pane
+map("n", "<C-\\>", ":TmuxNavigateLastActive<CR>")       -- Move last buffer/pane
+map("n", "<C-Space>", ":TmuxNavigateNext<CR>")          -- Move next buffer/pane
