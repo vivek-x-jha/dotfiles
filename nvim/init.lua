46ea21a024c1dae19ad8650a3daab9ba4be6bd40
Vivek Jha <vivek.x.jha@gmail.com>
2024-09-15 23:29:52 -0700

    Refactored nvim init to match lazy example

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,26 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
-vim.g.mapleader = " "
-
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
-if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_config = require "configs.lazy"
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 
 -- load plugins
-require("lazy").setup({
+local lazy_plugins = {
   {
     "NvChad/NvChad",
     lazy = false,
@@ -23,7 +29,9 @@ require("lazy").setup({
   },
 
   { import = "plugins" },
-}, lazy_config)
+}
+local lazy_config = require "configs.lazy"
+require("lazy").setup(lazy_plugins, lazy_config)
 
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,8 +3,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" }) -- Command Line w/ `;`
-map({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr>")           -- Save inCtrl + S always saves
-
+map({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr>")           -- Save in most modes
 map("n", "<C-h>", ":TmuxNavigateLeft<CR>")              -- Move left buffer/pane
 map("n", "<C-j>", ":TmuxNavigateDown<CR>")              -- Move down buffer/pane
 map("n", "<C-k>", ":TmuxNavigateUp<CR>")                -- Move up buffer/pane
