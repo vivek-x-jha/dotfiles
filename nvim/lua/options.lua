1ae42db5e5b65451b47057cf45556d530eb53173
Vivek Jha <vivek.x.jha@gmail.com>
2024-10-26 13:34:46 -0700

    feat(wezterm,neovim): created blinking cursor #TODO wezterm bug in tmux

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,3 +4,6 @@ local o = vim.o
 
 -- o.cursorlineopt ='both' -- to enable cursorline!
 o.relativenumber = true
+
+-- enable blinking cursor
+vim.opt.guicursor = "n-v-c:block-blinkwait300-blinkon200-blinkoff150,i-ci:ver25-blinkwait300-blinkon200-blinkoff150"
