843a3ca0dde44dadbc7e736bffde22a14197faaa
Vivek Jha <vivek.x.jha@gmail.com>
2024-10-27 13:49:26 -0700

    refactor(wezterm): added guard clause for config builder - mainly for older wezterm versions

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,7 +8,10 @@
 -- https://wezfurlong.org/wezterm/
 
 local wezterm = require 'wezterm'
-local config = wezterm.config_builder()
+local config = {}
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
 
 config.bold_brightens_ansi_colors = 'BrightOnly'
 config.color_scheme = 'Sourdiesel'
