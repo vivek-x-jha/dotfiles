2103a54f44006056cb160950f0a74669652625c9
Vivek Jha <vivek.x.jha@gmail.com>
2024-10-27 13:23:28 -0700

    refactor(wezterm): created color_scheme Sourdiesel.toml

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,11 +10,17 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.colors = require 'colors.sourdiesel'
+config.bold_brightens_ansi_colors = 'BrightOnly'
+config.color_scheme = 'Sourdiesel'
+
+config.command_palette_fg_color = '#cdd6f4'
+config.command_palette_bg_color = '#212030'
+config.command_palette_font_size = 16
+config.command_palette_rows = 20
 
 config.cursor_blink_rate = 300
 config.cursor_thickness = 0.25
-config.default_cursor_style = "BlinkingBlock"
+config.default_cursor_style = 'BlinkingBlock'
 
 config.enable_tab_bar = false
 
@@ -25,7 +31,7 @@ config.macos_window_background_blur = 50
 config.window_background_opacity = 0.9
 
 config.window_close_confirmation = 'NeverPrompt'
-config.window_decorations = "RESIZE"
+config.window_decorations = 'RESIZE'
 config.window_padding = {
   left   = 25,
   right  = 25,
