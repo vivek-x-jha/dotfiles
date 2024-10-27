5ed9f8f15ca8c93d735504e551b40f436124a24b
Vivek Jha <vivek.x.jha@gmail.com>
2024-10-27 01:15:16 -0700

    style(wezterm): removed comments and sorted config options alphabetically

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,33 +10,27 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
--- Title Bar
+config.colors = require 'colors.sourdiesel'
+
+config.cursor_blink_rate = 300
+config.cursor_thickness = 0.25
+config.default_cursor_style = "BlinkingBlock"
+
 config.enable_tab_bar = false
+
+config.font = wezterm.font('ComicShannsMono Nerd Font', { weight = 'Regular', italic = false })
+config.font_size = 16
+
+config.macos_window_background_blur = 15
+config.window_background_opacity = 0.8
+
 config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = "RESIZE"
-
--- Transparency & Blur
-config.window_background_opacity = 0.97
-config.macos_window_background_blur = 25
-
--- Padding
 config.window_padding = {
   left   = 25,
   right  = 25,
   top    = 20,
   bottom = 20
 }
-
--- Colorscheme
-config.colors = require 'colors.sourdiesel'
-
--- Cursor
-config.default_cursor_style = "BlinkingBlock"
-config.cursor_blink_rate = 300
-config.cursor_thickness = 0.25
-
--- Font
-config.font = wezterm.font('ComicShannsMono Nerd Font', { weight = 'Regular', italic = false })
-config.font_size = 16
 
 return config
