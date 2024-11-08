67a3daf1527ab553f55aca76830e47c3ea8a56fd
Vivek Jha <vivek.x.jha@gmail.com>
2024-11-07 17:59:59 -0800

    refactor(nvim-notify): created local vars for readability

--- nvim/lua/configs/notify.lua
+++ nvim/lua/configs/notify.lua
@@ -1,17 +1,20 @@
-require("notify").setup({
+local highlight = vim.api.nvim_set_hl
+local notify_opts = {
   background_colour = "#000000",
   fps = 60,
   stages = "fade",
-})
+}
 
-vim.api.nvim_set_hl(0, "NotifyERRORIcon",  { fg = "#f096b7" })
-vim.api.nvim_set_hl(0, "NotifyWARNIcon",   { fg = "#f3b175" })
-vim.api.nvim_set_hl(0, "NotifyINFOIcon",   { fg = "#d2fd9d" })
-vim.api.nvim_set_hl(0, "NotifyDEBUGIcon",  { fg = "#c9ccfb" })
-vim.api.nvim_set_hl(0, "NotifyTRACEIcon",  { fg = "#47e7b1" })
+highlight(0, "NotifyERRORIcon", { fg = "#f096b7" })
+highlight(0, "NotifyWARNIcon", { fg = "#f3b175" })
+highlight(0, "NotifyINFOIcon", { fg = "#d2fd9d" })
+highlight(0, "NotifyDEBUGIcon", { fg = "#c9ccfb" })
+highlight(0, "NotifyTRACEIcon", { fg = "#47e7b1" })
 
-vim.api.nvim_set_hl(0, "NotifyERRORTitle", { fg = "#f096b7" })
-vim.api.nvim_set_hl(0, "NotifyWARNTitle",  { fg = "#f3b175" })
-vim.api.nvim_set_hl(0, "NotifyINFOTitle",  { fg = "#d2fd9d" })
-vim.api.nvim_set_hl(0, "NotifyDEBUGTitle", { fg = "#c9ccfb" })
-vim.api.nvim_set_hl(0, "NotifyTRACETitle", { fg = "#47e7b1" })
+highlight(0, "NotifyERRORTitle", { fg = "#f096b7" })
+highlight(0, "NotifyWARNTitle", { fg = "#f3b175" })
+highlight(0, "NotifyINFOTitle", { fg = "#d2fd9d" })
+highlight(0, "NotifyDEBUGTitle", { fg = "#c9ccfb" })
+highlight(0, "NotifyTRACETitle", { fg = "#47e7b1" })
+
+require("notify").setup(notify_opts)
