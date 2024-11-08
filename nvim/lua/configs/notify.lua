375045e8830fc56e53db7c927f63d6ed69d795f3
Vivek Jha <vivek.x.jha@gmail.com>
2024-11-08 15:16:35 -0800

    refactor(nvim-notify): use function with 0 as namespace default instead of var

--- nvim/lua/configs/notify.lua
+++ nvim/lua/configs/notify.lua
@@ -1,20 +1,23 @@
-local highlight = vim.api.nvim_set_hl
+local function set_hl(name, opts)
+  vim.api.nvim_set_hl(0, name, opts)
+end
+
 local notify_opts = {
-  background_colour = "#000000",
+  background_colour = '#000000',
   fps = 60,
-  stages = "fade",
+  stages = 'fade',
 }
 
-highlight(0, "NotifyERRORIcon", { fg = "#f096b7" })
-highlight(0, "NotifyWARNIcon", { fg = "#f3b175" })
-highlight(0, "NotifyINFOIcon", { fg = "#d2fd9d" })
-highlight(0, "NotifyDEBUGIcon", { fg = "#c9ccfb" })
-highlight(0, "NotifyTRACEIcon", { fg = "#47e7b1" })
+set_hl('NotifyERRORIcon', { fg = '#f096b7' })
+set_hl('NotifyWARNIcon', { fg = '#f3b175' })
+set_hl('NotifyINFOIcon', { fg = '#d2fd9d' })
+set_hl('NotifyDEBUGIcon', { fg = '#c9ccfb' })
+set_hl('NotifyTRACEIcon', { fg = '#47e7b1' })
 
-highlight(0, "NotifyERRORTitle", { fg = "#f096b7" })
-highlight(0, "NotifyWARNTitle", { fg = "#f3b175" })
-highlight(0, "NotifyINFOTitle", { fg = "#d2fd9d" })
-highlight(0, "NotifyDEBUGTitle", { fg = "#c9ccfb" })
-highlight(0, "NotifyTRACETitle", { fg = "#47e7b1" })
+set_hl('NotifyERRORTitle', { fg = '#f096b7' })
+set_hl('NotifyWARNTitle', { fg = '#f3b175' })
+set_hl('NotifyINFOTitle', { fg = '#d2fd9d' })
+set_hl('NotifyDEBUGTitle', { fg = '#c9ccfb' })
+set_hl('NotifyTRACETitle', { fg = '#47e7b1' })
 
-require("notify").setup(notify_opts)
+require('notify').setup(notify_opts)
