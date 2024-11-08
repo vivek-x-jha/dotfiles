464ee7ff2edf381e1d89226c6e9f2b23a07d30ec
Vivek Jha <vivek.x.jha@gmail.com>
2024-11-07 22:21:12 -0800

    refactor(nvim-plugins): changed style of quoting to single quotes

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,104 +1,104 @@
 return {
 
   {
-    "nvim-tree/nvim-tree.lua",
-    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    'nvim-tree/nvim-tree.lua',
+    cmd = { 'NvimTreeToggle', 'NvimTreeFocus' },
     opts = function()
-      return require "configs.nvimtree"
+      return require 'configs.nvimtree'
     end,
   },
 
   {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' }
   },
 
   {
-    "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
+    'stevearc/conform.nvim',
+    event = 'BufWritePre', -- uncomment for format on save
     opts = function()
-      require "configs.conform"
+      require 'configs.conform'
     end,
   },
 
   {
-    "williamboman/mason.nvim",
+    'williamboman/mason.nvim',
     opts = {
       ensure_installed = {
-        "lua-language-server",
-        "stylua",
-        "html-lsp",
-        "css-lsp",
-        "prettier",
-        "pyre",
+        'lua-language-server',
+        'stylua',
+        'html-lsp',
+        'css-lsp',
+        'prettier',
+        'pyre',
       },
     },
   },
 
   {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = { "MunifTanjim/nui.nvim" }, -- if lazy-loaded, add proper `module="..."` entries
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'MunifTanjim/nui.nvim' }, -- if lazy-loaded, add proper `module='...'` entries
     config = function()
-      require "configs.noice"
+      require 'configs.noice'
     end,
   },
 
   {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
+    'neovim/nvim-lspconfig',
+    event = { 'BufReadPre', 'BufNewFile' },
     config = function()
-      require "configs.lspconfig"
+      require 'configs.lspconfig'
     end,
   },
 
   {
-    "rcarriga/nvim-notify",
-    module = "notify",
+    'rcarriga/nvim-notify',
+    module = 'notify',
     config = function()
-      require "configs.notify"
+      require 'configs.notify'
     end,
   },
 
   {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
     config = function()
-      require "configs.surround"
+      require 'configs.surround'
     end,
   },
 
   {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
+    'nvim-treesitter/nvim-treesitter',
+    event = { 'BufReadPre', 'BufNewFile' },
     config = function()
-      require "configs.treesitter"
+      require 'configs.treesitter'
     end,
   },
 
   {
-    "vivek-x-jha/tmux-vim-navigator",
+    'vivek-x-jha/tmux-vim-navigator',
     cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
     },
-    keys = {
-      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
+    -- keys = {
+    --   { '<c-h>',  '<cmd><C-U>TmuxNavigateLeft<cr>' },
+    --   { '<c-j>',  '<cmd><C-U>TmuxNavigateDown<cr>' },
+    --   { '<c-k>',  '<cmd><C-U>TmuxNavigateUp<cr>' },
+    --   { '<c-l>',  '<cmd><C-U>TmuxNavigateRight<cr>' },
+    --   { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    -- },
   },
 
   {
-    "tpope/vim-obsession",
-    cmd = "Obsession",
+    'tpope/vim-obsession',
+    cmd = 'Obsession',
   }
 
 }
