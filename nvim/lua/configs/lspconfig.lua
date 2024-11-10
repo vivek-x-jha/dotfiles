88c2cbfb2608714825cc27e35ca73c3aa79ab79d
Vivek Jha <vivek.x.jha@gmail.com>
2024-11-09 20:58:25 -0800

    feat(nvim): Updated plugins to match nvchad versions as well as configs
    #TODO figure out why not importing NvChad as the plugin breaks the plugin list

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,16 +1,98 @@
+local M = {}
+local map = vim.keymap.set
+
+-- export on_attach & capabilities
+M.on_attach = function(_, bufnr)
+  local function opts(desc)
+    return { buffer = bufnr, desc = 'LSP ' .. desc }
+  end
+
+  map('n', 'gD', vim.lsp.buf.declaration, opts 'Go to declaration')
+  map('n', 'gd', vim.lsp.buf.definition, opts 'Go to definition')
+  map('n', 'gi', vim.lsp.buf.implementation, opts 'Go to implementation')
+  map('n', '<leader>sh', vim.lsp.buf.signature_help, opts 'Show signature help')
+  map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts 'Add workspace folder')
+  map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts 'Remove workspace folder')
+
+  map('n', '<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, opts 'List workspace folders')
+
+  map('n', '<leader>D', vim.lsp.buf.type_definition, opts 'Go to type definition')
+  map('n', '<leader>ra', require 'nvchad.lsp.renamer', opts 'NvRenamer')
+
+  map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts 'Code action')
+  map('n', 'gr', vim.lsp.buf.references, opts 'Show references')
+end
+
+-- disable semanticTokens
+M.on_init = function(client, _)
+  if client.supports_method 'textDocument/semanticTokens' then
+    client.server_capabilities.semanticTokensProvider = nil
+  end
+end
+
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
+
+M.capabilities.textDocument.completion.completionItem = {
+  documentationFormat = { 'markdown', 'plaintext' },
+  snippetSupport = true,
+  preselectSupport = true,
+  insertReplaceSupport = true,
+  labelDetailsSupport = true,
+  deprecatedSupport = true,
+  commitCharactersSupport = true,
+  tagSupport = { valueSet = { 1 } },
+  resolveSupport = {
+    properties = {
+      'documentation',
+      'detail',
+      'additionalTextEdits',
+    },
+  },
+}
+
+M.defaults = function()
+  dofile(vim.g.base46_cache .. 'lsp')
+  require('nvchad.lsp').diagnostic_config()
+
+  require('lspconfig').lua_ls.setup {
+    on_attach = M.on_attach,
+    capabilities = M.capabilities,
+    on_init = M.on_init,
+
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { 'vim' },
+        },
+        workspace = {
+          library = {
+            vim.fn.expand '$VIMRUNTIME/lua',
+            vim.fn.expand '$VIMRUNTIME/lua/vim/lsp',
+            vim.fn.stdpath 'data' .. '/lazy/ui/nvchad_types',
+            vim.fn.stdpath 'data' .. '/lazy/lazy.nvim/lua/lazy',
+            '${3rd}/luv/library',
+          },
+          maxPreload = 100000,
+          preloadFileSize = 10000,
+        },
+      },
+    },
+  }
+end
+
 -- load defaults i.e lua_lsp
-require("nvchad.configs.lspconfig").defaults()
+M.defaults()
 
-local lspconfig = require "lspconfig"
-
-local servers = { "html", "cssls", "pyre" }
-local nvlsp = require "nvchad.configs.lspconfig"
+local lspconfig = require 'lspconfig'
+local servers = { 'html', 'cssls', 'pyre' }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
+    on_attach = M.on_attach,
+    on_init = M.on_init,
+    capabilities = M.capabilities,
   }
 end
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,51 @@
 return {
+
+  -- load luasnips + cmp related in insert mode only
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      {
+        -- snippet plugin
+        'L3MON4D3/LuaSnip',
+        dependencies = 'rafamadriz/friendly-snippets',
+        opts = { history = true, updateevents = 'TextChanged,TextChangedI' },
+        config = function(_, opts)
+          require('luasnip').config.set_config(opts)
+          require 'configs.lua_snip'
+        end,
+      },
+
+      -- autopairing of (){}[] etc
+      {
+        'windwp/nvim-autopairs',
+        opts = {
+          fast_wrap = {},
+          disable_filetype = { 'TelescopePrompt', 'vim' },
+        },
+        config = function(_, opts)
+          require('nvim-autopairs').setup(opts)
+
+          -- setup cmp for autopairs
+          local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+          require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done())
+        end,
+      },
+
+      -- cmp sources plugins
+      {
+        'saadparwaiz1/cmp_luasnip',
+        'hrsh7th/cmp-nvim-lua',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+      },
+    },
+    opts = function()
+      return require 'configs.nvimcmp'
+    end,
+  },
+
   {
     'stevearc/conform.nvim',
     event = 'BufWritePre',
@@ -13,16 +60,25 @@ return {
   },
 
   {
-    'nvim-treesitter/nvim-treesitter',
-    opts = {
-      ensure_installed = {
-        'bash',
-        'fish',
-        'markdown',
-        'toml',
-        'yaml',
+    'nvim-telescope/telescope.nvim',
+    cmd = 'Telescope',
+    dependencies = {
+      {
+        'nvim-treesitter/nvim-treesitter',
+        event = { 'BufReadPost', 'BufNewFile' },
+        cmd = { 'TSInstall', 'TSBufEnable', 'TSBufDisable', 'TSModuleInfo' },
+        build = ':TSUpdate',
+        opts = function()
+          return require 'configs.treesitter'
+        end,
+        config = function(_, opts)
+          require('nvim-treesitter.configs').setup(opts)
+        end,
       },
     },
+    opts = function()
+      return require 'configs.telescope'
+    end,
   },
 
   {
@@ -41,16 +97,10 @@ return {
 
   {
     'williamboman/mason.nvim',
-    opts = {
-      ensure_installed = {
-        'lua-language-server',
-        'stylua',
-        'html-lsp',
-        'css-lsp',
-        'prettier',
-        'pyre',
-      },
-    },
+    cmd = { 'Mason', 'MasonInstall', 'MasonInstallAll', 'MasonUpdate' },
+    opts = function()
+      return require 'configs.mason'
+    end,
   },
 
   {
@@ -101,5 +151,69 @@ return {
   {
     'tpope/vim-obsession',
     cmd = 'Obsession',
+  },
+
+  {
+    'lewis6991/gitsigns.nvim',
+    event = 'User FilePost',
+    opts = function()
+      return require 'configs.gitsigns'
+    end,
+  },
+
+  {
+    'folke/which-key.nvim',
+    keys = { '<leader>', '<c-w>', '"', "'", '`', 'c', 'v', 'g' },
+    cmd = 'WhichKey',
+    opts = function()
+      dofile(vim.g.base46_cache .. 'whichkey')
+      return {}
+    end,
+  },
+
+  'nvim-lua/plenary.nvim',
+  'nvzone/volt',
+  'nvzone/menu',
+  { 'nvzone/minty', cmd = { 'Huefy', 'Shades' } },
+
+  {
+    'nvchad/base46',
+    build = function()
+      require('base46').load_all_highlights()
+    end,
+  },
+
+  {
+    'nvchad/ui',
+    lazy = false,
+    config = function()
+      require 'nvchad'
+    end,
+  },
+
+  {
+    'nvim-tree/nvim-web-devicons',
+    opts = function()
+      dofile(vim.g.base46_cache .. 'devicons')
+      return { override = require 'nvchad.icons.devicons' }
+    end,
+  },
+
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    event = 'User FilePost',
+    opts = {
+      indent = { char = '│', highlight = 'IblChar' },
+      scope = { char = '│', highlight = 'IblScopeChar' },
+    },
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. 'blankline')
+
+      local hooks = require 'ibl.hooks'
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+      require('ibl').setup(opts)
+
+      dofile(vim.g.base46_cache .. 'blankline')
+    end,
   },
 }
