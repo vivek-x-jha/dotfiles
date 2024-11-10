69a8f344fb1076fb33509b730adf79849e2d4c8d
Vivek Jha <vivek.x.jha@gmail.com>
2024-11-09 20:58:25 -0800

    feat(nvim): Updated plugins to match nvchad versions as well as configs
    #TODO figure out why not importing NvChad as the plugin breaks the plugin list

---
+++ nvim/lua/configs/mason.lua
@@ -0,0 +1,24 @@
+dofile(vim.g.base46_cache .. 'mason')
+
+return {
+  PATH = 'skip',
+
+  ui = {
+    icons = {
+      package_pending = ' ',
+      package_installed = ' ',
+      package_uninstalled = ' ',
+    },
+  },
+
+  max_concurrent_installers = 10,
+
+  ensure_installed = {
+    'lua-language-server',
+    'stylua',
+    'html-lsp',
+    'css-lsp',
+    'prettier',
+    'pyre',
+  },
+}
