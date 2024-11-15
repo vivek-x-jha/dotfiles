fed3ee8a86cdea31ee087bee11f497be5cb1618b
Vivek Jha <vivek.x.jha@gmail.com>
2024-11-15 13:39:40 -0800

    refactor(nvim): used conform to replace double quotes with single quotes

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -3,22 +3,8 @@
 -- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
-local options = {
-
-  base46 = {
-    theme = 'sourdiesel',
-    transparency = true,
-  },
-
-  ui = {
-    statusline = {
-      theme = 'vscode_colored',
-    },
-  },
-
-  nvdash = {
-    load_on_startup = true,
-  },
+return {
+  base46 = { theme = 'sourdiesel', transparency = true },
+  ui = { statusline = { theme = 'vscode_colored' } },
+  nvdash = { load_on_startup = true },
 }
-
-return options
