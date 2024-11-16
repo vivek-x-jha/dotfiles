60fc8036825d73b5cb1153ca723d4ed5e0b611d0
Vivek Jha <vivek.x.jha@gmail.com>
2024-11-15 22:16:02 -0800

    feat(nvim-gitsigns): updated icons to minimal style to be consistent with shell

--- nvim/lua/configs/gitsigns.lua
+++ nvim/lua/configs/gitsigns.lua
@@ -2,7 +2,19 @@ require('utils.ui').set_base46 'git'
 
 return {
   signs = {
-    delete = { text = '󰍵' },
-    changedelete = { text = '󱕖' },
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '-' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '/' },
+    untracked = { text = '?' },
+  },
+  signs_staged = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '-' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '/' },
+    untracked = { text = '?' },
   },
 }
