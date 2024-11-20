c2f51a1a862d08736e84323e1f100fa3879ad1a3
Vivek Jha <vivek.x.jha@gmail.com>
2024-11-20 01:24:13 -0800

    fix(nvim): redownloaded and configured avante
    #TODO figure out why highlights in avante ask are not working

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,7 +1,4 @@
 return {
-  'stevearc/dressing.nvim',
-  'nvim-lua/plenary.nvim',
-  'MunifTanjim/nui.nvim',
   'nvzone/menu',
   'nvzone/volt',
   {
@@ -61,28 +58,47 @@ return {
     'yetone/avante.nvim',
     event = 'VeryLazy',
     lazy = false,
-    build = 'make',
+    version = false, -- set this if you want to always pull the latest change
     opts = function()
       return require 'configs.avante'
     end,
-  },
-
-  {
-    -- support for image pasting
-    'HakonHarnes/img-clip.nvim',
-    event = 'VeryLazy',
-    opts = function()
-      return require 'configs.imgclip'
-    end,
-  },
-
-  {
-    -- Make sure to set this up properly if you have lazy=true
-    'MeanderingProgrammer/render-markdown.nvim',
-    ft = { 'markdown', 'Avante' },
-    opts = function()
-      return require 'configs.markdown'
-    end,
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
   },
 
   -- load luasnips + cmp related in insert mode only
@@ -150,6 +166,7 @@ return {
   {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
   },
 
   {
