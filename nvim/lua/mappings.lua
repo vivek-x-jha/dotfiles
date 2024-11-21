d8fdeb4fe1d5fb2f6be3ad5543c07f6bd0dbe3aa
Vivek Jha <vivek.x.jha@gmail.com>
2024-11-20 21:04:32 -0800

    refactor(nvim): removed map comments for each individual general mapping

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -61,24 +61,14 @@ local fn = {
 }
 
 -- general
-
--- command line with ;
 map('n', ';', ':', { desc = 'CMD enter command mode' })
 
--- clear highlights
 map('n', '<Esc>', '<cmd>noh<CR>', { desc = 'general clear highlights' })
-
--- save in any mode
 map({ 'n', 'i', 'v' }, '<C-s>', '<cmd>w<CR>', { desc = 'general save file' })
-
--- copy file
 map('n', '<C-c>', '<cmd>%y+<CR>', { desc = 'general copy whole file' })
 
--- line numbers
 map('n', '<leader>n', '<cmd>set nu!<CR>', { desc = 'toggle line number' })
 map('n', '<leader>rn', '<cmd>set rnu!<CR>', { desc = 'toggle relative number' })
-
--- comment
 map('n', '<leader>/', 'gcc', { desc = 'toggle comment', remap = true })
 map('v', '<leader>/', 'gc', { desc = 'toggle comment', remap = true })
 
