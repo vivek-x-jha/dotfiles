3504ba1f858b01800c9c29b4d0ee1f82b3153068
Vivek Jha <vivek.x.jha@gmail.com>
2024-11-22 14:25:01 -0800

    refactor(wezterm): eliminated helper and keybindings file and provided fallback fornts and conditionals to check cross-platform

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,54 +7,47 @@
 -- https://wezfurlong.org/wezterm/
 
 local wezterm = require 'wezterm'
-local keybindings = require 'keybindings'
-local config = {}
 
-if wezterm.config_builder then config = wezterm.config_builder() end
+-- Configuration
+local cnf = {}
+if wezterm.config_builder then
+  cnf = wezterm.config_builder()
+end
 
-config.bold_brightens_ansi_colors = 'BrightOnly'
-config.color_scheme = 'Sourdiesel'
-
-config.command_palette_fg_color = '#cdd6f4'
-config.command_palette_bg_color = '#212030'
-config.command_palette_font_size = 16
-config.command_palette_rows = 20
-
-config.cursor_blink_rate = 300
-config.cursor_thickness = 0.25
-config.default_cursor_style = 'BlinkingBlock'
-
-config.enable_tab_bar = false
-
-config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'ExtraLight' })
-config.font_size = 15
-
-config.initial_rows = 40
-config.initial_cols = 100
-
-config.leader = { key = 'b', mods = 'CTRL' }
-config.keys = keybindings.keys
-config.key_tables = keybindings.key_tables
-
--- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
--- for i = 1, 9 do
---   table.insert(config.keys, {
---     key = tostring(i),
---     mods = "LEADER",
---     action = act.ActivateTab(i - 1)
---   })
--- end
-
-config.macos_window_background_blur = 50
-config.window_background_opacity = 0.9
-
-config.window_close_confirmation = 'NeverPrompt'
-config.window_decorations = 'RESIZE'
-config.window_padding = {
-  left   = 25,
-  right  = 25,
-  top    = 20,
-  bottom = 20
+-- Cross-platform settings
+cnf.color_scheme = 'Sourdiesel'
+cnf.font = wezterm.font_with_fallback {
+  { family = 'JetBrainsMono Nerd Font', weight = 'ExtraLight' }, -- Primary font
+  'Menlo', -- macOS fallback
+  'DejaVu Sans Mono', -- Linux fallback
+  'Consolas', -- Windows fallback
 }
+cnf.font_size = 15
+cnf.cursor_blink_rate = 300
+cnf.cursor_thickness = 0.25
+cnf.default_cursor_style = 'BlinkingBlock'
+cnf.window_background_opacity = 0.9
+cnf.window_decorations = 'RESIZE'
+cnf.window_padding = {
+  left = 25,
+  right = 25,
+  top = 20,
+  bottom = 20,
+}
+cnf.enable_tab_bar = false
 
-return config
+-- Command Palette: activate with <Ctrl + Shift + p>
+cnf.command_palette_fg_color = '#cdd6f4'
+cnf.command_palette_bg_color = '#212030'
+cnf.command_palette_font_size = 16
+cnf.command_palette_rows = 20
+
+-- Blur only available on MacOS
+if wezterm.target_triple == 'x86_64-apple-darwin' or wezterm.target_triple == 'aarch64-apple-darwin' then
+  cnf.macos_window_background_blur = 50
+end
+
+-- Style Tweaks
+cnf.bold_brightens_ansi_colors = 'BrightOnly'
+
+return cnf
