d5ef8f0edd546c2c35abf15746fa22e23bb74195
Vivek Jha <vivek.x.jha@gmail.com>
2024-11-23 16:26:30 -0800

    feat(nvim): added autocommands for maintaing folds on reopen

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,6 +1,5 @@
 local vapi = vim.api
-local ui_entered = vim.g.ui_entered
-local editorconfig = vim.g.editorconfig
+local g = vim.g
 
 -- user event that loads after UIEnter + only if file buf is there
 vapi.nvim_create_autocmd({ 'UIEnter', 'BufReadPost', 'BufNewFile' }, {
@@ -9,21 +8,34 @@ vapi.nvim_create_autocmd({ 'UIEnter', 'BufReadPost', 'BufNewFile' }, {
     local file = vapi.nvim_buf_get_name(args.buf)
     local buftype = vapi.nvim_get_option_value('buftype', { buf = args.buf })
 
-    if not ui_entered and args.event == 'UIEnter' then
-      ui_entered = true
+    if not g.ui_entered and args.event == 'UIEnter' then
+      g.ui_entered = true
     end
 
-    if file ~= '' and buftype ~= 'nofile' and ui_entered then
+    if file ~= '' and buftype ~= 'nofile' and g.ui_entered then
       vapi.nvim_exec_autocmds('User', { pattern = 'FilePost', modeline = false })
       vapi.nvim_del_augroup_by_name 'NvFilePost'
 
       vim.schedule(function()
         vapi.nvim_exec_autocmds('FileType', {})
 
-        if editorconfig then
+        if g.editorconfig then
           require('editorconfig').config(args.buf)
         end
       end)
     end
   end,
+})
+
+-- Maintains fold state on reopen
+vapi.nvim_create_autocmd({ 'BufWinLeave' }, {
+  pattern = { '*.*' },
+  desc = 'save view (folds), when closing file',
+  command = 'mkview',
+})
+
+vapi.nvim_create_autocmd({ 'BufWinEnter' }, {
+  pattern = { '*.*' },
+  desc = 'load view (folds), when opening file',
+  command = 'silent! loadview',
 })
