9d840523a714864518f51c032b0b5accf58acc16
Vivek Jha <vivek.x.jha@gmail.com>
2024-12-14 13:20:04 -0800

    fix(nvim): reverted cmp plugin to monolithic style mirroring what psiduck does in nvchad

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,21 +1,11 @@
 return {
-	'stevearc/dressing.nvim',
-	'nvim-lua/plenary.nvim',
 	'MunifTanjim/nui.nvim',
+	'nvim-lua/plenary.nvim',
 	'nvim-tree/nvim-web-devicons',
-	'zbirenbaum/copilot.lua',
-
-	-- cmp src plugins
-	'rafamadriz/friendly-snippets', -- luasnip dep
-	'saadparwaiz1/cmp_luasnip',
-	'hrsh7th/cmp-nvim-lua',
-	'hrsh7th/cmp-nvim-lsp',
-	'hrsh7th/cmp-buffer',
-	'hrsh7th/cmp-path',
-
-	-- ui
 	'nvzone/menu',
-	'nvzone/volt',
 	{ 'nvzone/minty', cmd = { 'Huefy', 'Shades' } },
+	'nvzone/volt',
 	{ 'tpope/vim-obsession', cmd = 'Obsession' },
+	'stevearc/dressing.nvim',
+	'zbirenbaum/copilot.lua',
 }
