cacf9f4a6a8ed9d6e2c4f892f7a67bc761a8ac21
Vivek Jha <vivek.x.jha@gmail.com>
2024-12-14 11:37:41 -0800

    refactor(nvim): updated some default highlights and rearranged plugins init

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,17 +1,21 @@
 return {
-	'nvzone/menu',
-	'nvzone/volt',
-	'rafamadriz/friendly-snippets',
 	'stevearc/dressing.nvim',
 	'nvim-lua/plenary.nvim',
 	'MunifTanjim/nui.nvim',
 	'nvim-tree/nvim-web-devicons',
 	'zbirenbaum/copilot.lua',
+
+	-- cmp src plugins
+	'rafamadriz/friendly-snippets', -- luasnip dep
 	'saadparwaiz1/cmp_luasnip',
 	'hrsh7th/cmp-nvim-lua',
 	'hrsh7th/cmp-nvim-lsp',
 	'hrsh7th/cmp-buffer',
 	'hrsh7th/cmp-path',
+
+	-- ui
+	'nvzone/menu',
+	'nvzone/volt',
 	{ 'nvzone/minty', cmd = { 'Huefy', 'Shades' } },
 	{ 'tpope/vim-obsession', cmd = 'Obsession' },
 }
