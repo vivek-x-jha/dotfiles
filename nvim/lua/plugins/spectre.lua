9f55de229b5fc3165b861a100da4a3ed5498cc2b
Vivek Jha <vivek.x.jha@gmail.com>
2024-12-15 22:12:42 -0800

    feat(nvim): updated spectre ui colorscheme and added autocmd to hide line numbers

--- nvim/lua/plugins/spectre.lua
+++ nvim/lua/plugins/spectre.lua
@@ -2,6 +2,29 @@
 return {
 	'nvim-pack/nvim-spectre',
 	opts = function()
+		vim.schedule(function()
+			local b16 = require 'ui.base16'
+
+			b16.highlight {
+				SpectreHeader = { fg = b16.brightmagenta, bg = b16.background, bold = true },
+				SpectreBody = { fg = b16.black, bg = b16.background, bold = true },
+				SpectreFile = { fg = b16.yellow, bg = b16.background },
+				SpectreDir = { fg = b16.blue, bg = b16.background },
+				SpectreSearch = { fg = b16.brightred, bg = b16.grey },
+				SpectreBorder = { fg = b16.brightblack, bg = b16.background },
+				SpectreReplace = { fg = b16.brightgreen, bg = b16.grey },
+			}
+		end)
+
+		-- Autocommand to turn off line numbers when Spectre is opened
+		vim.api.nvim_create_autocmd('FileType', {
+			pattern = 'spectre_panel',
+			callback = function()
+				vim.opt_local.number = false
+				vim.opt_local.relativenumber = false
+			end,
+		})
+
 		return {
 			result_padding = '',
 			default = {
@@ -11,9 +34,4 @@ return {
 			},
 		}
 	end,
-	-- keys = {
-	--   { '<leader>sr', '<cmd>lua require("spectre").open()<CR>', desc = 'Open Spectre' },
-	--   { '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', desc = 'Search Current Word' },
-	--   { '<leader>sp', '<cmd>lua require("spectre").open_file_search()<CR>', desc = 'Search in Current File' },
-	-- },
 }
