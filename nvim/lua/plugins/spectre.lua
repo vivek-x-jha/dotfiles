55aa80c6f6d8700e0d811eb6e64ec554ccc3ea6d
Vivek Jha <vivek.x.jha@gmail.com>
2024-12-17 16:22:32 -0800

    refactor(nvim): restructured highlights and themes under lua sub folder colors

--- nvim/lua/plugins/spectre.lua
+++ nvim/lua/plugins/spectre.lua
@@ -3,7 +3,7 @@ return {
 	'nvim-pack/nvim-spectre',
 	opts = function()
 		-- Force highlighting: spectre.setup() sets highlights after passing opts
-		vim.schedule(function() require('ui.highlights').setup 'spectre' end)
+		vim.schedule(function() require('colors.highlights').setup 'spectre' end)
 
 		return {
 			result_padding = '',
