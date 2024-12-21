23d2d0c2ef2870f2a2588ecef55bd2208dd6e1fe
Vivek Jha <vivek.x.jha@gmail.com>
2024-12-21 15:40:02 -0800

    refactor(nvim): moved nvim-cmp dependencies to main plugins file

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,14 +4,6 @@
 return {
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
-  dependencies = {
-    'saadparwaiz1/cmp_luasnip',
-    'hrsh7th/cmp-nvim-lua',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-  },
-
   opts = function()
     local cmp = require 'cmp'
 
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,11 @@
 return {
+  -- nvim-cmp dependencies
+  { 'saadparwaiz1/cmp_luasnip', event = 'InsertEnter' },
+  { 'hrsh7th/cmp-nvim-lua', event = 'InsertEnter' },
+  { 'hrsh7th/cmp-nvim-lsp', event = 'InsertEnter' },
+  { 'hrsh7th/cmp-buffer', event = 'InsertEnter' },
+  { 'hrsh7th/cmp-path', event = 'InsertEnter' },
+
   'MunifTanjim/nui.nvim',
   'nvim-lua/plenary.nvim',
   'nvim-tree/nvim-web-devicons',
