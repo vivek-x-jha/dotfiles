c777bb33438dc565aa39bc2c9ff48e300be8176d
Vivek Jha <vivek.x.jha@gmail.com>
2024-12-21 23:14:14 -0800

    style(nvim): reordered global options

--- nvim/lua/configs/options.lua
+++ nvim/lua/configs/options.lua
@@ -29,10 +29,10 @@ opt.guicursor = 'n-v-c:block-blinkwait300-blinkon200-blinkoff150,i-ci:ver25-blin
 
 -------------------------- Global Options -----------------------------
 
-o.laststatus = 3 -- only last window will always have a status line
-o.showmode = false -- hide mode display (i.e., -- INSERT --)
-o.clipboard = 'unnamedplus' -- use system clipboard for all yank, delete, change, and put operations
-o.cursorline = true -- highlight text line of the cursor
+o.number = true -- print line number in front of each line
+o.relativenumber = true -- show line number relative to the line with the cursor in front of each line
+o.numberwidth = 2 -- Minimal number of columns to use for the line number
+o.ruler = false -- Hide the line and column number of the cursor position
 
 o.expandtab = true -- in Insert mode: Use the appropriate number of spaces to insert a <Tab>
 o.shiftwidth = 2 -- number of spaces to use for each step of (auto)indent
@@ -42,17 +42,15 @@ o.softtabstop = 2 -- number of spaces that a <Tab> counts for while performing e
 
 o.ignorecase = true -- Ignore case in search patterns
 o.smartcase = true -- Override 'ignorecase' if search pattern contains upper case chars
+
+o.clipboard = 'unnamedplus' -- use system clipboard for all yank, delete, change, and put operations
+o.cursorline = true -- highlight text line of the cursor
+o.laststatus = 3 -- only last window will always have a status line
 o.mouse = 'a' -- Enable mouse support for all modes
-
-o.number = true -- print line number in front of each line
-o.relativenumber = true -- show line number relative to the line with the cursor in front of each line
-o.numberwidth = 2 -- Minimal number of columns to use for the line number
-o.ruler = false -- Hide the line and column number of the cursor position
-
+o.showmode = false -- hide mode display (i.e., -- INSERT --)
 o.signcolumn = 'yes' -- always draw the signcolumn
 o.splitbelow = true -- :split puts new window below current
 o.splitright = true -- :vsplit puts new window right current
 o.timeoutlen = 400 -- Time (ms) to wait for mapped sequence to complete
 o.undofile = true -- saves/restores undo history to an undo file
-
 o.updatetime = 250 -- interval for writing swap file to disk (also used by gitsigns)
