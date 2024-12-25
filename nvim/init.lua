6d6da1da535a28dbca2a5c7236545bebca7a4caf
Vivek Jha <vivek.x.jha@gmail.com>
2024-12-25 11:49:58 -0800

    refactor(nvim): variable and function updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,16 @@
---  _   _ ______ ______      _______ __  __
--- | \ | |  ____/ __ \ \    / /_   _|  \/  |
--- |  \| | |__ | |  | \ \  / /  | | | \  / |
--- | . ` |  __|| |  | |\ \/ /   | | | |\/| |
--- | |\  | |___| |__| | \  /   _| |_| |  | |
--- |_| \_|______\____/   \/   |_____|_|  |_|
--- https://neovim.io/doc/
-local g = vim.g
-local o = vim.o
-
-g.mapleader = ' '
-g.maplocalleader = '\\'
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
 
 -- Load plugins
 require 'configs.lazy'
 
 -- Load ui elements
-o.statusline = "%!v:lua.require('ui.statusline').setup()"
+vim.o.statusline = "%!v:lua.require('ui.statusline').setup()"
 require('ui.buffers').setup()
 
 -- Load colors and highlights
-g.colorscheme = 'colors.themes.sourdiesel'
-require('colors.highlights').setup(g.colorscheme)
+vim.g.colorscheme = 'colors.themes.sourdiesel'
+require('colors.highlights').setup(vim.g.colorscheme)
 
 -- Load core configs
 require 'configs.options'
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,17 +4,59 @@ return {
   event = 'User FilePost',
   config = function()
     local M = {}
+    local api = vim.api
     local fn = vim.fn
-    local lspconfig = require 'lspconfig'
     local lspbuf = vim.lsp.buf
-    local lspproto = vim.lsp.protocol
-    local listfolders = function() print(vim.inspect(lspbuf.list_workspace_folders())) end
-
-    -- Run diagnostics
+    local lspconfig = require 'lspconfig'
     local diag = vim.diagnostic
     local sev = diag.severity
     local icons = require 'ui.icons'
 
+    local remap = function(mode, keys, action, bufnr, desc)
+      local opts = { buffer = bufnr, desc = desc }
+      return vim.keymap.set(mode, keys, action, opts)
+    end
+
+    local listfolders = function() print(vim.inspect(lspbuf.list_workspace_folders())) end
+
+    local lsprename = function()
+      local buf = api.nvim_create_buf(false, true)
+      local var = fn.expand '<cword>'
+      local win = api.nvim_open_win(buf, true, {
+        height = 1,
+        style = 'minimal',
+        border = 'single',
+        row = 1,
+        col = 1,
+        relative = 'cursor',
+        width = #var + 15,
+        title = { { ' Renamer ', '@comment.danger' } },
+        title_pos = 'center',
+      })
+
+      vim.wo[win].winhl = 'Normal:Normal,FloatBorder:Removed'
+      api.nvim_set_current_win(win)
+
+      api.nvim_buf_set_lines(buf, 0, -1, true, { ' ' .. var })
+      api.nvim_input 'A'
+
+      remap({ 'i', 'n' }, '<Esc>', '<cmd>q<CR>', buf)
+
+      remap('i', '<CR>', function()
+        local newName = vim.trim(api.nvim_get_current_line())
+        api.nvim_win_close(win, true)
+
+        if #newName > 0 and newName ~= var then
+          local params = vim.lsp.util.make_position_params()
+          params.newName = newName
+          vim.lsp.buf_request(0, 'textDocument/rename', params)
+        end
+
+        vim.cmd.stopinsert()
+      end, buf)
+    end
+
+    -- set diagnostics options
     diag.config {
       virtual_text = { prefix = icons.virtualcircle },
       signs = {
@@ -29,61 +71,25 @@ return {
       float = { border = 'single' },
     }
 
-    -- Default border style
-    local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-      opts = opts or {}
-      opts.border = 'rounded'
-      return orig_util_open_floating_preview(contents, syntax, opts, ...)
-    end
-
-    local lsprename = function()
-      local api = vim.api
-      local var = fn.expand '<cword>'
-      local buf = api.nvim_create_buf(false, true)
-      local opts = { height = 1, style = 'minimal', border = 'single', row = 1, col = 1 }
-
-      opts.relative, opts.width = 'cursor', #var + 15
-      opts.title, opts.title_pos = { { ' Renamer ', '@comment.danger' } }, 'center'
-
-      local win = api.nvim_open_win(buf, true, opts)
-      vim.wo[win].winhl = 'Normal:Normal,FloatBorder:Removed'
-      api.nvim_set_current_win(win)
-
-      api.nvim_buf_set_lines(buf, 0, -1, true, { ' ' .. var })
-      api.nvim_input 'A'
-
-      vim.keymap.set({ 'i', 'n' }, '<Esc>', '<cmd>q<CR>', { buffer = buf })
-
-      vim.keymap.set('i', '<CR>', function()
-        local newName = vim.trim(api.nvim_get_current_line())
-        api.nvim_win_close(win, true)
-
-        if #newName > 0 and newName ~= var then
-          local params = vim.lsp.util.make_position_params()
-          params.newName = newName
-          vim.lsp.buf_request(0, 'textDocument/rename', params)
-        end
-
-        vim.cmd.stopinsert()
-      end, { buffer = buf })
-    end
-
     -- export on_attach & capabilities
     M.on_attach = function(_, bufnr)
-      local map = function(mode, lhs, rhs, desc) return vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = 'LSP ' .. desc }) end
+      local mappings = {
+        { 'n', 'gD', lspbuf.declaration, 'Go to declaration' },
+        { 'n', 'gd', lspbuf.definition, 'Go to definition' },
+        { 'n', 'gi', lspbuf.implementation, 'Go to implementation' },
+        { 'n', '<leader>sh', lspbuf.signature_help, 'Show signature help' },
+        { 'n', '<leader>wa', lspbuf.add_workspace_folder, 'Add workspace folder' },
+        { 'n', '<leader>wr', lspbuf.remove_workspace_folder, 'Remove workspace folder' },
+        { 'n', '<leader>wl', listfolders, 'List workspace folders' },
+        { 'n', '<leader>D', lspbuf.type_definition, 'Go to type definition' },
+        { 'n', '<leader>ra', lsprename, 'NvRenamer' },
+        { { 'n', 'v' }, '<leader>ca', lspbuf.code_action, 'Code action' },
+        { 'n', 'gr', lspbuf.references, 'Show references' },
+      }
 
-      map('n', 'gD', lspbuf.declaration, 'Go to declaration')
-      map('n', 'gd', lspbuf.definition, 'Go to definition')
-      map('n', 'gi', lspbuf.implementation, 'Go to implementation')
-      map('n', '<leader>sh', lspbuf.signature_help, 'Show signature help')
-      map('n', '<leader>wa', lspbuf.add_workspace_folder, 'Add workspace folder')
-      map('n', '<leader>wr', lspbuf.remove_workspace_folder, 'Remove workspace folder')
-      map('n', '<leader>wl', listfolders, 'List workspace folders')
-      map('n', '<leader>D', lspbuf.type_definition, 'Go to type definition')
-      map('n', '<leader>ra', lsprename, 'NvRenamer')
-      map({ 'n', 'v' }, '<leader>ca', lspbuf.code_action, 'Code action')
-      map('n', 'gr', lspbuf.references, 'Show references')
+      for _, keymap in ipairs(mappings) do
+        remap(keymap[1], keymap[2], keymap[3], bufnr, 'LSP ' .. keymap[4])
+      end
     end
 
     -- disable semanticTokens
@@ -91,7 +97,7 @@ return {
       if client.supports_method 'textDocument/semanticTokens' then client.server_capabilities.semanticTokensProvider = nil end
     end
 
-    M.capabilities = lspproto.make_client_capabilities()
+    M.capabilities = vim.lsp.protocol.make_client_capabilities()
 
     M.capabilities.textDocument.completion.completionItem = {
       documentationFormat = { 'markdown', 'plaintext' },
@@ -105,48 +111,45 @@ return {
       resolveSupport = { properties = { 'documentation', 'detail', 'additionalTextEdits' } },
     }
 
-    M.defaults = function()
-      lspconfig.lua_ls.setup {
-        on_attach = M.on_attach,
-        capabilities = M.capabilities,
-        on_init = M.on_init,
+    -- setup language servers
+    lspconfig.lua_ls.setup {
+      on_attach = M.on_attach,
+      capabilities = M.capabilities,
+      on_init = M.on_init,
 
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { 'vim' },
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' },
+          },
+          workspace = {
+            library = {
+              fn.expand '$VIMRUNTIME/lua',
+              fn.expand '$VIMRUNTIME/lua/vim/lsp',
+              fn.stdpath 'data' .. '/lazy/lazy.nvim/lua/lazy',
+              '${3rd}/luv/library',
             },
-            workspace = {
-              library = {
-                fn.expand '$VIMRUNTIME/lua',
-                fn.expand '$VIMRUNTIME/lua/vim/lsp',
-                fn.stdpath 'data' .. '/lazy/ui/nvchad_types',
-                fn.stdpath 'data' .. '/lazy/lazy.nvim/lua/lazy',
-                '${3rd}/luv/library',
-              },
-              maxPreload = 100000,
-              preloadFileSize = 10000,
-            },
+            maxPreload = 100000,
+            preloadFileSize = 10000,
           },
         },
-      }
-    end
-
-    M.defaults()
-
-    -- lsps with default configurations
-    local servers = {
-      'html',
-      'cssls',
-      'pyre',
+      },
     }
 
-    for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup {
-        on_attach = M.on_attach,
-        on_init = M.on_init,
-        capabilities = M.capabilities,
-      }
-    end
+    lspconfig.pyright.setup {
+      on_attach = M.on_attach,
+      on_init = M.on_init,
+      capabilities = M.capabilities,
+      settings = {
+        python = {
+          analysis = {
+            typeCheckingMode = 'strict',
+            autoSearchPaths = true,
+            diagnosticMode = 'workspace',
+            useLibraryCodeForTypes = true,
+          },
+        },
+      },
+    }
   end,
 }
