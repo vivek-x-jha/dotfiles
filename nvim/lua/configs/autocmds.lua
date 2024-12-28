51db458a111824aa8318fc9569b69a77fc698127
Vivek Jha <vivek.x.jha@gmail.com>
2024-12-28 14:37:54 -0800

    refactor(nvim): removed unecessary local vars as abstraction layer

--- nvim/lua/configs/autocmds.lua
+++ nvim/lua/configs/autocmds.lua
@@ -1,150 +1,127 @@
+local api = vim.api
 local g = vim.g
+local lsp = vim.lsp
 local wo = vim.wo
 
-local api = vim.api
-local aucmd = api.nvim_create_autocmd
-local augroup = api.nvim_create_augroup
-local bufline_cnt = api.nvim_buf_line_count
-local buflines = api.nvim_buf_get_lines
-local bufname = api.nvim_buf_get_name
-local clear_aucmds = api.nvim_clear_autocmds
-local curline = api.nvim_get_current_line
-local cursor = api.nvim_win_get_cursor
-local del_augroup = api.nvim_del_augroup_by_name
-local exec_aucmds = api.nvim_exec_autocmds
-local namespace = api.nvim_create_namespace
-local value = api.nvim_get_option_value
-
-local vlsp = vim.lsp
-local lsp_handlers = vlsp.handlers
-local lsp_buf = vlsp.buf
-local lsp_with = vlsp.with
-
-local tree_events = {
-  { events = { 'BufWritePost', 'BufDelete', 'BufNewFile' } },
-  { events = { 'DirChanged', 'FocusGained', 'CursorHold' } },
-  { events = { 'VimLeavePre', 'VimEnter' } },
-}
-
-local folds_augroup = augroup('Folds', { clear = false })
-local term_augroup = augroup('Terminal', { clear = false })
-
-local bufempty = function()
-  local buf_lines = buflines(0, 0, 1, false)
-  local no_buf_content = bufline_cnt(0) == 1 and buf_lines[1] == ''
-
-  return bufname(0) == '' and no_buf_content
-end
-
-local check_triggeredChars = function(triggerChars)
-  local cur_line = curline()
-  local pos = cursor(0)[2]
-  local prev_char = cur_line:sub(pos - 1, pos - 1)
-  local cur_char = cur_line:sub(pos, pos)
-
-  for _, char in ipairs(triggerChars) do
-    if cur_char == char or prev_char == char then return true end
-  end
-end
+local augroup = function(name, clear) return api.nvim_create_augroup(name, { clear = clear or true }) end
 
 ---------------------------- Initialization ----------------------------------
 
-aucmd('VimEnter', {
-  group = augroup('Dashboard', { clear = true }),
-  callback = function()
-    if bufempty() then vim.cmd 'Dashboard' end
-  end,
+api.nvim_create_autocmd('VimEnter', {
+  group = augroup 'DashAU',
   desc = 'Display Dashboard on blank startup',
+  callback = function()
+    local buf_lines = api.nvim_buf_get_lines(0, 0, 1, false)
+    local no_buf_content = buf_lines[1] == '' and api.nvim_buf_line_count(0) == 1
+    local empty_buf = no_buf_content and api.nvim_buf_get_name(0) == ''
+
+    if empty_buf then vim.cmd 'Dashboard' end
+  end,
 })
 
-aucmd('VimEnter', {
-  group = augroup('Showkeys', { clear = true }),
-  callback = function() vim.cmd 'ShowkeysToggle' end,
+api.nvim_create_autocmd('VimEnter', {
+  group = augroup 'ShowkeysAU',
   desc = 'Initialize Showkeys on startup',
+  callback = function() vim.cmd 'ShowkeysToggle' end,
 })
 
 ---------------------------- UI Preparation ----------------------------------
 
-aucmd('FileType', {
-  group = augroup('Spectre', { clear = true }),
+api.nvim_create_autocmd('TextYankPost', {
+  group = augroup 'YankAU',
+  desc = 'Highlight when yanking (copying) text',
+  callback = function() vim.highlight.on_yank { higroup = 'YankFlash', timeout = 200 } end,
+})
+
+api.nvim_create_autocmd('FileType', {
+  group = augroup 'Spectre',
   pattern = 'spectre_panel',
+  desc = 'Hide line numbers for Spectre',
   callback = function()
     vim.opt_local.number = false
     vim.opt_local.relativenumber = false
   end,
-  desc = 'Hide line numbers for Spectre',
 })
 
-aucmd('BufWinEnter', {
-  group = folds_augroup,
+api.nvim_create_autocmd('BufWinEnter', {
+  group = augroup('Folds', false),
   pattern = { '*.*' },
   desc = 'Load folds when opening file',
   command = 'silent! loadview',
 })
 
-aucmd('BufWinLeave', {
-  group = folds_augroup,
+api.nvim_create_autocmd('BufWinLeave', {
+  group = augroup('FoldsAU', false),
   pattern = { '*.*' },
   desc = 'Save folds when closing file',
   command = 'mkview',
 })
 
-aucmd('TermOpen', {
-  group = term_augroup,
-  callback = function() wo.cursorline = false end,
+api.nvim_create_autocmd('TermOpen', {
+  group = augroup('TerminalAU', false),
   desc = 'Disable cursorline in terminal buffers',
+  callback = function() wo.cursorline = false end,
 })
 
-aucmd('TermLeave', {
-  group = term_augroup,
-  callback = function() wo.cursorline = true end,
+api.nvim_create_autocmd('TermLeave', {
+  group = augroup('TerminalAU', false),
   desc = 'Re-enable cursorline after leaving terminal buffers',
+  callback = function() wo.cursorline = true end,
 })
 
-aucmd('TermClose', {
-  group = term_augroup,
-  callback = function(args) require('ui.terminal').save(args.buf, nil) end,
+api.nvim_create_autocmd('TermClose', {
+  group = augroup('TerminalAU', false),
   desc = 'Save terminal state on close',
+  callback = function(args) require('ui.terminal').save(args.buf, nil) end,
 })
 
-for _, action in ipairs(tree_events) do
-  aucmd(action.events, {
-    group = augroup('NvimTreeRefresh', { clear = true }),
-    pattern = action.pattern,
-    callback = function() require('nvim-tree.api').tree.reload() end,
-    desc = 'Reloads Nvim-Tree on most events',
-  })
-end
+api.nvim_create_autocmd({
+  'BufWritePost',
+  'BufDelete',
+  'BufReadPost',
+  'VimResized',
+  'FocusGained',
+  'ShellCmdPost',
+  'FileChangedShellPost',
+}, {
+  group = augroup 'TreeAU',
+  pattern = '*',
+  desc = 'Auto-refresh Nvim-Tree on file, Git, and resize events',
+  callback = function()
+    local nvt = require('nvim-tree.api').tree
+    if nvt.is_visible() then nvt.reload() end
+  end,
+})
 
 -- Load LSP Progress bar
 require('ui.statusline').autocmds()
 
 ---------------------------- Deferred ----------------------------------
 
-aucmd({ 'UIEnter', 'BufReadPost', 'BufNewFile' }, {
-  group = augroup('NvFilePost', { clear = true }),
+api.nvim_create_autocmd({ 'UIEnter', 'BufReadPost', 'BufNewFile' }, {
+  group = augroup 'FilePostAU',
+  desc = 'Wait to load user events on non-empty buffers',
   callback = function(args)
-    local file = bufname(args.buf)
-    local buftype = value('buftype', { buf = args.buf })
+    local file = api.nvim_buf_get_name(args.buf)
+    local buftype = api.nvim_get_option_value('buftype', { buf = args.buf })
 
     if not g.ui_entered and args.event == 'UIEnter' then g.ui_entered = true end
 
     if file ~= '' and buftype ~= 'nofile' and g.ui_entered then
-      exec_aucmds('User', { pattern = 'FilePost', modeline = false })
-      del_augroup 'NvFilePost'
+      api.nvim_exec_autocmds('User', { pattern = 'FilePost', modeline = false })
+      api.nvim_del_augroup_by_name 'FilePostAU'
 
       vim.schedule(function()
-        exec_aucmds('FileType', {})
+        api.nvim_exec_autocmds('FileType', {})
 
         if g.editorconfig then require('editorconfig').config(args.buf) end
       end)
     end
   end,
-  desc = 'Wait to load user events on non-empty buffers',
 })
 
 vim.schedule(function()
-  aucmd({
+  api.nvim_create_autocmd({
     'TextChanged',
     'TextChangedI',
     'TextChangedP',
@@ -153,47 +130,55 @@ vim.schedule(function()
     'WinScrolled',
     'BufEnter',
   }, {
-    group = augroup('Colorify', { clear = true }),
+    group = augroup 'ColorifyAU',
+    desc = 'Initialize Colorify Virtual Text',
     callback = function(args)
       local state = require 'ui.state'
-      state.ns = namespace 'Colorify'
+      state.ns = api.nvim_create_namespace 'Colorify'
 
       if vim.bo[args.buf].bl then require('ui.colorify').attach(args.buf, args.event) end
     end,
-    desc = 'Initialize Colorify Virtual Text',
   })
 
-  aucmd('LspAttach', {
-    group = augroup('LSP', { clear = true }),
+  api.nvim_create_autocmd('LspAttach', {
+    group = augroup 'LspAttachAU',
+    desc = 'Initialize LSP config',
     callback = function(args)
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      local client = lsp.get_client_by_id(args.data.client_id)
 
       if client then
         local signatureProvider = client.server_capabilities.signatureHelpProvider
 
         if signatureProvider and signatureProvider.triggerCharacters then
-          local LspAuGroup = augroup('LspSignature', { clear = false })
+          local lsp_sig_au = augroup('LspSignatureAU', false)
           local triggerChars = client.server_capabilities.signatureHelpProvider.triggerCharacters
 
-          lsp_handlers['textDocument/signatureHelp'] = lsp_with(lsp_handlers.signature_help, {
+          lsp.handlers['textDocument/signatureHelp'] = lsp.with(lsp.handlers.signature_help, {
             border = 'rounded',
             focusable = false,
             silent = true,
             max_height = 7,
           })
 
-          clear_aucmds { group = LspAuGroup, buffer = args.buf }
-          aucmd('TextChangedI', {
-            group = LspAuGroup,
+          api.nvim_clear_autocmds { group = lsp_sig_au, buffer = args.buf }
+
+          api.nvim_create_autocmd('TextChangedI', {
+            group = lsp_sig_au,
+            desc = 'Detects Trigger Characters on Insert',
             buffer = args.buf,
             callback = function()
-              if check_triggeredChars(triggerChars) then lsp_buf.signature_help() end
+              local cur_line = api.nvim_get_current_line()
+              local pos = api.nvim_win_get_cursor(0)[2]
+              local prev_char = cur_line:sub(pos - 1, pos - 1)
+              local cur_char = cur_line:sub(pos, pos)
+
+              for _, char in ipairs(triggerChars) do
+                if cur_char == char or prev_char == char then lsp.buf.signature_help() end
+              end
             end,
-            desc = 'Detects Trigger Characters on Insert',
           })
         end
       end
     end,
-    desc = 'Initialize LSP config',
   })
 end)
