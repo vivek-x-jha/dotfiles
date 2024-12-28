6025ce6accdf6b6f3e43d1e1785c7b42be2ec995
Vivek Jha <vivek.x.jha@gmail.com>
2024-12-28 14:57:01 -0800

    refactor(nvim): removed abstraction of remap function

--- nvim/lua/configs/mappings.lua
+++ nvim/lua/configs/mappings.lua
@@ -1,103 +1,101 @@
--- TODO standardize keymap descriptions
--- TODO fix scrolling move line up/down
+local key = vim.keymap
 local buf = require 'ui.buffers'
 local term = require 'ui.terminal'
 
-local remap = function(mode, keys, action, desc, remap)
-  local opts = { desc = desc, remap = remap or false }
-  vim.keymap.set(mode, keys, action, opts)
-end
+-- Command mode
+key.set('n', ';', ':', { desc = 'CMD enter command mode' })
+key.set('n', '<Esc>', '<cmd>noh<CR>', { desc = 'Clear highlights' })
 
-remap('n', ';', ':', 'CMD enter command mode')
+-- Save and Copy
+key.set({ 'n', 'i', 'v' }, '<C-s>', '<cmd>w<CR>', { desc = '[s]ave file' })
+key.set('n', '<C-c>', '<cmd>%y+<CR>', { desc = '[C]opy file' })
 
-remap('n', '<Esc>', '<cmd>noh<CR>', 'Clear highlights')
-remap({ 'n', 'i', 'v' }, '<C-s>', '<cmd>w<CR>', '[s]ave file')
-remap('n', '<C-c>', '<cmd>%y+<CR>', '[C]opy file')
+-- Toggle line numbers
+key.set('n', '<leader>n', '<cmd>set nu!<CR>', { desc = 'Toggle line number' })
+key.set('n', '<leader>rn', '<cmd>set rnu!<CR>', { desc = 'Toggle relative number' })
 
-remap('n', '<leader>n', '<cmd>set nu!<CR>', 'Toggle line number')
-remap('n', '<leader>rn', '<cmd>set rnu!<CR>', 'Toggle relative number')
-remap('n', '<leader>/', 'gcc', 'Toggle comment', true)
-remap('v', '<leader>/', 'gc', 'Toggle comment', true)
-remap('v', '<Tab>', '>gv', 'Indent selection')
-remap('v', '<S-Tab>', '<gv', 'Outdent selection')
+-- Comments
+key.set('n', '<leader>/', 'gcc', { desc = 'Toggle comment', remap = true })
+key.set('v', '<leader>/', 'gc', { desc = 'Toggle comment', remap = true })
 
--- insert mode
-remap('i', '<C-b>', '<ESC>I', 'Move [b]eginning of line')
-remap('i', '<C-e>', '<End>', 'Move [e]nd of line')
-remap('i', '<C-h>', '<Left>', 'Move left')
-remap('i', '<C-l>', '<Right>', 'Move right')
-remap('i', '<C-j>', '<Down>', 'Move down')
-remap('i', '<C-k>', '<Up>', 'Move up')
+-- Indentation
+key.set('v', '<Tab>', '>gv', { desc = 'Indent selection' })
+key.set('v', '<S-Tab>', '<gv', { desc = 'Outdent selection' })
 
--- scrolling
-remap('n', '<C-d>', '<C-d>zz', 'Page [d]own and center')
-remap('n', '<C-u>', '<C-u>zz', 'Page [u]p and center')
+-- Insert mode navigation
+key.set('i', '<C-b>', '<ESC>I', { desc = 'Move [b]eginning of line' })
+key.set('i', '<C-e>', '<End>', { desc = 'Move [e]nd of line' })
+key.set('i', '<C-h>', '<Left>', { desc = 'Move left' })
+key.set('i', '<C-l>', '<Right>', { desc = 'Move right' })
+key.set('i', '<C-j>', '<Down>', { desc = 'Move down' })
+key.set('i', '<C-k>', '<Up>', { desc = 'Move up' })
 
-remap('v', 'J', ":m '>+1<CR>gv=gv", 'Move selection down')
-remap('v', 'K', ":m '<-2<CR>gv=gv", 'Move selection up')
+-- Scrolling
+-- TODO fix scrolling move line up/down
+key.set('n', '<C-d>', '<C-d>zz', { desc = 'Page [d]own and center' })
+key.set('n', '<C-u>', '<C-u>zz', { desc = 'Page [u]p and center' })
 
--- obsession
-remap('n', '<leader>ob', '<cmd>Obsession<CR>', 'Toggle [ob]session')
-remap('n', '<leader>oo', function()
+-- Moving selections
+key.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+key.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+
+-- Sessions
+key.set('n', '<leader>ob', '<cmd>Obsession<CR>', { desc = 'Toggle [ob]session' })
+key.set('n', '<leader>oo', function()
   vim.cmd 'silent! Obsession'
   vim.cmd 'silent! Obsession'
   vim.notify('Updating workspace state: Session.vim', vim.log.levels.INFO)
-end, 'Save workspace config')
+end, { desc = 'Save workspace config' })
 
--- buffers
-remap('n', '<leader>b', '<cmd>enew<CR>', 'Open [b]uffer')
-remap('n', '<tab>', buf.next, 'Next buffer')
-remap('n', '<S-tab>', buf.prev, 'Prev buffer')
-remap('n', '<leader>x', buf.close, 'Close buffer')
+-- Buffers
+key.set('n', '<leader>b', '<cmd>enew<CR>', { desc = 'Open [b]uffer' })
+key.set('n', '<tab>', buf.next, { desc = 'Next buffer' })
+key.set('n', '<S-tab>', buf.prev, { desc = 'Prev buffer' })
+key.set('n', '<leader>x', buf.close, { desc = 'Close buffer' })
 
--- nvimtree
-remap('n', '<C-n>', function()
+-- NvimTree
+key.set('n', '<C-n>', function()
   require('nvim-tree.api').tree.toggle { focus = false }
   vim.cmd 'wincmd ='
-end, 'Toggle file explorer')
+end, { desc = 'Toggle file explorer' })
 
-remap('n', '<leader>e', function()
+key.set('n', '<leader>e', function()
   require('nvim-tree.api').tree.open()
   vim.cmd 'wincmd ='
-end, 'Focus file [e]xplorer')
+end, { desc = 'Focus file [e]xplorer' })
 
--- terminal
-remap('t', '<C-x>', '<C-\\><C-N>', 'Escape terminal mode')
+-- Terminal
+key.set('t', '<C-x>', '<C-\\><C-N>', { desc = 'Escape terminal mode' })
 
-remap('n', '<leader>h', function() term.open { pos = 'sp' } end, 'Open [h]orizontal terminal')
-remap('n', '<leader>v', function() term.open { pos = 'vsp' } end, 'Open [v]ertical terminal')
+key.set('n', '<leader>h', function() term.open { pos = 'sp' } end, { desc = 'Open [h]orizontal terminal' })
+key.set('n', '<leader>v', function() term.open { pos = 'vsp' } end, { desc = 'Open [v]ertical terminal' })
 
-remap({ 'n', 't' }, '<A-v>', function() term.toggle { pos = 'vsp', id = 'vtoggleTerm' } end, 'Toggle [v]ertical terminal')
-remap({ 'n', 't' }, '<A-h>', function() term.toggle { pos = 'sp', id = 'htoggleTerm' } end, 'Toggle [h]orizontal terminal')
-remap({ 'n', 't' }, '<A-i>', function() term.toggle { pos = 'float', id = 'floatTerm' } end, 'Toggle [f]loating terminal')
+key.set({ 'n', 't' }, '<A-v>', function() term.toggle { pos = 'vsp', id = 'vtoggleTerm' } end, { desc = 'Toggle [v]ertical terminal' })
+key.set({ 'n', 't' }, '<A-h>', function() term.toggle { pos = 'sp', id = 'htoggleTerm' } end, { desc = 'Toggle [h]orizontal terminal' })
+key.set({ 'n', 't' }, '<A-i>', function() term.toggle { pos = 'float', id = 'floatTerm' } end, { desc = 'Toggle [f]loating terminal' })
 
--- telescope
-remap('n', '<leader>cm', '<cmd>Telescope git_commits<CR>', 'Telescope git [c]o[m]mits')
-remap('n', '<leader>fa', '<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>', 'Telescope [f]ind [a]ll files')
-remap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', 'Telescope [f]ind [b]uffers')
-remap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', 'Telescope [f]ind [f]iles')
-remap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', 'Telescope [h]elp page')
-remap('n', '<leader>fo', '<cmd>Telescope oldfiles<CR>', 'Telescope [f]ind [o]ldfiles')
-remap('n', '<leader>fw', '<cmd>Telescope live_grep<CR>', 'Telescope [f]ind [w]ord')
-remap('n', '<leader>fz', '<cmd>Telescope current_buffer_fuzzy_find<CR>', 'Telescope [f]u[z]zy find in current buffer')
-remap('n', '<leader>gt', '<cmd>Telescope git_status<CR>', 'Telescope [g]it s[t]atus')
-remap('n', '<leader>ma', '<cmd>Telescope marks<CR>', 'Telescope [m]arks')
-remap('n', '<leader>pt', '<cmd>Telescope terms<CR>', 'Telescope [p]ick [t]erm')
+-- Telescope
+key.set('n', '<leader>cm', '<cmd>Telescope git_commits<CR>', { desc = 'Telescope git [c]o[m]mits' })
+key.set('n', '<leader>fa', '<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>', { desc = 'Telescope [f]ind [a]ll files' })
+key.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { desc = 'Telescope [f]ind [b]uffers' })
+key.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { desc = 'Telescope [f]ind [f]iles' })
+key.set('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { desc = 'Telescope [h]elp page' })
+key.set('n', '<leader>fo', '<cmd>Telescope oldfiles<CR>', { desc = 'Telescope [f]ind [o]ldfiles' })
+key.set('n', '<leader>fw', '<cmd>Telescope live_grep<CR>', { desc = 'Telescope [f]ind [w]ord' })
+key.set('n', '<leader>fz', '<cmd>Telescope current_buffer_fuzzy_find<CR>', { desc = 'Telescope [f]u[z]zy find in current buffer' })
+key.set('n', '<leader>gt', '<cmd>Telescope git_status<CR>', { desc = 'Telescope [g]it s[t]atus' })
+key.set('n', '<leader>ma', '<cmd>Telescope marks<CR>', { desc = 'Telescope [m]arks' })
+key.set('n', '<leader>pt', '<cmd>Telescope terms<CR>', { desc = 'Telescope [p]ick [t]erm' })
 
--- global lsp remappings
-remap('n', '<leader>ds', vim.diagnostic.setloclist, 'LSP diagnostic loclist')
+-- LSP
+key.set('n', '<leader>ds', vim.diagnostic.setloclist, { desc = 'LSP diagnostic loclist' })
 
--- whichkey
-remap('n', '<leader>wK', '<cmd>WhichKey <CR>', 'Toggle all [w]hichkey [K]eymaps')
-remap('n', '<leader>wk', function() vim.cmd('WhichKey ' .. vim.fn.input 'WhichKey: ') end, 'Search [w]hich[k]ey')
+-- WhichKey
+key.set('n', '<leader>wK', '<cmd>WhichKey <CR>', { desc = 'Toggle all [w]hichkey [K]eymaps' })
+key.set('n', '<leader>wk', function() vim.cmd('WhichKey ' .. vim.fn.input 'WhichKey: ') end, { desc = 'Search [w]hich[k]ey' })
 
--- showkey
-remap('n', '<leader>sk', '<cmd>ShowkeysToggle<CR>', 'Toggle [s]how[k]eys')
+-- Showkeys
+key.set('n', '<leader>sk', '<cmd>ShowkeysToggle<CR>', { desc = 'Toggle [s]how[k]eys' })
 
--- conform
-remap('n', '<leader>fm', function() require('conform').format { lsp_fallback = true } end, '[f]or[m]at file with linter')
-
--- spectre
-remap('n', '<leader>S', '<cmd>SpectreToggle<CR>', 'Toggle [S]earch & Replace')
-remap({ 'n', 'v' }, '<leader>sw', '<cmd>SpectreCurrWord<CR>', '[s]earch current [w]ord')
-remap('n', '<leader>sf', '<cmd>SpectreCurrFile<CR>', '[s]earch current word & [f]ile')
+-- Conform
+key.set('n', '<leader>fm', function() require('conform').format { lsp_fallback = true } end, { desc = '[f]or[m]at file with linter' })
