a6c53eb4a10caea1d00f1d8af343414e1f0b1723
Vivek Jha <vivek.x.jha@gmail.com>
2025-01-02 14:21:47 -0800

    refactor(nvim): made autopairs declarative and removed config

--- nvim/lua/plugins/autocmps.lua
+++ nvim/lua/plugins/autocmps.lua
@@ -57,16 +57,16 @@ return {
   {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
-    opts = {
-      fast_wrap = {},
-      disable_filetype = { 'TelescopePrompt', 'vim' },
-    },
-    config = function(_, opts)
-      require('nvim-autopairs').setup(opts)
-
-      -- setup cmp for autopairs
+    opts = function()
+      --- @type table setup cmp for autopairs
       local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+
       require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
+      return {
+        fast_wrap = {},
+        disable_filetype = { 'TelescopePrompt', 'vim' },
+      }
     end,
   },
 
