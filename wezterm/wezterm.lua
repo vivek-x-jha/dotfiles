35df447c8c985de86da0914dade4706efa7b479b
Vivek Jha <vivek.x.jha@gmail.com>
2025-01-09 01:03:24 -0800

    fix(wezterm): reverted to old config style

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,44 +1,45 @@
 -- https://wezfurlong.org/wezterm/
 
-local wez = require 'wezterm'
+local wezterm = require 'wezterm'
 
-return vim.tbl_deep_extend('force', wez.config_builder() or {}, {
-  -- Cross-platform settings
-  color_scheme = 'Sourdiesel',
-  font = wez.font_with_fallback {
-    { family = 'JetBrainsMono Nerd Font', weight = 'ExtraLight' }, -- Primary font
-    'Menlo', -- macOS fallback
-    'DejaVu Sans Mono', -- Linux fallback
-    'Consolas', -- Windows fallback
-  },
-  font_size = 15,
-  cursor_blink_rate = 300,
-  cursor_thickness = 0.25,
-  default_cursor_style = 'BlinkingBlock',
-  window_background_opacity = 0.9,
-  window_close_confirmation = 'NeverPrompt',
-  window_decorations = 'RESIZE',
-  window_padding = {
-    left = 20,
-    right = 15,
-    top = 15,
-    bottom = 12,
-  },
-  enable_tab_bar = false,
+-- Configuration
+local config = wezterm.config_builder() or {}
 
-  -- Command Palette: activate with <Ctrl + Shift + p>
-  command_palette_fg_color = '#cdd6f4',
-  command_palette_bg_color = '#212030',
-  command_palette_font_size = 16,
-  command_palette_rows = 20,
+-- Cross-platform settings
+config.color_scheme = 'Sourdiesel'
+config.font = wezterm.font_with_fallback {
+  { family = 'JetBrainsMono Nerd Font', weight = 'ExtraLight' }, -- Primary font
+  'Menlo', -- macOS fallback
+  'DejaVu Sans Mono', -- Linux fallback
+  'Consolas', -- Windows fallback
+}
+config.font_size = 15
+config.cursor_blink_rate = 300
+config.cursor_thickness = 0.25
+config.default_cursor_style = 'BlinkingBlock'
+config.window_background_opacity = 0.9
+config.window_close_confirmation = 'NeverPrompt'
+config.window_decorations = 'RESIZE'
+config.window_padding = {
+  left = 20,
+  right = 15,
+  top = 15,
+  bottom = 12,
+}
+config.enable_tab_bar = false
 
-  -- Blur only available on MacOS
-  macos_window_background_blur = (
-    wez.target_triple == 'x86_64-apple-darwin' or wez.target_triple == 'aarch64-apple-darwin'
-  )
-      and 50
-    or nil,
+-- Command Palette: activate with <Ctrl + Shift + p>
+config.command_palette_fg_color = '#cdd6f4'
+config.command_palette_bg_color = '#212030'
+config.command_palette_font_size = 16
+config.command_palette_rows = 20
 
-  -- Style Tweaks
-  bold_brightens_ansi_colors = 'BrightOnly',
-})
+-- Blur only available on MacOS
+if wezterm.target_triple == 'x86_64-apple-darwin' or wezterm.target_triple == 'aarch64-apple-darwin' then
+  config.macos_window_background_blur = 50
+end
+
+-- Style Tweaks
+config.bold_brightens_ansi_colors = 'BrightOnly'
+
+return config
