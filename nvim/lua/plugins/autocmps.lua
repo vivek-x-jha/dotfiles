3c347a8c80eddeb98de00a5b3ee628a0c272092c
Vivek Jha <vivek.x.jha@gmail.com>
2025-01-09 19:37:45 -0800

    refactor(nvim): removed unecessary local vars and used utils

--- nvim/lua/plugins/autocmps.lua
+++ nvim/lua/plugins/autocmps.lua
@@ -1,3 +1,6 @@
+local aucmd = require('configs.utils').create_auto_command
+local augroup = require('configs.utils').create_auto_group
+
 return {
   -- https://github.com/saghen/blink.cmp
   {
@@ -20,34 +23,31 @@ return {
     'L3MON4D3/LuaSnip',
     dependencies = 'rafamadriz/friendly-snippets',
     opts = function()
-      local api = vim.api
-      local aucmd = api.nvim_create_autocmd
-      local currbuf = api.nvim_get_current_buf
-      local g = vim.g
-      local luasnip = require 'luasnip'
-      local cnf = luasnip.config
-      local sess = luasnip.session
       local options = { history = true, updateevents = 'TextChanged,TextChangedI' }
+      local luasnip = require 'luasnip'
 
-      cnf.set_config(options)
+      luasnip.config.set_config(options)
 
       -- vscode format
-      require('luasnip.loaders.from_vscode').lazy_load { exclude = g.vscode_snippets_exclude or {} }
-      require('luasnip.loaders.from_vscode').lazy_load { paths = g.vscode_snippets_path or '' }
+      require('luasnip.loaders.from_vscode').lazy_load { exclude = vim.g.vscode_snippets_exclude or {} }
+      require('luasnip.loaders.from_vscode').lazy_load { paths = vim.g.vscode_snippets_path or '' }
 
       -- snipmate format
       require('luasnip.loaders.from_snipmate').load()
-      require('luasnip.loaders.from_snipmate').lazy_load { paths = g.snipmate_snippets_path or '' }
+      require('luasnip.loaders.from_snipmate').lazy_load { paths = vim.g.snipmate_snippets_path or '' }
 
       -- lua format
       require('luasnip.loaders.from_lua').load()
-      require('luasnip.loaders.from_lua').lazy_load { paths = g.lua_snippets_path or '' }
+      require('luasnip.loaders.from_lua').lazy_load { paths = vim.g.lua_snippets_path or '' }
 
-      aucmd('InsertLeave', {
+      aucmd {
+        desc = 'Reset Snippet',
+        group = augroup 'LuaSnipAU',
+        event = 'InsertLeave',
         callback = function()
-          if sess.current_nodes[currbuf()] and not sess.jump_active then luasnip.unlink_current() end
+          if luasnip.session.current_nodes[vim.api.nvim_get_current_buf()] and not luasnip.session.jump_active then luasnip.unlink_current() end
         end,
-      })
+      }
 
       return options
     end,
