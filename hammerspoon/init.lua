8f7823832259b8f6c2af5ddadc3486e6b7bbc3e0
Vivek Jha <vivek.x.jha@gmail.com>
2025-01-17 13:10:19 -0800

    refactor(hammerspoon): abstracted loading mechanism to utility functions set_console() and bind_hotkeys()

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,17 +1,8 @@
 -- https://www.hammerspoon.org/docs/
 package.path = table.concat { hs.configdir, '/configs/?.lua;', package.path }
 
-local options = require 'options'
-local utl = require 'utils'
+-- set hammerspoon theme
+require('utils').set_console 'configs.options'
 
--- Set console theme
-for setting, opts in pairs(options) do
-  if setting == 'toolbar' and not opts then
-    hs.console.toolbar(nil)
-  else
-    hs.console[setting](opts)
-  end
-end
-
--- Set hotkeys
-utl.bind_hotkeys '/configs/hotkeys'
+-- load hotkeys
+require('utils').bind_hotkeys 'configs.hotkeys'
