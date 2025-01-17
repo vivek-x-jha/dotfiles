c9dcf00ac55b3d3f5824955a85b7f12a6abd92a8
Vivek Jha <vivek.x.jha@gmail.com>
2025-01-17 13:10:19 -0800

    refactor(hammerspoon): abstracted loading mechanism to utility functions set_console() and bind_hotkeys()

--- hammerspoon/configs/utils.lua
+++ hammerspoon/configs/utils.lua
@@ -1,10 +1,31 @@
 return {
-  bind_hotkeys = function(directory)
-    for file in hs.fs.dir(hs.configdir .. directory) do
+  set_console = function(opts_path)
+    local options = require(opts_path)
+
+    for setting, opts in pairs(options) do
+      if setting == 'toolbar' and not opts then
+        hs.console.toolbar(nil)
+      else
+        hs.console[setting](opts)
+      end
+    end
+  end,
+
+  bind_hotkeys = function(hotkey_path)
+    -- Translate the dot-based hotkey_path into a file path
+    local path = hs.configdir .. '/' .. hotkey_path:gsub('%.', '/')
+
+    if not hs.fs.attributes(path) then
+      print('Error: hotkey_path does not exist: ', path)
+      return
+    end
+
+    -- Iterate over files in the hotkey_path
+    for file in hs.fs.dir(path) do
       if file:sub(-4) == '.lua' then
-        -- Build the module name relative to the base directory
-        local module_name = 'hotkeys.' .. file:sub(1, -5)
+        local module_name = hotkey_path .. '.' .. file:sub(1, -5)
         local success, mappings = pcall(require, module_name)
+
         if success and type(mappings) == 'table' then
           for _, spec in ipairs(mappings) do
             if spec.mods and spec.key and spec.desc and spec.command then
@@ -14,7 +35,7 @@ return {
             end
           end
         else
-          print('Failed to load ' .. file .. ': ' .. tostring(mappings))
+          print('Failed to load ' .. module_name .. ': ' .. tostring(mappings))
         end
       end
     end
