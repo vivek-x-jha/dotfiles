e43cd52beaeeb5827b2ee20f66d23049a6277695
Vivek Jha <vivek.x.jha@gmail.com>
2025-02-10 00:44:37 -0800

    fix(bootstrap): updated entire script with step counter increments, commenting, and fixed 1password ssh agent with sed, and fixed atuin login and logout

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 # TODO Fix zsh compinit insecure directories warning: chmod -R go-w "$(brew --prefix)/share"
 
-# Turn on 1Password SSH Agent
-# https://developer.1password.com/docs/ssh/get-started#step-3-turn-on-the-1password-ssh-agent)
-
 # Ensure Xcode installed
 command -v xcode-select &> /dev/null || { echo 'Please run: xcode-select --install'; exit 1; }
 
-declare -i step=1
-
 echo "󰓒 INSTALLATION START 󰓒"
-echo "󰓒 [$step/14] INSTALLING PACKAGE MANAGER 󰓒" && step+=1
+step=0
+
+((step++)); echo "󰓒 [$step/14] INSTALLING PACKAGE MANAGER 󰓒"
+
+# Ensure Homebrew installed
 command -v brew &> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Set Homebrew path based on architecture
@@ -22,11 +21,12 @@ esac
 
 # Load Homebrew env vars and prepend to path in current session
 eval "$("$HOMEBREW_BIN/brew" shellenv)"
+echo "Homebrew successfully installed: HOMEBREW_BIN=$(brew --prefix)/bin"
 
-echo "󰓒 [2/14] INSTALLING TOOLS & APPS 󰓒"
-read -p 'Install [all/tools/apps] (Press Enter to Skip): '  install_type
-read -p 'Run Homebrew diagnostics and maintenance? [y/n]: ' brew_diagnostics
-read -p 'Run brew cask upgrade? [y/n]: '                    brew_cask_upgrade
+((step++)); echo "󰓒 [$step/14] INSTALLING COMMANDS & APPS 󰓒"
+
+# Load Homebrew install vars
+read -p 'Install [all/cmds/apps] (Press Enter to Skip): ' install_type
 
 brew_install () {
   local brewfile='https://raw.githubusercontent.com/vivek-x-jha/dotfiles/refs/heads/main/brew/.Brewfile'
@@ -37,27 +37,31 @@ brew_install () {
   esac
 } 
 
+# Install choice
 case "$install_type" in
-  'all'  ) brew_install ;;
-  'tools') brew_install '^tap '  '-n1 brew tap'
-           brew_install '^brew ' 'brew install' ;;
-  'apps' ) brew_install '^tap '  '-n1 brew tap'
-           brew_install '^brew ' 'brew install --cask' ;;
+  'all' ) brew_install ;;
+  'cmds') brew_install '^tap '  '-n1 brew tap'
+          brew_install '^brew ' 'brew install' ;;
+  'apps') brew_install '^tap '  '-n1 brew tap'
+          brew_install '^brew ' 'brew install --cask' ;;
 esac
 
-echo "󰓒 [$step/14] RUNNING HOMEBREW DIAGNOSTICS 󰓒" && step+=1
+read -p 'Run Homebrew diagnostics? [Y/y] (Press Enter to Skip): ' brew_diagnostics
+((step++))
 if [[ "$brew_diagnostics" =~ ^[Yy]$ ]]; then
+  echo "󰓒 [$step/14] RUNNING HOMEBREW DIAGNOSTICS 󰓒"
   brew upgrade
   brew cleanup
   brew doctor
+else
+  echo "󰓒 [$step/14] SKIPPING HOMEBREW DIAGNOSTICS 󰓒"
 fi
 
-echo "󰓒 [$step/14] INITIALIZING BREW CASK UPGRADE 󰓒" && step+=1
+((step++)); echo "󰓒 [$step/14] INITIALIZING BREW CASK UPGRADE 󰓒"
 brew tap buo/cask-upgrade
-brew install brew-cask-upgrade
-[[ "$brew_cask_upgrade" =~ ^[Yy]$ ]] && brew cu -af
+read -p 'Run brew cask upgrade? [Y/y] (Press Enter to Skip): ' && [[ $REPLY =~ ^[Yy]$ ]] && brew cu -af
 
-echo "󰓒 [$step/14] SET ENVIRONMENT 󰓒" && step+=1
+((step++)); echo "󰓒 [$step/14] SET ENVIRONMENT 󰓒"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
@@ -117,7 +121,7 @@ while true; do
   [ -z "$RE_ENTER" ] && break
 done
 
-echo "󰓒 [$step/14] CREATE SYMLINKS & DIRECTORIES 󰓒" && step+=1
+((step++)); echo "󰓒 [$step/14] CREATE SYMLINKS & DIRECTORIES 󰓒"
 
 symlink() {
   local src="$1"
@@ -191,76 +195,91 @@ symlinks=(
 
 for ((i=0; i<${#symlinks[@]}; i+=3)); do symlink "${symlinks[i]}" "${symlinks[i+1]}" "${symlinks[i+2]}"; done
 
-echo "󰓒 [$step/14] CONFIGURE MACOS OPTIONS 󰓒" && step+=1
+((step++)); echo "󰓒 [$step/14] CONFIGURE MACOS OPTIONS 󰓒"
+num=0
 
-echo "opt1: Change default screenshots location to '~/Pictures/screenshots/'"
+((num++)); echo "opt${num}: Change default screenshots location to ~/Pictures/screenshots/"
 [ -d "$HOME/Pictures/screenshots" ] || mkdir "$HOME/Pictures/screenshots"
 defaults write com.apple.screencapture location -string "$HOME/Pictures/screenshots"
 
-echo 'opt2: Speed up dock animation'
+((num++)); echo "opt${num}: Speed up dock animation"
 defaults write com.apple.dock autohide-delay -float 0.1
 
-echo 'opt3: Remove dock autohide animation'
+((num++)); echo "opt${num}: Remove dock autohide animation"
 defaults write com.apple.dock autohide-time-modifier -int 0
 
-echo 'opt4: Show app switcher on all screens'
+((num++)); echo "opt${num}: Show app switcher on all screens"
 defaults write com.apple.dock appswitcher-all-displays -bool true
 
-echo 'opt5: Shorten Mission Control animation'
+((num++)); echo "opt${num}: Shorten Mission Control animation"
 defaults write com.apple.dock expose-animation-duration -float 0.1
 
-echo 'opt6: Use list view in Finder'
+((num++)); echo "opt${num}: Use list view in Finder"
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
-echo 'opt7: Enable quitting Finder via ⌘ + Q'
+((num++)); echo "opt${num}: Enable quitting Finder via ⌘ + Q"
 defaults write com.apple.finder QuitMenuItem -bool true
 
-echo 'opt8: Show hidden files in Finder'
+((num++)); echo "opt${num}: Show hidden files in Finder"
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
-echo 'opt9: Disable file extension change warning'
+((num++)); echo "opt${num}: Disable file extension change warning"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-echo "󰓒 [$step/14] CONFIGURE GIT AND GITHUB CLI 󰓒" && step+=1
+((step++)); echo "󰓒 [$step/14] CONFIGURE GIT AND GITHUB CLI 󰓒"
 
+# Update dotfiles git url to ssh
 git -C "$HOME/.dotfiles" remote set-url origin "git@github.com:$GITHUB_NAME/dotfiles.git"
+git -C "$HOME/.dotfiles" remote --verbose
 
+# Update git config
 git config --global user.name       "$GIT_NAME"
 git config --global user.email      "$GIT_EMAIL"
 git config --global user.signingkey "$GIT_SIGNINGKEY"
 
+# Update ssh allowed signers
 echo "$GIT_EMAIL $GIT_SIGNINGKEY" > "$XDG_CONFIG_HOME/ssh/allowed_signers"
 
+# Update 1password ssh agent: https://developer.1password.com/docs/ssh/agent/config
+sed -i '' "s/vault = \"Private\"/vault = \"$OP_VAULT\"/g" "$XDG_CONFIG_HOME/1Password/ssh/agent.toml"
+
+# Authenticate GitHub CLI
 command -v gh &> /dev/null || brew install gh && gh auth login
 
-cat <<EOF > "$XDG_CONFIG_HOME/1Password/ssh/agent.toml"
-# https://developer.1password.com/docs/ssh/agent/config
+((step++)); echo "󰓒 [$step/14] SETUP ATUIN & SYNC 󰓒"
 
-[[ssh-keys]]
-item = "GitHub Auth Key"
-vault = "$OP_VAULT"
-
-[[ssh-keys]]
-item = "GitHub Signing Key"
-vault = "$OP_VAULT"
-EOF
-
-echo "󰓒 [$step/14] SETUP ATUIN & SYNC 󰓒" && step+=1
+# Ensure 1password cli installed
 command -v op &> /dev/null || brew install 1password-cli
-op signin && op item create \
+
+# Authenticate 1password & create Atuin Sync login
+op signin &>/dev/null && \
+op item create \
   --vault "$OP_VAULT" \
   --category login \
   --title "$ATUIN_OP_TITLE" \
   --generate-password='letters,digits,symbols,32' \
   "username=$ATUIN_USERNAME" \
   "email[text]=$ATUIN_EMAIL" \
-  "key[password]=update this with \$(atuin key)" &>/dev/null
+  "key[password]=update this with \$(atuin key)" &>/dev/null && \
+ATUIN_PASSWORD=$(op item get "$ATUIN_OP_TITLE" --vault "$OP_VAULT" --fields password)
 
-command -v atuin &> /dev/null || brew install atuin && atuin register -u "$ATUIN_USERNAME" -e "$ATUIN_EMAIL"
+# Ensure Atuin installed
+command -v atuin &> /dev/null || brew install atuin
+
+# Logout of current session before registering
+atuin logout
+atuin register -u "$ATUIN_USERNAME" -e "$ATUIN_EMAIL"
+atuin login -u "$ATUIN_USERNAME" -p "$ATUIN_PASSWORD"
+
+# Update Atuin Sync with generated key
 op item edit "$ATUIN_OP_TITLE" "key=$(atuin key)"
+
+# Sync shell history & integrate with Atuin history
 atuin import auto && atuin sync
 
-echo "󰓒 [$step/14] DOWNLOAD AND INSTALL PYTHON 󰓒" && step+=1
+((step++)); echo "󰓒 [$step/14] DOWNLOAD AND INSTALL PYTHON 󰓒"
+
+# Downloads & installs Python - cleans installer after finishing
 if [ ! -d "$PYTHON_APP_PATH" ]; then
   echo "Python 3.13 not found: Downloading and installing..."
 
@@ -273,20 +292,23 @@ else
   echo "Python $PYTHON_VERSION already installed"
 fi
 
-echo "󰓒 [$step/14] SETUP ITERM 󰓒" && step+=1
-touch "$HOME/.hushlogin" && echo 'Created ~/.hushlogin (Surpresses iterm2 login message)' 
+# Hide tty login message for iterm
+((step++)); echo "󰓒 [$step/14] SURPRESS ITERM2 LOGIN 󰓒"
+echo 'Created ~/.hushlogin'
+touch "$HOME/.hushlogin" 
 
-echo "󰓒 [$step/14] LOAD BAT THEMES 󰓒" && step+=1
+# Need to run this any time bat theme folder changes
+((step++)); echo "󰓒 [$step/14] LOAD BAT THEMES 󰓒"
 command -v bat &> /dev/null || brew install bat && bat cache --build
 
-echo "󰓒 [$step/14] SETUP TOUCHID SUDO 󰓒" && step+=1
+# Ensure touchid possible in interactive mode or tmux
+((step++)); echo "󰓒 [$step/14] SETUP TOUCHID SUDO 󰓒"
 brew list | grep -q pam-reattach || brew install pam-reattach
 sudo cp -f "$HOME/.dotfiles/sudo/sudo_local" /etc/pam.d/sudo_local
 [[ "$(uname -m)" == 'x86_64' ]] && sudo sed -i '' 's|/opt/homebrew|/usr/local|g' /etc/pam.d/sudo_local
 
 # Installs lazy.nvim and plugins
-echo "󰓒 [$step/14] SETUP NEOVIM 󰓒" && step+=1
-cd; command -v nvim &> /dev/null || nvim
-
 # After installation finishes run :MasonInstall lua-language-server basedpyright
-echo "󰓒 INSTALLATION COMPLETE 󰓒"
+((step++)); echo "󰓒 [$step/14] SETUP NEOVIM 󰓒"
+cd; echo "󰓒 INSTALLATION COMPLETE 󰓒"
+command -v nvim &> /dev/null || exec nvim
