ffa468cd7d5bd89ad2dd4079fa973e32d556a086
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-02 20:49:33 -0800

    feat(bash): turned on vim mode
    fixed transien prompt for bash

--- blesh/init.sh
+++ blesh/init.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-# https://starship.rs/advanced-config/#transientprompt-and-transientrightprompt-in-cmd
-bleopt prompt_ps1_final='$(starship module time)'
+bleopt prompt_ps1_transient=always
