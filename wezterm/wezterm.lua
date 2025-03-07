2482f7282d468c48f3d084ee23a82b8a481894ea
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-06 23:23:27 -0800

    style(wezterm): added empty lines to separate logical sections

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,6 +9,7 @@ config.color_scheme = 'Sourdiesel'
 
 -- Set font opts
 config.font_size = 15
+
 config.font = wezterm.font_with_fallback {
   { family = 'JetBrainsMono Nerd Font', weight = 'ExtraLight' }, -- Primary font
   'Menlo', -- macOS fallback
@@ -22,9 +23,11 @@ config.cursor_thickness = 0.25
 config.default_cursor_style = 'BlinkingBlock'
 
 -- Set window opts
+config.macos_window_background_blur = wezterm.target_triple:match 'apple%-darwin' and 60 -- Blur only available on macOS
 config.window_background_opacity = 0.9
 config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = 'RESIZE'
+
 config.window_frame = {
   border_left_width = 2,
   border_right_width = 2,
@@ -35,6 +38,7 @@ config.window_frame = {
   border_bottom_color = '#5c617d',
   border_top_color = '#5c617d',
 }
+
 config.window_padding = {
   left = 20,
   right = 15,
@@ -50,9 +54,6 @@ config.command_palette_fg_color = '#cdd6f4'
 config.command_palette_bg_color = '#212030'
 config.command_palette_font_size = 16
 config.command_palette_rows = 20
-
--- Blur only available on MacOS
-config.macos_window_background_blur = wezterm.target_triple:match 'apple%-darwin' and 60
 
 -- Style Tweaks
 config.bold_brightens_ansi_colors = false
