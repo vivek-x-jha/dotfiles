dded41cf9e834f60f0aa2c06261f85dd5104a1ba
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-07 12:33:00 -0800

    refactor(wezterm): made wezterm config monolithic
    also updated stylua to sync with nvim stylua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,15 +1,31 @@
--- https://wezfurlong.org/wezterm/
-
--- Create config table
+-- https://wezterm.org/config/files.html?h=config
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder() or {}
 
--- Set terminal colors
-config.color_scheme = 'Sourdiesel'
+-- https://wezterm.org/config/appearance.html#defining-your-own-colors
+config.bold_brightens_ansi_colors = false
+config.colors = {
+  ansi = { '#cccccc', '#ffc7c7', '#ceffc9', '#fdf7cd', '#c4effa', '#eccef0', '#8ae7c5', '#f4f3f2' },
+  background = '#212030',
+  brights = { '#5c617d', '#f096b7', '#d2fd9d', '#f3b175', '#80d7fe', '#c9ccfb', '#47e7b1', '#ffffff' },
+  compose_cursor = '#f2cdcd',
+  cursor_bg = '#cdd6f4',
+  cursor_border = '#f4f3f2',
+  cursor_fg = '#313244',
+  foreground = '#f4f3f2',
+  selection_bg = '#5c617d',
+  selection_fg = '#f4f3f2',
+  split = '#5c617d',
+}
 
--- Set font opts
+-- Command Palette: activate with <Ctrl + Shift + p>
+config.command_palette_fg_color = '#cdd6f4'
+config.command_palette_bg_color = '#212030'
+config.command_palette_font_size = 16
+config.command_palette_rows = 20
+
+-- https://wezterm.org/config/fonts.html?h=fonts
 config.font_size = 15
-
 config.font = wezterm.font_with_fallback {
   { family = 'JetBrainsMono Nerd Font', weight = 'ExtraLight' }, -- Primary font
   'Menlo', -- macOS fallback
@@ -23,11 +39,15 @@ config.cursor_thickness = 0.25
 config.default_cursor_style = 'BlinkingBlock'
 
 -- Set window opts
-config.macos_window_background_blur = wezterm.target_triple:match 'apple%-darwin' and 60 -- Blur only available on macOS
+config.enable_tab_bar = false
+
+if wezterm.target_triple:match 'apple%-darwin' then config.macos_window_background_blur = 60 end
 config.window_background_opacity = 0.9
+
 config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = 'RESIZE'
 
+-- https://wezterm.org/config/lua/config/window_frame.html?h=window_frame
 config.window_frame = {
   border_left_width = 2,
   border_right_width = 2,
@@ -39,23 +59,12 @@ config.window_frame = {
   border_top_color = '#5c617d',
 }
 
+-- https://wezterm.org/config/lua/config/window_padding.html?h=window_padd
 config.window_padding = {
   left = 20,
   right = 15,
   top = 15,
   bottom = 12,
 }
-
--- Disable title bar
-config.enable_tab_bar = false
-
--- Command Palette: activate with <Ctrl + Shift + p>
-config.command_palette_fg_color = '#cdd6f4'
-config.command_palette_bg_color = '#212030'
-config.command_palette_font_size = 16
-config.command_palette_rows = 20
-
--- Style Tweaks
-config.bold_brightens_ansi_colors = false
 
 return config
