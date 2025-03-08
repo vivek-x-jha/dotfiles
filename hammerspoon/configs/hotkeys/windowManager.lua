110e65126923f0c251083824a47bb83a3761fe80
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-08 10:52:47 -0800

    feat(hammerspoon): created move_app utility to encapsulate app posiioning on screen

--- hammerspoon/configs/hotkeys/windowManager.lua
+++ hammerspoon/configs/hotkeys/windowManager.lua
@@ -1,131 +1,70 @@
 local ctrl_alt = { 'ctrl', 'alt' }
 local ctrl_alt_cmd = { 'ctrl', 'alt', 'cmd' }
 
+local move = require('utils').move_app
+
 return {
   {
     desc = 'Left Half',
     mods = ctrl_alt,
     key = 'Left',
-    command = function()
-      local win = hs.window.focusedWindow()
-      if win then
-        win:moveToUnit(hs.layout.left50)
-      else
-        hs.alert.show 'No window to move to left half'
-      end
-    end,
+    command = function() move { x = 0, y = 0, w = 0.5, h = 1 } end,
   },
 
   {
     desc = 'Right Half',
     mods = ctrl_alt,
     key = 'Right',
-    command = function()
-      local win = hs.window.focusedWindow()
-      if win then
-        win:moveToUnit(hs.layout.right50)
-      else
-        hs.alert.show 'No window to move to right half'
-      end
-    end,
+    command = function() move { x = 0.5, y = 0, w = 0.5, h = 1 } end,
   },
 
   {
     desc = 'Center Half',
     mods = ctrl_alt,
     key = 'C',
-    command = function()
-      local win = hs.window.focusedWindow()
-      if win then
-        win:moveToUnit(hs.geometry.rect(0.25, 0.25, 0.5, 0.5))
-      else
-        hs.alert.show 'No window to center'
-      end
-    end,
+    command = function() move { x = 0.25, y = 0.25, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Top Half',
     mods = ctrl_alt,
     key = 'Up',
-    command = function()
-      local win = hs.window.focusedWindow()
-      if win then
-        win:moveToUnit(hs.geometry.rect(0, 0, 1, 0.5))
-      else
-        hs.alert.show 'No window to move to top half'
-      end
-    end,
+    command = function() move { x = 0, y = 0, w = 1, h = 0.5 } end,
   },
 
   {
     desc = 'Bottom Half',
     mods = ctrl_alt,
     key = 'Down',
-    command = function()
-      local win = hs.window.focusedWindow()
-      if win then
-        win:moveToUnit(hs.geometry.rect(0, 0.5, 1, 0.5))
-      else
-        hs.alert.show 'No window to move to bottom half'
-      end
-    end,
+    command = function() move { x = 0, y = 0.5, w = 1, h = 0.5 } end,
   },
 
   {
     desc = 'Top Left',
     mods = ctrl_alt,
     key = 'U',
-    command = function()
-      local win = hs.window.focusedWindow()
-      if win then
-        win:moveToUnit(hs.geometry.rect(0, 0, 0.5, 0.5))
-      else
-        hs.alert.show 'No window to move to top left'
-      end
-    end,
+    command = function() move { x = 0, y = 0, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Top Right',
     mods = ctrl_alt,
     key = 'I',
-    command = function()
-      local win = hs.window.focusedWindow()
-      if win then
-        win:moveToUnit(hs.geometry.rect(0.5, 0, 0.5, 0.5))
-      else
-        hs.alert.show 'No window to move to top right'
-      end
-    end,
+    command = function() move { x = 0.5, y = 0, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Bottom Left',
     mods = ctrl_alt,
     key = 'J',
-    command = function()
-      local win = hs.window.focusedWindow()
-      if win then
-        win:moveToUnit(hs.geometry.rect(0, 0.5, 0.5, 0.5))
-      else
-        hs.alert.show 'No window to move to bottom left'
-      end
-    end,
+    command = function() move { x = 0, y = 0.5, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Bottom Right',
     mods = ctrl_alt,
     key = 'K',
-    command = function()
-      local win = hs.window.focusedWindow()
-      if win then
-        win:moveToUnit(hs.geometry.rect(0.5, 0.5, 0.5, 0.5))
-      else
-        hs.alert.show 'No window to move to bottom right'
-      end
-    end,
+    command = function() move { x = 0.5, y = 0.5, w = 0.5, h = 0.5 } end,
   },
 
   {
