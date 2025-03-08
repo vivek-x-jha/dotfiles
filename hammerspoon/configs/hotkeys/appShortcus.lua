f03182067df23aa636a9463e458ccee6567b55dc
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-08 15:48:17 -0800

    refactor(hammerspoon): created global variables HYPER and CTRL_ALT

--- hammerspoon/configs/hotkeys/appShortcus.lua
+++ hammerspoon/configs/hotkeys/appShortcus.lua
@@ -4,56 +4,56 @@ return {
   {
     key = 'a',
     desc = 'Toggle Arc Browser',
-    mods = { 'ctrl', 'alt', 'cmd' },
+    mods = HYPER,
     command = function() toggle 'Arc' end,
   },
 
   {
     key = 'c',
     desc = 'Toggle ChatGPT',
-    mods = { 'ctrl', 'alt', 'cmd' },
+    mods = HYPER,
     command = function() toggle 'ChatGPT' end,
   },
 
   {
     key = 'm',
     desc = 'Toggle Mimestream',
-    mods = { 'ctrl', 'alt', 'cmd' },
+    mods = HYPER,
     command = function() toggle 'Mimestream' end,
   },
 
   {
     key = 'n',
     desc = 'Toggle Notion Calendar',
-    mods = { 'ctrl', 'alt', 'cmd' },
+    mods = HYPER,
     command = function() toggle 'Notion Calendar' end,
   },
 
   {
     key = 'p',
     desc = 'Toggle Photos',
-    mods = { 'ctrl', 'alt', 'cmd' },
+    mods = HYPER,
     command = function() toggle 'Photos' end,
   },
 
   {
     key = 's',
     desc = 'Toggle Spotify',
-    mods = { 'ctrl', 'alt', 'cmd' },
+    mods = HYPER,
     command = function() toggle 'Spotify' end,
   },
 
   {
     key = 't',
     desc = 'Toggle Messages',
-    mods = { 'ctrl', 'alt', 'cmd' },
+    mods = HYPER,
     command = function() toggle 'Messages' end,
   },
 
   {
     key = 'w',
     desc = 'Toggle WezTerm',
-    mods = { 'ctrl', 'alt', 'cmd' },
+    mods = HYPER,
     command = function() toggle 'WezTerm' end,
   },
 }
--- hammerspoon/configs/hotkeys/windowManager.lua
+++ hammerspoon/configs/hotkeys/windowManager.lua
@@ -3,91 +3,91 @@ local move = require('utils').move_app
 return {
   {
     desc = 'Left Half',
-    mods = { 'ctrl', 'alt' },
+    mods = CTRL_ALT,
     key = 'Left',
     command = function() move { x = 0, y = 0, w = 0.5, h = 1 } end,
   },
 
   {
     desc = 'Right Half',
-    mods = { 'ctrl', 'alt' },
+    mods = CTRL_ALT,
     key = 'Right',
     command = function() move { x = 0.5, y = 0, w = 0.5, h = 1 } end,
   },
 
   {
     desc = 'Center Half',
-    mods = { 'ctrl', 'alt' },
+    mods = CTRL_ALT,
     key = 'C',
     command = function() move { x = 0.25, y = 0.25, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Top Half',
-    mods = { 'ctrl', 'alt' },
+    mods = CTRL_ALT,
     key = 'Up',
     command = function() move { x = 0, y = 0, w = 1, h = 0.5 } end,
   },
 
   {
     desc = 'Bottom Half',
-    mods = { 'ctrl', 'alt' },
+    mods = CTRL_ALT,
     key = 'Down',
     command = function() move { x = 0, y = 0.5, w = 1, h = 0.5 } end,
   },
 
   {
     desc = 'Top Left',
-    mods = { 'ctrl', 'alt' },
+    mods = CTRL_ALT,
     key = 'U',
     command = function() move { x = 0, y = 0, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Top Right',
-    mods = { 'ctrl', 'alt' },
+    mods = CTRL_ALT,
     key = 'I',
     command = function() move { x = 0.5, y = 0, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Bottom Left',
-    mods = { 'ctrl', 'alt' },
+    mods = CTRL_ALT,
     key = 'J',
     command = function() move { x = 0, y = 0.5, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Bottom Right',
-    mods = { 'ctrl', 'alt' },
+    mods = CTRL_ALT,
     key = 'K',
     command = function() move { x = 0.5, y = 0.5, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Maximize',
-    mods = { 'ctrl', 'alt' },
+    mods = CTRL_ALT,
     key = 'F',
     command = function() move 'maximize' end,
   },
 
   {
     desc = 'Left Display',
-    mods = { 'ctrl', 'alt', 'cmd' },
+    mods = HYPER,
     key = 'Left',
     command = function() move 'next' end,
   },
 
   {
     desc = 'Right Display',
-    mods = { 'ctrl', 'alt', 'cmd' },
+    mods = HYPER,
     key = 'Right',
     command = function() move 'previous' end,
   },
 
   {
     desc = 'Almost Maximize',
-    mods = { 'ctrl', 'alt' },
+    mods = CTRL_ALT,
     key = 'A',
     command = function()
       local win = hs.window.focusedWindow()
