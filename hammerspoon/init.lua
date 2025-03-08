59928bcdd2de2c76fdca73c4e3e27779dcf6ae7d
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-08 15:48:17 -0800

    refactor(hammerspoon): created global variables HYPER and CTRL_ALT

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,7 @@
 -- https://www.hammerspoon.org/docs/
+HYPER = { 'ctrl', 'alt', 'cmd' }
+CTRL_ALT = { 'ctrl', 'alt' }
+
 package.path = table.concat { hs.configdir, '/configs/?.lua;', package.path }
 
 -- set hammerspoon theme
