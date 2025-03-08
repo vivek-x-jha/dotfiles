e8fb014c6233785bef98220270191db648108140
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-08 15:17:47 -0800

    feat(hammerspoon): updated list of app shortcuts

--- hammerspoon/configs/hotkeys/appShortcus.lua
+++ hammerspoon/configs/hotkeys/appShortcus.lua
@@ -2,17 +2,17 @@ local toggle = require('utils').toggle_app
 
 return {
   {
-    key = 'w',
-    desc = 'Toggle WezTerm',
-    mods = { 'ctrl', 'alt', 'cmd' },
-    command = function() toggle 'WezTerm' end,
-  },
-
-  {
     key = 'a',
     desc = 'Toggle Arc Browser',
     mods = { 'ctrl', 'alt', 'cmd' },
     command = function() toggle 'Arc' end,
+  },
+
+  {
+    key = 'c',
+    desc = 'Toggle ChatGPT',
+    mods = { 'ctrl', 'alt', 'cmd' },
+    command = function() toggle 'ChatGPT' end,
   },
 
   {
@@ -23,9 +23,37 @@ return {
   },
 
   {
+    key = 'n',
+    desc = 'Toggle Notion Calendar',
+    mods = { 'ctrl', 'alt', 'cmd' },
+    command = function() toggle 'Notion Calendar' end,
+  },
+
+  {
     key = 'p',
     desc = 'Toggle Photos',
     mods = { 'ctrl', 'alt', 'cmd' },
     command = function() toggle 'Photos' end,
+  },
+
+  {
+    key = 's',
+    desc = 'Toggle Spotify',
+    mods = { 'ctrl', 'alt', 'cmd' },
+    command = function() toggle 'Spotify' end,
+  },
+
+  {
+    key = 't',
+    desc = 'Toggle Messages',
+    mods = { 'ctrl', 'alt', 'cmd' },
+    command = function() toggle 'Messages' end,
+  },
+
+  {
+    key = 'w',
+    desc = 'Toggle WezTerm',
+    mods = { 'ctrl', 'alt', 'cmd' },
+    command = function() toggle 'WezTerm' end,
   },
 }
