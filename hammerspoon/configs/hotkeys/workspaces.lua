9cda35964e8efefbb332ab7060e9950fbc31acf4
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-10 02:32:42 -0700

    refactor(hammerspoon): moved move_max_app func to utils

--- hammerspoon/configs/hotkeys/workspaces.lua
+++ hammerspoon/configs/hotkeys/workspaces.lua
@@ -1,3 +1,5 @@
+local workspace = require('configs.utils').move_max_app
+
 return {
   {
     key = '0',
@@ -11,33 +13,10 @@ return {
         return
       end
 
-      -- Assign displays
-      local centerScreen = screens[1]
-      local leftScreen = screens[2]
-      local rightScreen = screens[3]
-
-      -- Function to move, unhide, and focus an app
-      local function moveAndShowApp(appName, screen)
-        local app = hs.application.get(appName)
-        if app then
-          app:activate() -- Brings the app to the foreground
-          local win = app:mainWindow()
-          if win then
-            win:moveToScreen(screen)
-            win:maximize()
-            win:focus() -- Ensure the window is visible and active
-          else
-            hs.alert.show(appName .. ' has no open window')
-          end
-        else
-          hs.alert.show(appName .. ' not running')
-        end
-      end
-
       -- Move apps to their respective screens and make them visible
-      moveAndShowApp('ChatGPT', leftScreen)
-      moveAndShowApp('WezTerm', centerScreen)
-      moveAndShowApp('Arc', rightScreen)
+      workspace('ChatGPT', screens[2])
+      workspace('WezTerm', screens[1])
+      workspace('Arc', screens[3])
     end,
   },
 }
