8ed85863e4f7de080f31c4accc286c7bd02a2044
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-13 03:25:53 -0700

    refactor(update-icons): refactored function o fix any lsp warnings
    synced both beween bash and zsh with only difference in sorting strategy

--- bash/funcs/update.sh
+++ bash/funcs/update.sh
@@ -9,70 +9,69 @@ update_brew() {
 }
 
 update_icons() {
-command -v fileicon &> /dev/null || { printf "${BRIGHTRED}%s %s${BRIGHTYELLOW}%s${RESET}\n" "" "'fileicon' dependency not found" " - please install or check in \$PATH."; return 1; }
+  # Create directory-icons association
+  command -v fileicon &> /dev/null || { printf "${BRIGHTRED}%s %s${BRIGHTYELLOW}%s${RESET}\n" "" "'fileicon' dependency not found" " - please install or check in \$PATH."; return 1; }
 
   local table_width=60
   declare -A dir_icons=(
-    /Applications/1Password.app            ~/Pictures/icons/1password-macos.png
-    /Applications/ChatGPT.app              ~/Pictures/icons/chatgpt.png
-    /Applications/Discord.app              ~/Pictures/icons/discord-macos.png
-    /Applications/Docker.app               ~/Pictures/icons/docker.png
-    /Applications/Figma.app                ~/Pictures/icons/figma.png
-    /Applications/Firefox.app              ~/Pictures/icons/firefox-macos.png
-    /Applications/Google\ Chrome.app       ~/Pictures/icons/chrome.png
-    /Applications/iTerm.app                ~/Pictures/icons/iterm2.png
-    /Applications/iTermAI.app              ~/Pictures/icons/ai-brain.png
-    /Applications/KeyCastr.app             ~/Pictures/icons/keycastr.png
-    /Applications/Mimestream.app           ~/Pictures/icons/gmail.png
-    /Applications/Neovim-iTerm.app         ~/Pictures/icons/neovim-iterm.png
-    /Applications/Neovim-Wezterm.app       ~/Pictures/icons/neovim-wezterm.png
-    /Applications/Postman.app              ~/Pictures/icons/postman.png
-    /Applications/Slack.app                ~/Pictures/icons/slack.png
-    /Applications/Spotify.app              ~/Pictures/icons/spotify.png
-    /Applications/Tex                      ~/Pictures/icons/tex.png
-    /Applications/Visual\ Studio\ Code.app ~/Pictures/icons/vscode.png
-    /Applications/WezTerm.app              ~/Pictures/icons/wezterm.png
-    /Applications/WhatsApp.app             ~/Pictures/icons/whatsapp.png
-
-    ~/Developer                            ~/Pictures/icons/developer.png
-    ~/Downloads                            ~/Pictures/icons/download.png
-    ~/Dropbox/content                      ~/Pictures/icons/content.png
-    ~/Pictures/icons                       ~/Pictures/icons/png.png
-    ~/Pictures/screenshots                 ~/Pictures/icons/screenshot.png
-    ~/Pictures/wallpapers                  ~/Pictures/icons/wallpaper.png
-    ~/.dotfiles                            ~/Pictures/icons/gear.png
+    [/Applications/1Password.app]=~/Pictures/icons/1password-macos.png
+    [/Applications/ChatGPT.app]=~/Pictures/icons/chatgpt.png
+    [/Applications/Discord.app]=~/Pictures/icons/discord-macos.png
+    [/Applications/Docker.app]=~/Pictures/icons/docker.png
+    [/Applications/Figma.app]=~/Pictures/icons/figma.png
+    [/Applications/Firefox.app]=~/Pictures/icons/firefox-macos.png
+    [/Applications/Google\ Chrome.app]=~/Pictures/icons/chrome.png
+    [/Applications/iTerm.app]=~/Pictures/icons/iterm2.png
+    [/Applications/iTermAI.app]=~/Pictures/icons/ai-brain.png
+    [/Applications/KeyCastr.app]=~/Pictures/icons/keycastr.png
+    [/Applications/Mimestream.app]=~/Pictures/icons/gmail.png
+    [/Applications/Neovim-iTerm.app]=~/Pictures/icons/neovim-iterm.png
+    [/Applications/Neovim-Wezterm.app]=~/Pictures/icons/neovim-wezterm.png
+    [/Applications/Postman.app]=~/Pictures/icons/postman.png
+    [/Applications/Slack.app]=~/Pictures/icons/slack.png
+    [/Applications/Spotify.app]=~/Pictures/icons/spotify.png
+    [/Applications/Tex]=~/Pictures/icons/tex.png
+    [/Applications/Visual\ Studio\ Code.app]=~/Pictures/icons/vscode.png
+    [/Applications/WezTerm.app]=~/Pictures/icons/wezterm.png
+    [/Applications/WhatsApp.app]=~/Pictures/icons/whatsapp.png
+    [~/Developer]=~/Pictures/icons/developer.png
+    [~/Downloads]=~/Pictures/icons/download.png
+    [~/Dropbox/content]=~/Pictures/icons/content.png
+    [~/Pictures/icons]=~/Pictures/icons/png.png
+    [~/Pictures/screenshots]=~/Pictures/icons/screenshot.png
+    [~/Pictures/wallpapers]=~/Pictures/icons/wallpaper.png
+    [~/.dotfiles]=~/Pictures/icons/gear.png
   )
 
   # Create Directory-Icons Table Header
   print_separator() { printf "${BRIGHTBLACK}%-${table_width}s${RESET}\n" | tr ' ' '-'; }
 
   print_separator
-  printf "%-2s ${BLUE}%-37s ${MAGENTA}%-5s${RESET}\n" '' 'App / Directory' '~/Pictures/icons/'
+  printf "%-2s ${BLUE}%-37s ${MAGENTA}%-5s${RESET}\n" '' 'App / Directory' \~/Pictures/icons/
   print_separator
 
   # Sort keys while preserving spaces in the directories
-  IFS=$'\n' sorted_dirs=($(for dir in "${!dir_icons[@]}"; do echo "$dir"; done | sort))
+  mapfile -t sorted_dirs < <(printf '%s\n' "${!dir_icons[@]}" | sort)
 
   local dir_count="${#sorted_dirs[@]}"
   local success_count=$dir_count
 
   # Create Directory-Icons Table Rows
+  local icon directory
+
   for dir in "${sorted_dirs[@]}"; do
+    icon=${dir_icons[$dir]}
+    directory=${dir/#$HOME/\~}
 
-    local icon=${dir_icons["$dir"]}
-    local expanded_dir=$(echo "$dir" | sed "s|^$HOME|~|")  # Replace $HOME with ~ for output
-    local basename_dir=$(basename "$expanded_dir")
-    local basename_icon=$(basename "$icon")
+    # Function to print the status of setting file icons
+    print_status() { printf "${1}%-1s ${WHITE}%-37s ${BRIGHTMAGENTA}%-5s${RESET}\n" "$2" "$directory" "$(basename "$icon")"; }
 
-    if sudo fileicon set "$dir" "$icon" &> /dev/null; then
-      printf "${GREEN}%-1s ${WHITE}%-37s ${BRIGHTMAGENTA}%-5s${RESET}\n" " " "$expanded_dir" "$basename_icon"
-    elif [[ $basename_dir == 'Mimestream.app' ]]; then                        
-      printf "${GREEN}%-1s ${WHITE}%-37s ${BRIGHTMAGENTA}%-5s${RESET}\n" " " "$expanded_dir" "$basename_icon"
-    else                                                                      
-      printf "${RED}%-1s ${WHITE}%-37s ${BRIGHTMAGENTA}%-5s${RESET}\n"   " " "$expanded_dir" "$basename_icon"
+    if sudo fileicon set "$dir" "$icon" &> /dev/null || [[ $(basename "$directory") == 'Mimestream.app' ]]; then
+      print_status "${GREEN}" " "
+    else
+      print_status "${RED}" " "
       ((success_count--))
     fi
-
   done
 
   # Footer
@@ -81,7 +80,6 @@ command -v fileicon &> /dev/null || { printf "${BRIGHTRED}%s %s${BRIGHTYELLOW}%s
   print_separator
   printf "${GREEN}%-2s ${BRIGHTBLACK}%-37s ${RESET}\n" "$success_count" 'Folder icon(s) updated successfully'
   [ $fail_count -eq 0 ] || printf "${RED}%-2s ${BRIGHTBLACK}%-37s ${RESET}\n" "$fail_count" 'Folder icon(s) failed to update'
-  echo
 }
 
 update_texlive() {
