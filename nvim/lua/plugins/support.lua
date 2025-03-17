27911aad37350d149fa2a3b871954aba2b01fcac
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-17 16:14:15 -0700

    feat(nvim): installed smartyank with default config
    TODO learn to use

--- nvim/lua/plugins/support.lua
+++ nvim/lua/plugins/support.lua
@@ -1,4 +1,29 @@
 return {
+  -- https://github.com/ibhagwan/smartyank.nvim
+  {
+    'ibhagwan/smartyank.nvim',
+    opts = {
+      highlight = {
+        enabled = true,
+        higroup = 'YankFlash',
+        timeout = 2000,
+      },
+      clipboard = {
+        enabled = true,
+      },
+      tmux = {
+        enabled = true,
+        cmd = { 'tmux', 'set-buffer', '-w' },
+      },
+      osc52 = {
+        enabled = true,
+        ssh_only = true,
+        silent = false,
+        echo_hl = 'Directory',
+      },
+    },
+  },
+
   -- https://github.com/folke/todo-comments.nvim
   {
     'folke/todo-comments.nvim',
