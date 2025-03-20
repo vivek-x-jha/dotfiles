150d68b85e71fbdbd80d58640cd40067f85713c9
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-20 16:55:30 -0700

    feat(fzf): updated atl+c shell integration border label

--- fzf/config.sh
+++ fzf/config.sh
@@ -60,7 +60,7 @@ export FZF_DEFAULT_OPTS="
 "
 
 # https://junegunn.github.io/fzf/shell-integration/#alt-c
-export FZF_ALT_C_OPTS="--header 'builtin cd --' --border-label ' 󰉖 subdirectory '"
+export FZF_ALT_C_OPTS="--header 'builtin cd --' --border-label ' 󰉖 jump subdirs '"
 
 # https://github.com/ajeetdsouza/zoxide?tab=readme-ov-file#environment-variables
 export _ZO_FZF_OPTS="$FZF_DEFAULT_OPTS --header 'zoxide query --interactive' --border-label ' 󰉖 jump list ' --preview 'echo {} | cut -f2- | xargs -I{} $showdir'"
