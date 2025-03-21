f8ccef2c845ece00e558c8fa9c967de9ce3ccab6
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-21 14:53:48 -0700

    fix(shells): updated homebrew shellenv eval logic

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ step=0
 
 # Install Homebrew
 [[ -x /opt/homebrew/bin/brew || -x /usr/local/bin/brew ]] || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/opt/homebrew/bin/brew shellenv 2>/dev/null)" || eval "$(/usr/local/bin/brew shellenv)"
+eval "$(/opt/homebrew/bin/brew shellenv 2>/dev/null || /usr/local/bin/brew shellenv)"
 
 ((step++)); echo "󰓒 [$step/13] INSTALLING COMMANDS & APPS 󰓒"
 
