8fd9259302bd6641bab57bf77ffdc513aa10e3ec
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-20 20:09:01 -0700

    refactor(nvim): removed unneeded cmds field for vim tmux navigator plugin

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -17,13 +17,6 @@ return {
   -- https://github.com/christoomey/vim-tmux-navigator
   {
     'christoomey/vim-tmux-navigator',
-    cmd = {
-      'TmuxNavigateLeft',
-      'TmuxNavigateDown',
-      'TmuxNavigateUp',
-      'TmuxNavigateRight',
-      'TmuxNavigatePrevious',
-    },
     keys = {
       { '<c-h>', '<cmd>TmuxNavigateLeft<CR>', { desc = 'switch window left' } },
       { '<c-j>', '<cmd>TmuxNavigateDown<CR>', { desc = 'switch window down' } },
