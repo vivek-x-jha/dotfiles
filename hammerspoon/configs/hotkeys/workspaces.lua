ff58e9767b12b4f769370cff7b46b3e6edeb8fd4
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-28 20:06:14 -0700

    feat(hammerspoon): created single monitor workspace hotkey

--- hammerspoon/configs/hotkeys/workspaces.lua
+++ hammerspoon/configs/hotkeys/workspaces.lua
@@ -3,7 +3,28 @@ local utl = require 'utils'
 return {
   {
     key = '1',
-    desc = 'Set utl.positionApp: ChatGPT -> WezTerm -> Arc',
+    desc = 'Set Single Monitor Workspace',
+    mods = HYPER,
+    command = function()
+      local screens = hs.screen.allScreens()
+
+      -- Arc @ top right
+      utl.positionApp('Arc', screens[1])
+      utl.moveApp { x = 0.5, y = 0, w = 0.5, h = 0.5 }
+
+      -- ChatGPT @ bottom right
+      utl.positionApp('ChatGPT', screens[1])
+      utl.moveApp { x = 0.5, y = 0.5, w = 0.5, h = 0.5 }
+
+      -- Wezterm @ left half
+      utl.positionApp('WezTerm', screens[1])
+      utl.moveApp { x = 0, y = 0, w = 0.5, h = 1 }
+    end,
+  },
+
+  {
+    key = '2',
+    desc = 'Set 2 External Monitor Workspace',
     mods = HYPER,
     command = function()
       -- Get all screens
