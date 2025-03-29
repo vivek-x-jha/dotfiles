64d7dfca67802f5961f6e4567b8e0b622302feef
Vivek Jha <vivek.x.jha@gmail.com>
2025-03-29 16:06:56 -0700

    refactor(nvim): reshuffled lsp server variable names

--- nvim/lua/plugins/masonlspconfig.lua
+++ nvim/lua/plugins/masonlspconfig.lua
@@ -1,8 +1,10 @@
 local utl = require 'configs.utils'
-local lsp_servers = vim.fn.stdpath 'config' .. '/lua/lsp'
+local servers = vim.fn.stdpath 'config' .. '/lua/lsp'
 
 -- https://github.com/williamboman/mason-lspconfig.nvim
 return {
   'williamboman/mason-lspconfig.nvim',
-  opts = { ensure_installed = utl.filenames(lsp_servers) },
+  opts = {
+    ensure_installed = utl.filenames(servers),
+  },
 }
