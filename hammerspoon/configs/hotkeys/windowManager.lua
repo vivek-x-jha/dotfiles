a4749cdf38b808d28086ebf77e2fa3c7fadaff2d
Vivek Jha <vivek.x.jha@gmail.com>
2025-04-01 12:44:46 -0700

    refactor(hammerspoon): removed global variables for hotkeys and just use table of keystroke strings instead

--- hammerspoon/configs/hotkeys/windowManager.lua
+++ hammerspoon/configs/hotkeys/windowManager.lua
@@ -3,91 +3,91 @@ local utl = require 'utils'
 return {
   {
     desc = 'Left Half',
-    mods = CTRL_ALT,
+    mods = { 'ctrl', 'alt' },
     key = 'Left',
     command = function() utl.moveApp { x = 0, y = 0, w = 0.5, h = 1 } end,
   },
 
   {
     desc = 'Right Half',
-    mods = CTRL_ALT,
+    mods = { 'ctrl', 'alt' },
     key = 'Right',
     command = function() utl.moveApp { x = 0.5, y = 0, w = 0.5, h = 1 } end,
   },
 
   {
     desc = 'Center Half',
-    mods = CTRL_ALT,
+    mods = { 'ctrl', 'alt' },
     key = 'C',
     command = function() utl.moveApp { x = 0.25, y = 0.25, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Top Half',
-    mods = CTRL_ALT,
+    mods = { 'ctrl', 'alt' },
     key = 'Up',
     command = function() utl.moveApp { x = 0, y = 0, w = 1, h = 0.5 } end,
   },
 
   {
     desc = 'Bottom Half',
-    mods = CTRL_ALT,
+    mods = { 'ctrl', 'alt' },
     key = 'Down',
     command = function() utl.moveApp { x = 0, y = 0.5, w = 1, h = 0.5 } end,
   },
 
   {
     desc = 'Top Left',
-    mods = CTRL_ALT,
+    mods = { 'ctrl', 'alt' },
     key = 'U',
     command = function() utl.moveApp { x = 0, y = 0, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Top Right',
-    mods = CTRL_ALT,
+    mods = { 'ctrl', 'alt' },
     key = 'I',
     command = function() utl.moveApp { x = 0.5, y = 0, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Bottom Left',
-    mods = CTRL_ALT,
+    mods = { 'ctrl', 'alt' },
     key = 'J',
     command = function() utl.moveApp { x = 0, y = 0.5, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Bottom Right',
-    mods = CTRL_ALT,
+    mods = { 'ctrl', 'alt' },
     key = 'K',
     command = function() utl.moveApp { x = 0.5, y = 0.5, w = 0.5, h = 0.5 } end,
   },
 
   {
     desc = 'Maximize',
-    mods = CTRL_ALT,
+    mods = { 'ctrl', 'alt' },
     key = 'F',
     command = function() utl.moveApp 'maximize' end,
   },
 
   {
     desc = 'Left Display',
-    mods = HYPER,
+    mods = { 'ctrl', 'alt', 'cmd' },
     key = 'Left',
     command = function() utl.moveApp 'next' end,
   },
 
   {
     desc = 'Right Display',
-    mods = HYPER,
+    mods = { 'ctrl', 'alt', 'cmd' },
     key = 'Right',
     command = function() utl.moveApp 'previous' end,
   },
 
   {
     desc = 'Almost Maximize',
-    mods = CTRL_ALT,
+    mods = { 'ctrl', 'alt' },
     key = 'A',
     command = function()
       local win = hs.window.focusedWindow()
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,7 +5,4 @@ package.path = table.concat { hs.configdir, '/configs/?.lua;', package.path }
 require('utils').setConsole 'configs.options'
 
 -- load hotkeys
-HYPER = { 'ctrl', 'alt', 'cmd' }
-CTRL_ALT = { 'ctrl', 'alt' }
-
 require('utils').bindHotkeys 'configs.hotkeys'
