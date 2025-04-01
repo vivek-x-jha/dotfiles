b8e52003a25b0bc88fca658129f9b3badd003322
Vivek Jha <vivek.x.jha@gmail.com>
2025-04-01 16:07:13 -0700

    refactor(shells): synced update-icon between bash and zsh
    only difference is starting array index and how to handle ~ vs '~'

--- bash/funcs/update.sh
+++ bash/funcs/update.sh
@@ -9,7 +9,10 @@ update_feature_branches() {
 
   for branch in "${branches[@]}"; do
     # Check if branch exists
-    git branch --format='%(refname:short)' | grep -Fxq "$branch" || { printf "${RED}  '%s' not found${RESET} - run ${GREEN}'git branch'${RESET} to show available branches\n" "$branch"; continue; }
+    git branch --format='%(refname:short)' | grep -Fxq "$branch" || {
+      printf "${RED}  '%s' not found${RESET} - run ${GREEN}'git branch'${RESET} to show available branches\n" "$branch"
+      continue
+    }
 
     # Perfom merge
     git switch "$branch" &>/dev/null
@@ -24,77 +27,77 @@ update_feature_branches() {
 }
 
 update_icons() {
-  # Create directory-icons association
-  command -v fileicon &>/dev/null || { printf "${BRIGHTRED}%s %s${BRIGHTYELLOW}%s${RESET}\n" "" "'fileicon' dependency not found" " - please install or check in \$PATH."; return 1; }
+  # Check for 'fileicon' binary
+  command -v fileicon &>/dev/null || {
+    printf "${BRIGHTRED}%s %s${BRIGHTYELLOW}%s${RESET}\n" "" "'fileicon' dependency not found" " - please install or check in \$PATH."
+    return 1
+  }
 
-  local table_width=60
-  declare -A dir_icons=(
-    [/Applications/1Password.app]=~/Pictures/icons/1password-macos.png
-    [/Applications/ChatGPT.app]=~/Pictures/icons/chatgpt.png
-    [/Applications/Cursor.app]=~/Pictures/icons/cursor-ai-macos.png
-    [/Applications/Discord.app]=~/Pictures/icons/discord-macos.png
-    [/Applications/Docker.app]=~/Pictures/icons/docker.png
-    [/Applications/Figma.app]=~/Pictures/icons/figma.png
-    [/Applications/Firefox.app]=~/Pictures/icons/firefox-macos.png
-    [/Applications/Google\ Chrome.app]=~/Pictures/icons/chrome.png
-    [/Applications/iTerm.app]=~/Pictures/icons/iterm2.png
-    [/Applications/iTermAI.app]=~/Pictures/icons/ai-brain.png
-    [/Applications/KeyCastr.app]=~/Pictures/icons/keycastr.png
-    [/Applications/Mimestream.app]=~/Pictures/icons/gmail.png
-    [/Applications/Neovim-iTerm.app]=~/Pictures/icons/neovim-iterm.png
-    [/Applications/Neovim-Wezterm.app]=~/Pictures/icons/neovim-wezterm.png
-    [/Applications/Postman.app]=~/Pictures/icons/postman.png
-    [/Applications/Slack.app]=~/Pictures/icons/slack.png
-    [/Applications/Spotify.app]=~/Pictures/icons/spotify.png
-    [/Applications/Visual\ Studio\ Code.app]=~/Pictures/icons/vscode.png
-    [/Applications/WezTerm.app]=~/Pictures/icons/wezterm.png
-    [/Applications/WhatsApp.app]=~/Pictures/icons/whatsapp.png
-    [~/Developer]=~/Pictures/icons/developer.png
-    [~/Downloads]=~/Pictures/icons/download.png
-    [~/Dropbox/content]=~/Pictures/icons/content.png
-    [~/Pictures/icons]=~/Pictures/icons/png.png
-    [~/Pictures/screenshots]=~/Pictures/icons/screenshot.png
-    [~/Pictures/wallpapers]=~/Pictures/icons/wallpaper.png
-    [~/.dotfiles]=~/Pictures/icons/gear.png
+  table_width=60
+
+  # Directory - Icons
+  dir_icons=(
+    "/Applications/1Password.app" "$HOME/Pictures/icons/1password-macos.png"
+    "/Applications/ChatGPT.app" "$HOME/Pictures/icons/chatgpt.png"
+    "/Applications/Cursor.app" "$HOME/Pictures/icons/cursor-ai-macos.png"
+    "/Applications/Discord.app" "$HOME/Pictures/icons/discord-macos.png"
+    "/Applications/Docker.app" "$HOME/Pictures/icons/docker.png"
+    "/Applications/Figma.app" "$HOME/Pictures/icons/figma.png"
+    "/Applications/Firefox.app" "$HOME/Pictures/icons/firefox-macos.png"
+    "/Applications/Google Chrome.app" "$HOME/Pictures/icons/chrome.png"
+    "/Applications/iTerm.app" "$HOME/Pictures/icons/iterm2.png"
+    "/Applications/iTermAI.app" "$HOME/Pictures/icons/ai-brain.png"
+    "/Applications/KeyCastr.app" "$HOME/Pictures/icons/keycastr.png"
+    "/Applications/Mimestream.app" "$HOME/Pictures/icons/gmail.png"
+    "/Applications/Neovim-iTerm.app" "$HOME/Pictures/icons/neovim-iterm.png"
+    "/Applications/Neovim-Wezterm.app" "$HOME/Pictures/icons/neovim-wezterm.png"
+    "/Applications/Postman.app" "$HOME/Pictures/icons/postman.png"
+    "/Applications/Slack.app" "$HOME/Pictures/icons/slack.png"
+    "/Applications/Spotify.app" "$HOME/Pictures/icons/spotify.png"
+    "/Applications/Visual Studio Code.app" "$HOME/Pictures/icons/vscode.png"
+    "/Applications/WezTerm.app" "$HOME/Pictures/icons/wezterm.png"
+    "/Applications/WhatsApp.app" "$HOME/Pictures/icons/whatsapp.png"
+    "$HOME/Developer" "$HOME/Pictures/icons/developer.png"
+    "$HOME/Downloads" "$HOME/Pictures/icons/download.png"
+    "$HOME/Dropbox/content" "$HOME/Pictures/icons/content.png"
+    "$HOME/Pictures/icons" "$HOME/Pictures/icons/png.png"
+    "$HOME/Pictures/screenshots" "$HOME/Pictures/icons/screenshot.png"
+    "$HOME/Pictures/wallpapers" "$HOME/Pictures/icons/wallpaper.png"
+    "$HOME/.dotfiles" "$HOME/Pictures/icons/gear.png"
   )
 
-  # Create Directory-Icons Table Header
-  print_separator() { printf "${BRIGHTBLACK}%-${table_width}s${RESET}\n" | tr ' ' '-'; }
+  print_separator() {
+    printf "${BRIGHTBLACK}%-${table_width}s${RESET}\n" | tr ' ' '-'
+  }
 
+  # Create header
   print_separator
   printf "%-2s ${BLUE}%-37s ${MAGENTA}%-5s${RESET}\n" '' 'App / Directory' \~/Pictures/icons/
   print_separator
 
-  # Sort keys while preserving spaces in the directories
-  mapfile -t sorted_dirs < <(printf '%s\n' "${!dir_icons[@]}" | sort)
+  # Create rows
+  for ((i = 0; i < ${#dir_icons[@]}; i += 2)); do
+    dir="${dir_icons[i]}"
+    icon="${dir_icons[i + 1]}"
+    short_dir="${dir/#$HOME/'~'}"
 
-  local dir_count="${#sorted_dirs[@]}"
-  local success_count=$dir_count
+    print_status() {
+      printf "${1}%-1s ${WHITE}%-37s ${BRIGHTMAGENTA}%-5s${RESET}\n" "$2" "$short_dir" "$(basename "$icon")"
+    }
 
-  # Create Directory-Icons Table Rows
-  local icon directory
-
-  for dir in "${sorted_dirs[@]}"; do
-    icon=${dir_icons[$dir]}
-    directory=${dir/#$HOME/\~}
-
-    # Function to print the status of setting file icons
-    print_status() { printf "${1}%-1s ${WHITE}%-37s ${BRIGHTMAGENTA}%-5s${RESET}\n" "$2" "$directory" "$(basename "$icon")"; }
-
-    if sudo fileicon set "$dir" "$icon" &>/dev/null || [[ $(basename "$directory") == 'Mimestream.app' ]]; then
+    if sudo fileicon set "$dir" "$icon" &>/dev/null || [[ $(basename "$dir") == 'Mimestream.app' ]]; then
       print_status "${GREEN}" " "
+      ((success++))
     else
       print_status "${RED}" " "
-      ((success_count--))
+      ((fail++))
     fi
   done
 
-  # Footer
-  local fail_count=$(( dir_count - success_count ))
-
+  # Create Footer
   print_separator
-  printf "${GREEN}%-2s ${BRIGHTBLACK}%-37s ${RESET}\n" "$success_count" 'Folder icon(s) updated successfully'
-  [ $fail_count -eq 0 ] || printf "${RED}%-2s ${BRIGHTBLACK}%-37s ${RESET}\n" "$fail_count" 'Folder icon(s) failed to update'
+  printf "${GREEN}%-2s ${BRIGHTBLACK}%-37s${RESET}\n" "${success:-0}" 'Folder icon(s) updated successfully'
+  [[ -z $fail ]] || printf "${RED}%-2s ${BRIGHTBLACK}%-37s${RESET}\n" "$fail" 'Folder icon(s) failed to update'
 }
 
 update_all() {
