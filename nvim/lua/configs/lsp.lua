61ce36287b1d6fc51cedbf6f745d114d460b6f88
Vivek Jha <vivek.x.jha@gmail.com>
2025-04-01 16:33:38 -0700

    feat(nvim): uninstalled nvim-lspconfig
    moved keymaps into configs/lsp

--- nvim/lua/configs/lsp.lua
+++ nvim/lua/configs/lsp.lua
@@ -1,7 +1,29 @@
 local icn = require 'ui.icons'
 local servers = require('configs.utils').filenames(vim.fn.stdpath 'config' .. '/lsp')
 
--- configure diagnostics
+-- on_attach to set LSP keymaps
+local on_attach = function(bufnr)
+  local map = function(mode, lhs, rhs, desc) vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc }) end
+
+  -- LSP functionality
+  map('n', 'gd', vim.lsp.buf.definition, 'LSP: Go to Definition')
+  map('n', 'gD', vim.lsp.buf.declaration, 'LSP: Go to Declaration')
+  map('n', 'gi', vim.lsp.buf.implementation, 'LSP: Go to Implementation')
+  map('n', 'gr', vim.lsp.buf.references, 'LSP: Go to References')
+  map('n', 'K', vim.lsp.buf.hover, 'LSP: Hover Documentation')
+  map('n', '<C-k>', vim.lsp.buf.signature_help, 'LSP: Signature Help')
+  map('n', '<leader>rn', vim.lsp.buf.rename, 'LSP: Rename Symbol')
+  map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, 'LSP: Code Action')
+  map('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, 'LSP: Format Document')
+
+  -- Diagnostics
+  map('n', '[d', vim.diagnostic.goto_prev, 'Diagnostic: Previous')
+  map('n', ']d', vim.diagnostic.goto_next, 'Diagnostic: Next')
+  map('n', '<leader>e', vim.diagnostic.open_float, 'Diagnostic: Open Float')
+  map('n', '<leader>q', vim.diagnostic.setloclist, 'Diagnostic: Set Loclist')
+end
+
+-- Configure diagnostics
 vim.diagnostic.config {
   float = { border = 'single' },
   severity_sort = true,
@@ -25,6 +47,7 @@ vim.diagnostic.config {
   },
 }
 
+-- Initialize language servers
 for _, server in ipairs(servers) do
   vim.lsp.enable(server)
 end
