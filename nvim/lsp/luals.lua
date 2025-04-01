1ebd4986dd60a7fe0d344587f28f591b410038f1
Vivek Jha <vivek.x.jha@gmail.com>
2025-04-01 12:43:11 -0700

    fix(nvim): added nvim lua subfolder as part of lua workspace library path for type checking
    also corrected hammerspoon path

--- nvim/lsp/luals.lua
+++ nvim/lsp/luals.lua
@@ -16,11 +16,12 @@ return {
 
       workspace = {
         library = {
+          vim.fn.stdpath 'config' .. '/lua',
           vim.fn.expand '$VIMRUNTIME/lua',
           vim.fn.expand '$VIMRUNTIME/lua/vim/lsp',
           vim.fn.stdpath 'data' .. '/lazy/lazy.nvim/lua/lazy',
           '${3rd}/luv/library',
-          vim.fn.expand(vim.fn.stdpath 'config' .. '/hammerspoon/Spoons/EmmyLua.spoon/annotations'),
+          os.getenv 'XDG_CONFIG_HOME' .. '/hammerspoon/configs',
         },
         maxPreload = 100000,
         preloadFileSize = 10000,
