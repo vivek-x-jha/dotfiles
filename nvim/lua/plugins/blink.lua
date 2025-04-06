9b33e6442811a3d096b3855aad5804baff3bc91a
Vivek Jha <vivek.x.jha@gmail.com>
2025-04-05 22:34:02 -0700

    refactor(nvim): moved luansip as a dependency for blink
    cleaned up luasnip config and moved InsertLeave autocommand to autocmds.lua

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -6,32 +6,11 @@ return {
     'L3MON4D3/LuaSnip',
     version = 'v2.*',
     opts = function()
-      local options = { history = true, updateevents = 'TextChanged,TextChangedI' }
-      local luasnip = require 'luasnip'
+      for _, style in ipairs { 'from_vscode', 'from_snipmate', 'from_lua' } do
+        require('luasnip.loaders.' .. style).lazy_load()
+      end
 
-      luasnip.config.set_config(options)
-
-      -- vscode format
-      require('luasnip.loaders.from_vscode').lazy_load { exclude = vim.g.vscode_snippets_exclude or {} }
-      require('luasnip.loaders.from_vscode').lazy_load { paths = vim.g.vscode_snippets_path or '' }
-
-      -- snipmate format
-      require('luasnip.loaders.from_snipmate').load()
-      require('luasnip.loaders.from_snipmate').lazy_load { paths = vim.g.snipmate_snippets_path or '' }
-
-      -- lua format
-      require('luasnip.loaders.from_lua').load()
-      require('luasnip.loaders.from_lua').lazy_load { paths = vim.g.lua_snippets_path or '' }
-
-      vim.api.nvim_create_autocmd('InsertLeave', {
-        desc = 'Reset Snippet',
-        group = vim.api.nvim_create_augroup('LuaSnipAU', {}),
-        callback = function()
-          if luasnip.session.current_nodes[vim.api.nvim_get_current_buf()] and not luasnip.session.jump_active then luasnip.unlink_current() end
-        end,
-      })
-
-      return options
+      return { history = true, updateevents = 'TextChanged,TextChangedI' }
     end,
   },
 
