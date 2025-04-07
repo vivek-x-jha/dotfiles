80bc31d7a00bb7170956bd1aa1439c79311dfcc0
Vivek Jha <vivek.x.jha@gmail.com>
2025-04-06 18:47:10 -0700

    style(nvim): removed empty lines between fields
    blink and conform configs

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -15,29 +15,12 @@ return {
   },
 
   opts = {
-    keymap = {
-      preset = 'default',
-    },
-
-    appearance = {
-      nerd_font_variant = 'mono',
-    },
-
-    completion = {
-      documentation = { auto_show = false },
-    },
-
-    snippets = {
-      preset = 'luasnip',
-    },
-
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-
-    fuzzy = {
-      implementation = 'prefer_rust_with_warning',
-    },
+    keymap = { preset = 'default' },
+    appearance = { nerd_font_variant = 'mono' },
+    completion = { documentation = { auto_show = false } },
+    snippets = { preset = 'luasnip' },
+    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
   },
 
   opts_extend = {
