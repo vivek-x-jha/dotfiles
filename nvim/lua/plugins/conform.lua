6eb314588608f4f69f270ad45526bf0b24afbd3a
Vivek Jha <vivek.x.jha@gmail.com>
2025-04-06 18:47:10 -0700

    style(nvim): removed empty lines between fields
    blink and conform configs

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,16 +3,13 @@ return {
   'stevearc/conform.nvim',
   event = 'BufWritePre',
   opts = {
+    format_on_save = { timeout_ms = 500, lsp_format = 'fallback' },
     formatters_by_ft = {
       lua = { 'stylua' },
-      bash = { 'shfmt' },
-      sh = { 'shfmt' },
-      zsh = { 'shfmt' },
       python = { 'ruff' },
-    },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = 'fallback',
+      bash = { 'shfmt' },
+      zsh = { 'shfmt' },
+      sh = { 'shfmt' },
     },
   },
 }
