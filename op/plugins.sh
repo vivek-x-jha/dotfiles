9670f195fb93070c91eb16971ca828043a0112d8
Vivek Jha <vivek.x.jha@gmail.com>
2025-04-14 22:00:23 -0700

    feat(mysql): added authentication via 1password shell plugin
    TODO fix login: user is root instead of mubuntu and passwod mangling prevents login

--- op/plugins.sh
+++ op/plugins.sh
@@ -1,4 +1,5 @@
-# https://developer.1password.com/docs/cli/
+# https://developer.1password.com/docs/cli/shell-plugins
 
 export OP_PLUGIN_ALIASES_SOURCED=1
 alias gh="op plugin run -- gh"
+alias mysql="op plugin run -- mysql"
