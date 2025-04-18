24f45bdcf021ddab0457ecfe1ea14a19f4b6fe19
Vivek Jha <vivek.x.jha@gmail.com>
2025-04-17 17:49:08 -0700

    refactor(nvim): moved options directly into init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,10 @@
+-- Establish leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
---- @type string Set path to lazy repo
+-- Bootstrap lazy if not installed
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
--- Bootstrap lazy if not installed
 if not vim.uv.fs_stat(lazypath) then
   local out = vim.fn.system {
     'git',
@@ -30,11 +30,56 @@ end
 -- Prepend lazy to rtp
 vim.opt.rtp:prepend(lazypath)
 
--- Load options
-require 'configs.options'
+-- Prepend mason to PATH
+local is_windows = vim.fn.has 'win32' ~= 0
+local sep = is_windows and '\\' or '/'
+local delim = is_windows and ';' or ':'
+local masonbin = { vim.fn.stdpath 'data', 'mason', 'bin' }
+
+vim.env.PATH = table.concat(masonbin, sep) .. delim .. vim.env.PATH
+
+-- Disable providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
+-- Set scoped opts
+vim.opt.fillchars = { eob = ' ' } -- remove trailing ~ on buffers
+vim.opt.guicursor = 'n-v-c:block-blinkwait300-blinkon200-blinkoff150,i-ci:ver25-blinkwait300-blinkon200-blinkoff150' -- enable blinking cursor
+vim.opt.shortmess:append 'sI' -- disable nvim intro
+vim.opt.whichwrap:append '<>[]hl' -- go to previous/next line with h,l,left arrow and right arrow when cursor reaches EOL or BOL
+
+-- Set global opts
+vim.o.number = true -- print line number in front of each line
+vim.o.relativenumber = true -- show relative line numbers
+vim.o.numberwidth = 2 -- minimal number of columns for line number
+vim.o.ruler = false -- hide the line/column position
+
+vim.o.expandtab = true -- use spaces instead of tabs
+vim.o.shiftwidth = 2 -- spaces per indent
+vim.o.smartindent = true -- autoindent smartly
+vim.o.tabstop = 2 -- spaces per tab
+vim.o.softtabstop = 2 -- editing width of a tab
+
+vim.o.ignorecase = true -- ignore case in search
+vim.o.smartcase = true -- override ignorecase if uppercase in search
+
+vim.o.clipboard = 'unnamedplus' -- use system clipboard
+vim.o.cursorline = true -- highlight the current line
+vim.o.laststatus = 3 -- global statusline
+vim.o.mouse = 'a' -- enable mouse support
+vim.o.showmode = false -- don't show --INSERT--
+vim.o.signcolumn = 'yes' -- always show sign column
+vim.o.splitbelow = true -- new horizontal splits below
+vim.o.splitright = true -- new vertical splits to the right
+vim.o.timeoutlen = 400 -- keymap timeout
+vim.o.undofile = true -- persistent undo
+vim.o.updatetime = 250 -- swap write & CursorHold delay
 
 -- Load plugins
-require('lazy').setup(require 'configs.lazy')
+local lazyopts = require 'configs.lazy'
+require('lazy').setup(lazyopts)
 
 -- Load LSP
 require('configs.lsp').setup { linters = { 'shellcheck' } }
@@ -42,14 +87,14 @@ require('configs.lsp').setup { linters = { 'shellcheck' } }
 -- Load buffer manager
 require('ui.buffers').setup()
 
--- Load highlights and colorscheme
+-- Load colors
 require('configs.highlights').setup()
 
 -- Load statusline
 vim.o.statusline = "%!v:lua.require('ui.statusline').setup()"
 
--- Load & schedule auto commands
+-- Load event triggers (some deferred)
 require 'configs.autocmds'
 
--- Schedule keymaps
+-- Load keymaps (deferred)
 vim.schedule(function() require 'configs.keymaps' end)
