af0c4ccfe4fcecb9b47bad3dd72e25d42bd86204
Vivek Jha <vivek.x.jha@gmail.com>
2025-04-20 15:36:51 -0700

    refactor(tmux): reogranzied tmux plugins so they are grouped by plugin and added repo links as docs

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -1,6 +1,15 @@
--- https://github.com/christoomey/vim-tmux-navigator
+-- # https://github.com/christoomey/vim-tmux-navigator?tab=readme-ov-file#vim-tmux-navigator
 return {
   'christoomey/vim-tmux-navigator',
+  cmd = {
+    'TmuxNavigateLeft',
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
+    'TmuxNavigatePrevious',
+    'TmuxNavigatorProcessList',
+  },
+
   keys = {
     { '<c-h>', '<cmd>TmuxNavigateLeft<CR>', { desc = 'switch window left' } },
     { '<c-j>', '<cmd>TmuxNavigateDown<CR>', { desc = 'switch window down' } },
