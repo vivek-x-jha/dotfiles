7f0c3f591b2a8a017e3d218cf353db6e49aab047
Vivek Jha <vivek.x.jha@gmail.com>
2025-09-08 23:36:48 -0700

    refactor(nvim): added function fzf and variable stats to clean buttons

--- nvim/lua/ui/dashboard.lua
+++ nvim/lua/ui/dashboard.lua
@@ -1,9 +1,18 @@
+local icn = require 'ui.icons'
 local M = {}
 local utl = {}
-local api = vim.api
-local g = vim.g
 
-local icn = require 'ui.icons'
+---Format plugin statistics from lazy.nvim
+---@return string -- "  Loaded 20/30 plugins in 50ms"
+local plugin_stats = function()
+  local stats = require('lazy').stats()
+  return table.concat { '  Loaded ', stats.loaded, '/', stats.count, ' plugins in ', math.floor(stats.startuptime), 'ms' }
+end
+
+---Create an fzf-lua command string
+---@param cmd string  -- the fzf-lua method name (e.g., "files", "buffers")
+---@return string -- a Lua command string to run
+local fzf = function(cmd) return table.concat { 'lua require("fzf-lua").', cmd, '()' } end
 
 local header = {
   '                                                       ',
@@ -20,29 +29,20 @@ local header = {
 
 local buttons = {
   { txt = '─', hl = 'DashLine', no_gap = true, rep = true },
-  {
-    txt = function()
-      local stats = require('lazy').stats()
-      local ms = math.floor(stats.startuptime) .. ' ms'
-      return '  Loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms
-    end,
-    hl = 'DashPlugins',
-    no_gap = true,
-  },
-
+  { txt = plugin_stats(), hl = 'DashPlugins', no_gap = true },
   { txt = '─', hl = 'DashLine', no_gap = true, rep = true },
-  { txt = icn.search .. '  Files', hl = 'DashFiles', keys = 'ff', cmd = 'lua require("fzf-lua").files()' },
-  { txt = icn.recent .. '  Recent Files', hl = 'DashRecent', keys = 'fo', cmd = 'lua require("fzf-lua").oldfiles()' },
-  { txt = icn.warn .. '  Event Triggers', hl = 'DashTrigger', keys = 'fa', cmd = 'lua require("fzf-lua").autocmds()' },
+  { txt = icn.search .. '  Files', hl = 'DashFiles', keys = 'ff', cmd = fzf 'files' },
+  { txt = icn.recent .. '  Recent Files', hl = 'DashRecent', keys = 'fo', cmd = fzf 'oldfiles' },
+  { txt = icn.warn .. '  Event Triggers', hl = 'DashTrigger', keys = 'fa', cmd = fzf 'autocmds' },
   { txt = icn.gear .. '  Settings', hl = 'DashSettings', keys = 's', cmd = table.concat { 'edit ', vim.fn.stdpath 'config', '/init.lua' } },
-  { txt = icn.fuzzy .. '  Search Word', hl = 'DashWord', keys = 'fw', cmd = 'lua require("fzf-lua").live_grep()' },
-  { txt = icn.cmd_hist .. '  Command History', hl = 'DashCmdHist', keys = 'fc', cmd = 'lua require("fzf-lua").command_history()' },
-  { txt = icn.file .. '  Buffers', hl = 'DashBuffers', keys = 'fb', cmd = 'lua require("fzf-lua").buffers()' },
-  { txt = icn.vim .. '  Neovim Commands', hl = 'DashNvimCmds', keys = 'fn', cmd = 'lua require("fzf-lua").commands()' },
-  { txt = icn.git .. '  Git Files', hl = 'DashGitFiles', keys = 'fg', cmd = 'lua require("fzf-lua").git_files()' },
-  { txt = icn.log .. '  Git Log', hl = 'DashGitLog', keys = 'glg', cmd = 'lua require("fzf-lua").git_commits()' },
-  { txt = icn.status .. '  Git Status', hl = 'DashGitStatus', keys = 'gst', cmd = 'lua require("fzf-lua").git_status()' },
-  { txt = icn.branch .. '  Git Switch', hl = 'DashGitSwitch', keys = 'gsw', cmd = 'lua require("fzf-lua").git_branches()' },
+  { txt = icn.fuzzy .. '  Search Word', hl = 'DashWord', keys = 'fw', cmd = fzf 'live_grep' },
+  { txt = icn.cmd_hist .. '  Command History', hl = 'DashCmdHist', keys = 'fc', cmd = fzf 'command_history' },
+  { txt = icn.file .. '  Buffers', hl = 'DashBuffers', keys = 'fb', cmd = fzf 'buffers' },
+  { txt = icn.vim .. '  Neovim Commands', hl = 'DashNvimCmds', keys = 'fn', cmd = fzf 'commands' },
+  { txt = icn.git .. '  Git Files', hl = 'DashGitFiles', keys = 'fg', cmd = fzf 'git_files' },
+  { txt = icn.log .. '  Git Log', hl = 'DashGitLog', keys = 'glg', cmd = fzf 'git_commits' },
+  { txt = icn.status .. '  Git Status', hl = 'DashGitStatus', keys = 'gst', cmd = fzf 'git_status' },
+  { txt = icn.branch .. '  Git Switch', hl = 'DashGitSwitch', keys = 'gsw', cmd = fzf 'git_branches' },
 }
 
 utl.remap = function(keys, action, buf)
@@ -52,38 +52,38 @@ utl.remap = function(keys, action, buf)
 end
 
 utl.txt_pad = function(str, max_str_w)
-  local av = math.floor((max_str_w - api.nvim_strwidth(str)) / 2)
+  local av = math.floor((max_str_w - vim.api.nvim_strwidth(str)) / 2)
   local spacing = string.rep(' ', av)
 
   return spacing .. str .. spacing
 end
 
 utl.btn_gap = function(txt1, txt2, max_str_w)
-  local nonbuttonlength = max_str_w - api.nvim_strwidth(txt1) - #txt2
+  local nonbuttonlength = max_str_w - vim.api.nvim_strwidth(txt1) - #txt2
   local spacing = string.rep(' ', nonbuttonlength)
 
   return txt1 .. spacing .. txt2
 end
 
 M.setup = function(buf, win, action)
-  buf = buf or api.nvim_create_buf(false, true)
-  win = win or api.nvim_get_current_win()
+  buf = buf or vim.api.nvim_create_buf(false, true)
+  win = win or vim.api.nvim_get_current_win()
   action = action or 'open'
 
-  local ns = api.nvim_create_namespace 'dashboard'
-  local winh = api.nvim_win_get_height(win)
-  local winw = api.nvim_win_get_width(win)
+  local ns = vim.api.nvim_create_namespace 'dashboard'
+  local winh = vim.api.nvim_win_get_height(win)
+  local winw = vim.api.nvim_win_get_width(win)
 
-  g.dashboard_buf = buf
-  g.dashboard_win = win
+  vim.g.dashboard_buf = buf
+  vim.g.dashboard_win = win
 
   local dashboard_w = 0
 
-  if action == 'open' then api.nvim_win_set_buf(0, buf) end
+  if action == 'open' then vim.api.nvim_win_set_buf(0, buf) end
 
   ------------------------ find largest string's width -----------------------------
   for _, val in ipairs(header) do
-    local headerw = api.nvim_strwidth(val)
+    local headerw = vim.api.nvim_strwidth(val)
     if headerw > dashboard_w then dashboard_w = headerw end
   end
 
@@ -95,7 +95,7 @@ M.setup = function(buf, win, action)
   for _, val in ipairs(buttons) do
     local str = type(val.txt) == 'string' and val.txt or val.txt()
     str = val.keys and str .. val.keys or str
-    local w = api.nvim_strwidth(str)
+    local w = vim.api.nvim_strwidth(str)
 
     if dashboard_w < w then dashboard_w = w end
 
@@ -137,13 +137,13 @@ M.setup = function(buf, win, action)
   end
 
   -- set text + highlight
-  api.nvim_buf_set_lines(buf, 0, -1, false, empty_str)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, empty_str)
   local key_lines = {}
 
   for i, item in ipairs(dashboard) do
     item.txt = '  ' .. item.txt .. '  '
     local opt = { virt_text_win_col = col_i, virt_text = { { item.txt, item.hl } } }
-    api.nvim_buf_set_extmark(buf, ns, row_i + i, 0, opt)
+    vim.api.nvim_buf_set_extmark(buf, ns, row_i + i, 0, opt)
 
     if item.cmd then table.insert(key_lines, { i = row_i + i + 1, cmd = item.cmd }) end
   end
@@ -151,18 +151,18 @@ M.setup = function(buf, win, action)
   ------------------------------------ keybinds ------------------------------------------
   vim.wo[win].virtualedit = 'all'
   local btn_start_i = row_i + #header + 2
-  api.nvim_win_set_cursor(win, { btn_start_i, col_i + 5 })
+  vim.api.nvim_win_set_cursor(win, { btn_start_i, col_i + 5 })
 
   utl.remap({ 'k', '<up>' }, function()
     local cur = vim.fn.line '.'
     local target_line = cur == key_lines[1].i and key_lines[#key_lines].i or cur - 2
-    api.nvim_win_set_cursor(win, { target_line, col_i + 5 })
+    vim.api.nvim_win_set_cursor(win, { target_line, col_i + 5 })
   end, buf)
 
   utl.remap({ 'j', '<down>' }, function()
     local cur = vim.fn.line '.'
     local target_line = cur == key_lines[#key_lines].i and key_lines[1].i or cur + 2
-    api.nvim_win_set_cursor(win, { target_line, col_i + 5 })
+    vim.api.nvim_win_set_cursor(win, { target_line, col_i + 5 })
   end, buf)
 
   utl.remap({ '<cr>' }, function()
@@ -187,30 +187,30 @@ M.setup = function(buf, win, action)
   }
 
   for _, optl in ipairs(opt_local) do
-    api.nvim_set_option_value(optl[1], optl[2], optl[3])
+    vim.api.nvim_set_option_value(optl[1], optl[2], optl[3])
   end
 
-  g['dashboard_displayed'] = true
+  vim.g['dashboard_displayed'] = true
 
   if action == 'redraw' then return end
 
   ----------------------- autocmds -----------------------------
-  local group_id = api.nvim_create_augroup('NvdashAu', { clear = true })
+  local group_id = vim.api.nvim_create_augroup('NvdashAu', { clear = true })
 
-  api.nvim_create_autocmd('BufWinLeave', {
+  vim.api.nvim_create_autocmd('BufWinLeave', {
     group = group_id,
     buffer = buf,
     callback = function()
-      g.dashboard_displayed = false
-      api.nvim_del_augroup_by_name 'NvdashAu'
+      vim.g.dashboard_displayed = false
+      vim.api.nvim_del_augroup_by_name 'NvdashAu'
     end,
   })
 
-  api.nvim_create_autocmd({ 'WinResized', 'VimResized' }, {
+  vim.api.nvim_create_autocmd({ 'WinResized', 'VimResized' }, {
     group = group_id,
     callback = function()
-      vim.bo[g.dashboard_buf].ma = true
-      require('ui.dashboard').setup(g.dashboard_buf, g.dashboard_win, 'redraw')
+      vim.bo[vim.g.dashboard_buf].ma = true
+      require('ui.dashboard').setup(vim.g.dashboard_buf, vim.g.dashboard_win, 'redraw')
     end,
   })
 end
