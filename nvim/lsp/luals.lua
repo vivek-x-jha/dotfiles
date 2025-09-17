5d112b8401ad24160f44a090471b24b1740de987
Vivek Jha <vivek.x.jha@gmail.com>
2025-09-16 23:56:34 -0700

    feat(lua-configs): unified and updated .luarc.json files for nvim + hammerspoon + weztem

--- nvim/lsp/luals.lua
+++ nvim/lsp/luals.lua
@@ -1,16 +1,5 @@
 local lua_paths = vim.api.nvim_get_runtime_file('', true)
-local xdg_config = os.getenv 'XDG_CONFIG_HOME' or vim.fs.joinpath(os.getenv 'HOME', '.config')
-
--- Always include Neovim config's lua/
 table.insert(lua_paths, vim.fs.joinpath(vim.fn.stdpath 'config', 'lua'))
-
--- Hammerspoon configs (if present)
-local hs_config = vim.fs.joinpath(xdg_config, 'hammerspoon', 'lua')
-if vim.uv.fs_stat(hs_config) then table.insert(lua_paths, hs_config) end
-
--- WezTerm config folder (if present)
-local wez_config = vim.fs.joinpath(xdg_config, 'wezterm')
-if vim.uv.fs_stat(wez_config) then table.insert(lua_paths, wez_config) end
 
 return {
   cmd = { 'lua-language-server' },
@@ -19,15 +8,9 @@ return {
 
   settings = {
     Lua = {
-      runtime = { version = 'LuaJIT' },
       workspace = {
         library = lua_paths,
-        checkThirdParty = false,
-        maxPreload = 2000,
-        preloadFileSize = 150,
       },
-      format = { enable = false },
-      telemetry = { enable = false },
     },
   },
 }
