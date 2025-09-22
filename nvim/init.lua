bd7c9c081e814ce00528755fcf263ef2ba419947
Vivek Jha <vivek.x.jha@gmail.com>
2025-09-22 10:00:59 -0700

    feat(nvim): removed unused conform format hotkey

--- nvim/init.lua
+++ nvim/init.lua
@@ -162,8 +162,6 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   callback = function(args) require('conform').format { bufnr = args.buf } end,
 })
 
-vim.keymap.set('n', '<leader>fm', function() require('conform').format { lsp_fallback = true } end, { desc = '[f]or[m]at file with linter' })
-
 -- Multi Modal Picker
 require('fzf-lua').setup {
   winopts = {
