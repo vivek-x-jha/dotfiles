060ee93d584bfbb7bfe8d197463e8d4d29d3bf53
Vivek Jha <vivek.x.jha@gmail.com>
2025-09-24 13:49:20 -0700

    refactor(nvim): alpha sorted vim.pack.add() urls

--- nvim/init.lua
+++ nvim/init.lua
@@ -86,23 +86,23 @@ vim.lsp.enable(servers)
 ------------------------------------ [3/5] Plugins ------------------------------------
 
 vim.pack.add {
-  { src = 'https://github.com/windwp/nvim-autopairs' },
   { src = 'https://github.com/saghen/blink.cmp' },
-  { src = 'https://github.com/L3MON4D3/LuaSnip' }, -- blink snippets
-  { src = 'https://github.com/kylechui/nvim-surround' },
   { src = 'https://github.com/stevearc/conform.nvim' },
+  { src = 'https://github.com/ibhagwan/fzf-lua' },
+  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
   { src = 'https://github.com/lukas-reineke/indent-blankline.nvim' },
-  { src = 'https://github.com/nvim-pack/nvim-spectre' },
-  { src = 'https://github.com/nvim-lua/plenary.nvim' }, -- spectre dependency
+  { src = 'https://github.com/L3MON4D3/LuaSnip' }, -- blink snippets
   { src = 'https://github.com/folke/noice.nvim' },
   { src = 'https://github.com/MunifTanjim/nui.nvim' }, -- noice dependency
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' }, -- noice syntax parsing
-  { src = 'https://github.com/rcarriga/nvim-notify' },
+  { src = 'https://github.com/windwp/nvim-autopairs' },
   { src = 'https://github.com/brenoprata10/nvim-highlight-colors' },
+  { src = 'https://github.com/rcarriga/nvim-notify' },
+  { src = 'https://github.com/nvim-pack/nvim-spectre' },
+  { src = 'https://github.com/kylechui/nvim-surround' },
   { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' }, -- noice syntax parsing
   { src = 'https://github.com/nvim-tree/nvim-web-devicons' }, -- nvim-tree + statusline icons
-  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
-  { src = 'https://github.com/ibhagwan/fzf-lua' },
+  { src = 'https://github.com/nvim-lua/plenary.nvim' }, -- spectre dependency
   { src = 'https://github.com/christoomey/vim-tmux-navigator' },
 }
 
