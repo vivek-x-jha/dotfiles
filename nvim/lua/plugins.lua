8bc31e9cf6823b2e130091411b9ea795a7fda8ea
Vivek Jha <vivek.x.jha@gmail.com>
2025-10-04 16:00:08 -0700

    style(nvim): updated nested fields in setup funcs to be more readable and vertical

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,17 +1,17 @@
 --- @type table<string, string> -- Custom icons
 local icons = require 'icons'
 
--- Color Previews
+-- [1/15] Color Previews
 require('nvim-highlight-colors').setup { render = 'foreground' }
 
--- LuaSnip: loaders + basic setup (mirrors the Lazy `dependencies.opts`)
+-- [2/15] LuaSnip: loaders + basic setup (mirrors the Lazy `dependencies.opts`)
 require('luasnip.loaders.from_vscode').lazy_load()
 require('luasnip.loaders.from_snipmate').lazy_load()
 require('luasnip.loaders.from_lua').lazy_load()
 
 require('luasnip').setup { history = true, updateevents = 'TextChanged,TextChangedI' }
 
--- Autocompletion: https://cmp.saghen.dev/installation
+-- [3/15] Autocompletion: https://cmp.saghen.dev/installation
 require('blink.cmp').setup {
   keymap = { preset = 'default' },
   appearance = { nerd_font_variant = 'mono' },
@@ -51,10 +51,10 @@ require('blink.cmp').setup {
   },
 }
 
--- Autopair quotes, paranthesis, brackets, braces
+-- [4/15] Autopair quotes, paranthesis, brackets, braces
 require('nvim-autopairs').setup { fast_wrap = {}, disable_filetype = { 'vim' } }
 
--- Formatters
+-- [5/15] Formatters
 require('conform').setup {
   format_on_save = { timeout_ms = 500, lsp_format = 'fallback' },
 
@@ -67,34 +67,73 @@ require('conform').setup {
   },
 }
 
--- Fuzzy finder / picker
+-- [6/15] Fuzzy finder / picker
 require('fzf-lua').setup {
-  winopts = { preview = { winopts = { number = false, signcolumn = 'yes' } } },
-  files = { header = 'fd --type f', winopts = { title = table.concat { ' ', icons.search, ' files ' }, title_flags = false } },
-  autocmds = { header = ':autocmd', winopts = { title = table.concat { ' ', icons.warn, ' event triggers ' }, title_flags = false } },
-  buffers = { header = ':ls', winopts = { title = table.concat { ' ', icons.file, ' buffers ' }, title_flags = false } },
-  commands = { header = ':command', winopts = { title = table.concat { ' ', icons.vim, ' commands ' }, title_flags = false } },
-  command_history = { header = ':history', winopts = { title = table.concat { ' ', icons.cmd_hist, ' command history ' }, title_flags = false } },
-  grep = { header = ':!rg --vimgrep', winopts = { title = table.concat { ' ', icons.fuzzy, ' fuzzy search' }, title_flags = false } },
+  winopts = {
+    preview = { winopts = { number = false, signcolumn = 'yes' } },
+  },
+
+  files = {
+    header = 'fd --type f',
+    winopts = { title = ' ' .. icons.search .. ' files ', title_flags = false },
+  },
+
+  autocmds = {
+    header = ':autocmd',
+    winopts = { title = ' ' .. icons.event .. ' event triggers ', title_flags = false },
+  },
+
+  buffers = {
+    header = ':ls',
+    winopts = { title = ' ' .. icons.file .. ' buffers ', title_flags = false },
+  },
+
+  commands = {
+    header = ':command',
+    winopts = { title = ' ' .. icons.vim .. ' commands ', title_flags = false },
+  },
+
+  command_history = {
+    header = ':history',
+    winopts = { title = ' ' .. icons.cmd_hist .. ' command history ', title_flags = false },
+  },
+
+  grep = {
+    header = ':!rg --vimgrep',
+    winopts = { title = ' ' .. icons.fuzzy .. ' fuzzy search', title_flags = false },
+  },
 
   oldfiles = {
     header = ':FzfLua oldfiles',
     include_current_session = true,
     cwd_only = true,
-    winopts = { title = table.concat { ' ', icons.recent, ' recent files ' }, title_flags = false },
+    winopts = { title = ' ' .. icons.recent .. ' recent files ', title_flags = false },
   },
 
   git = {
-    files = { header = ':!git ls-files --exclude-standard', winopts = { title = table.concat { ' ', icons.git, ' ', icons.file, ' files (git) ' } } },
-    branches = { header = ':!git branch --all --color && git switch', winopts = { title = table.concat { ' ', icons.branch, ' branches ' } } },
-    commits = { header = ':!git log --color --pretty=format:"..."', winopts = { title = table.concat { ' ', icons.log, '  git commits ' } } },
+    files = {
+      header = ':!git ls-files --exclude-standard',
+      winopts = { title = ' ' .. icons.git .. ' ' .. icons.file .. ' files (git) ' },
+    },
+
+    branches = {
+      header = ':!git branch --all --color && git switch',
+      winopts = { title = ' ' .. icons.branch .. ' branches ' },
+    },
+
+    commits = {
+      header = ':!git log --color --pretty=format:"..."',
+      winopts = { title = ' ' .. icons.log .. '  git commits ' },
+    },
+
     status = {
       header = ':!git -c color.status=false --no-optional-locks status --porcelain=v1 -u',
-      winopts = { title = table.concat { ' ', icons.status, ' git status ' } },
+      winopts = { title = ' ' .. icons.status .. ' git status ' },
     },
   },
 }
 
+-- [7/15] Gutter Git info
 ---@type table<string, { text: string, show_count?: boolean }> -- Git sign column icons
 local gs_icons = {
   add = { text = '+' },
@@ -107,10 +146,10 @@ local gs_icons = {
 
 require('gitsigns').setup { signs = gs_icons, signs_staged = gs_icons }
 
--- Markers for indentation
+-- [8/15] Markers for indentation
 require('ibl').setup { indent = { char = '┊' } }
 
--- Syntax Highlighting
+-- [9/15] Syntax Highlighting
 require('nvim-treesitter.configs').setup {
   -- LuaLS type (TSConfig) expects these keys; set them explicitly:
   modules = {},
@@ -134,11 +173,11 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
--- UI for pop-ups
+-- [10/15] UI for pop-ups
 vim.notify = require 'notify'
 vim.notify.setup { background_colour = os.getenv 'WEZTERM_BG_HEX', fps = 60, stages = 'fade' }
 
--- Notification + Cmd Line UI Manager
+-- [11/15] Notification + Cmd Line UI Manager
 require('noice').setup {
   presets = { command_palette = true, long_message_to_split = true },
 
@@ -152,13 +191,13 @@ require('noice').setup {
   },
 }
 
--- Search & Replace
+-- [12/15] Search & Replace
 require('spectre').setup { result_padding = '', default = { replace = { cmd = 'sed' } } }
 
--- Auto bracket/paranthesis/quote wrapping
+-- [13/15] Auto bracket/paranthesis/quote wrapping
 require('nvim-surround').setup()
 
--- Additional Icon set
+-- [14/15] Additional Icon set
 require('nvim-web-devicons').setup {
   override = {
     default_icon = { icon = icons.file, name = 'Default' },
@@ -169,18 +208,28 @@ require('nvim-web-devicons').setup {
   },
 }
 
--- File Explorer
+-- [15/15] File Explorer
 require('nvim-tree').setup {
   hijack_cursor = true,
   disable_netrw = true,
   sync_root_with_cwd = true,
   view = { preserve_window_proportions = true },
   update_focused_file = { enable = true },
-  diagnostics = { enable = true, show_on_dirs = true, icons = { hint = 'H', info = 'I', warning = 'W', error = 'E' } },
   modified = { enable = true },
   filters = { git_ignored = false },
   live_filter = { prefix = icons.search .. ' ' },
   help = { sort_by = 'desc' },
+
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    icons = {
+      hint = 'H',
+      info = 'I',
+      warning = 'W',
+      error = 'E',
+    },
+  },
 
   renderer = {
     root_folder_label = false,
@@ -189,7 +238,10 @@ require('nvim-tree').setup {
     highlight_diagnostics = true,
     highlight_modified = 'icon',
 
-    indent_markers = { enable = true, icons = { edge = '┊', item = '┊' } },
+    indent_markers = {
+      enable = true,
+      icons = { edge = '┊', item = '┊' },
+    },
 
     icons = {
       modified_placement = 'signcolumn',
@@ -197,8 +249,24 @@ require('nvim-tree').setup {
 
       glyphs = {
         modified = icons.modified,
-        folder = { default = icons.folder, empty = '󱧹', empty_open = '󰷏', open = '󰷏', symlink = '󱉆' },
-        git = { renamed = '𝙍', staged = '+', unmerged = '!', unstaged = '~', untracked = '?', ignored = '', deleted = '✘' },
+
+        folder = {
+          default = icons.folder,
+          empty = '󱧹',
+          empty_open = '󰷏',
+          open = '󰷏',
+          symlink = '󱉆',
+        },
+
+        git = {
+          renamed = '𝙍',
+          staged = '+',
+          unmerged = '!',
+          unstaged = '~',
+          untracked = '?',
+          ignored = '',
+          deleted = '✘',
+        },
       },
     },
   },
