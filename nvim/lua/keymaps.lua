0b22a568a5721496f4d2719d8514dc8263dbcd58
Vivek Jha <vivek.x.jha@gmail.com>
2025-10-09 17:28:24 -0700

    refactor(nvim): added local vars to shorten code line lengths

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,93 +1,96 @@
+local keymap = vim.keymap.set
+
 -- Open picker
-vim.keymap.set('n', '<leader>ff', function() require('fzf-lua').files() end, { desc = '[F]ind [F]iles' })
-vim.keymap.set('n', '<leader>fo', function() require('fzf-lua').oldfiles() end, { desc = '[R]ecent [B]uffers' })
-vim.keymap.set('n', '<leader>fa', function() require('fzf-lua').autocmds() end, { desc = '[F]ind Neovim [A]uto-commands' })
-vim.keymap.set('n', '<leader>fw', function() require('fzf-lua').live_grep() end, { desc = '[F]ind [W]ord' })
-vim.keymap.set('n', '<leader>fc', function() require('fzf-lua').command_history() end, { desc = '[F]ind [C]ommands' })
-vim.keymap.set('n', '<leader>fb', function() require('fzf-lua').buffers() end, { desc = '[F]ind [B]uffers' })
-vim.keymap.set('n', '<leader>fn', function() require('fzf-lua').commands() end, { desc = '[F]ind Neovim [C]ommands' })
-vim.keymap.set('n', '<leader>fg', function() require('fzf-lua').git_files() end, { desc = '[F]ind [G]it Files' })
-vim.keymap.set('n', '<leader>glg', function() require('fzf-lua').git_commits() end, { desc = '[G]it [L]og Graph' })
-vim.keymap.set('n', '<leader>gst', function() require('fzf-lua').git_status() end, { desc = '[G]it [St]atus' })
-vim.keymap.set('n', '<leader>gsw', function() require('fzf-lua').git_branches() end, { desc = '[G]it [S]witch' })
+keymap('n', '<leader>ff', function() require('fzf-lua').files() end, { desc = '[F]ind [F]iles' })
+keymap('n', '<leader>fo', function() require('fzf-lua').oldfiles() end, { desc = '[R]ecent [B]uffers' })
+keymap('n', '<leader>fa', function() require('fzf-lua').autocmds() end, { desc = '[F]ind Neovim [A]uto-commands' })
+keymap('n', '<leader>fw', function() require('fzf-lua').live_grep() end, { desc = '[F]ind [W]ord' })
+keymap('n', '<leader>fc', function() require('fzf-lua').command_history() end, { desc = '[F]ind [C]ommands' })
+keymap('n', '<leader>fb', function() require('fzf-lua').buffers() end, { desc = '[F]ind [B]uffers' })
+keymap('n', '<leader>fn', function() require('fzf-lua').commands() end, { desc = '[F]ind Neovim [C]ommands' })
+keymap('n', '<leader>fg', function() require('fzf-lua').git_files() end, { desc = '[F]ind [G]it Files' })
+keymap('n', '<leader>glg', function() require('fzf-lua').git_commits() end, { desc = '[G]it [L]og Graph' })
+keymap('n', '<leader>gst', function() require('fzf-lua').git_status() end, { desc = '[G]it [St]atus' })
+keymap('n', '<leader>gsw', function() require('fzf-lua').git_branches() end, { desc = '[G]it [S]witch' })
+keymap('n', '<leader>un', function() require('pickers.notify').notify_history_picker() end, { desc = 'Notifications: history (fzf-lua)' })
 
 -- Toggle git blame
-vim.keymap.set('n', '<leader>gb', function() require('gitsigns').toggle_current_line_blame() end, { desc = 'Toggle [g]it [b]lame' })
+keymap('n', '<leader>gb', function() require('gitsigns').toggle_current_line_blame() end, { desc = 'Toggle [g]it [b]lame' })
 
 -- Toggle search and replace
-vim.keymap.set('n', '<leader>S', function() require('spectre').toggle() end, { desc = 'Toggle [S]pectre' })
-vim.keymap.set('n', '<leader>sw', function() require('spectre').open_visual { select_word = true } end, { desc = '[S]earch current [w]ord' })
-vim.keymap.set('v', '<leader>sw', function() require('spectre').open_visual() end, { desc = '[S]earch current [w]ord' })
+keymap('n', '<leader>S', function() require('spectre').toggle() end, { desc = 'Toggle [S]pectre' })
+keymap('n', '<leader>sw', function() require('spectre').open_visual { select_word = true } end, { desc = '[S]earch current [w]ord' })
+keymap('v', '<leader>sw', function() require('spectre').open_visual() end, { desc = '[S]earch current [w]ord' })
 
 -- Toggle File Explorer
-vim.keymap.set('n', '<C-n>', function() require('nvim-tree.api').tree.toggle { focus = false } end, { desc = 'Toggle file explorer' })
-vim.keymap.set('n', '<leader>e', function() require('nvim-tree.api').tree.open() end, { desc = 'Focus file [e]xplorer' })
+keymap('n', '<C-n>', function() require('nvim-tree.api').tree.toggle { focus = false } end, { desc = 'Toggle file explorer' })
+keymap('n', '<leader>e', function() require('nvim-tree.api').tree.open() end, { desc = 'Focus file [e]xplorer' })
 
 -- Dismiss notifications
-vim.keymap.set('n', '<leader>nd', function() require('noice').cmd 'dismiss' end, { desc = 'Clear notifications' })
+keymap('n', '<leader>nd', function() require('noice').cmd 'dismiss' end, { desc = 'Clear notifications' })
 
 ------------------------------------ Local Hotkeys ------------------------------------
 
 -- Open command line
-vim.keymap.set('n', ';', ':', { desc = 'Enter CMD mode w/o <Shift>' })
+keymap('n', ';', ':', { desc = 'Enter CMD mode w/o <Shift>' })
 
 -- Toggle line numbers
-vim.keymap.set('n', '<leader>n', '<cmd>set nu!<CR>', { desc = 'Toggle line number' })
-vim.keymap.set('n', '<leader>rn', '<cmd>set rnu!<CR>', { desc = 'Toggle relative number' })
+keymap('n', '<leader>n', '<cmd>set nu!<CR>', { desc = 'Toggle line number' })
+keymap('n', '<leader>rn', '<cmd>set rnu!<CR>', { desc = 'Toggle relative number' })
 
 -- Comments
-vim.keymap.set('n', '<leader>/', 'gcc', { desc = 'Toggle comment', remap = true })
-vim.keymap.set('v', '<leader>/', 'gc', { desc = 'Toggle comment', remap = true })
+keymap('n', '<leader>/', 'gcc', { desc = 'Toggle comment', remap = true })
+keymap('v', '<leader>/', 'gc', { desc = 'Toggle comment', remap = true })
 
 -- Indentation
-vim.keymap.set('v', '<Tab>', '>gv', { desc = 'Indent selection' })
-vim.keymap.set('v', '<S-Tab>', '<gv', { desc = 'Outdent selection' })
+keymap('v', '<Tab>', '>gv', { desc = 'Indent selection' })
+keymap('v', '<S-Tab>', '<gv', { desc = 'Outdent selection' })
 
 -- Insert mode navigation
-vim.keymap.set('i', '<C-b>', '<ESC>I', { desc = 'Move [b]eginning of line' })
-vim.keymap.set('i', '<C-e>', '<End>', { desc = 'Move [e]nd of line' })
-vim.keymap.set('i', '<C-h>', '<Left>', { desc = 'Move left' })
-vim.keymap.set('i', '<C-l>', '<Right>', { desc = 'Move right' })
-vim.keymap.set('i', '<C-j>', '<Down>', { desc = 'Move down' })
-vim.keymap.set('i', '<C-k>', '<Up>', { desc = 'Move up' })
+keymap('i', '<C-b>', '<ESC>I', { desc = 'Move [b]eginning of line' })
+keymap('i', '<C-e>', '<End>', { desc = 'Move [e]nd of line' })
+keymap('i', '<C-h>', '<Left>', { desc = 'Move left' })
+keymap('i', '<C-l>', '<Right>', { desc = 'Move right' })
+keymap('i', '<C-j>', '<Down>', { desc = 'Move down' })
+keymap('i', '<C-k>', '<Up>', { desc = 'Move up' })
 
 -- Scrolling
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Page [d]own and center' })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Page [u]p and center' })
-vim.keymap.set('n', 'n', 'nzz', { desc = 'Next search result and center' })
-vim.keymap.set('n', 'N', 'Nzz', { desc = 'Previous search result and center' })
+keymap('n', '<C-d>', '<C-d>zz', { desc = 'Page [d]own and center' })
+keymap('n', '<C-u>', '<C-u>zz', { desc = 'Page [u]p and center' })
+keymap('n', 'n', 'nzz', { desc = 'Next search result and center' })
+keymap('n', 'N', 'Nzz', { desc = 'Previous search result and center' })
 
 -- Moving selections
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+keymap('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+keymap('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
 
 -- Sessions
-vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', '<cmd>write<CR>', { desc = '[S]ave file' })
-vim.keymap.set('n', '<leader>rr', '<cmd>restart<CR>', { desc = 'Reinitialize Neovim' })
-vim.keymap.set('n', '<leader>oo', function()
+keymap({ 'n', 'i', 'v' }, '<C-s>', '<cmd>write<CR>', { desc = '[S]ave file' })
+keymap('n', '<leader>rr', '<cmd>restart<CR>', { desc = 'Reinitialize Neovim' })
+keymap('n', '<leader>oo', function()
   vim.cmd 'silent! mksession! Session.vim'
   vim.notify('Updating workspace state: Session.vim', vim.log.levels.INFO)
 end, { desc = 'Save workspace config' })
 
 -- Buffers
-vim.keymap.set('n', '<leader>b', '<cmd>enew<CR>', { desc = 'Open [b]uffer' })
-vim.keymap.set('n', '<leader>x', '<cmd>bdelete<CR>', { desc = 'Close buffer' })
-vim.keymap.set('n', '<C-c>', '<cmd>%y+<CR>', { desc = '[C]opy file' })
-vim.keymap.set('n', '<Esc>', '<cmd>noh<CR>', { desc = 'Clear highlights' })
+keymap('n', '<leader>b', '<cmd>enew<CR>', { desc = 'Open [b]uffer' })
+keymap('n', '<leader>x', '<cmd>bdelete<CR>', { desc = 'Close buffer' })
+keymap('n', '<C-c>', '<cmd>%y+<CR>', { desc = '[C]opy file' })
+keymap('n', '<Esc>', '<cmd>noh<CR>', { desc = 'Clear highlights' })
 
 -- Terminal
 ---@type NvTerminal -- terminal buffer
 local terminal = require 'nvim-terminal'
 
-vim.keymap.set('t', '<C-x>', '<C-\\><C-N>', { desc = 'Escape terminal mode' })
-vim.keymap.set('n', '<leader>h', function() terminal.open { pos = 'sp' } end, { desc = 'Open [h]orizontal terminal' })
-vim.keymap.set('n', '<leader>v', function() terminal.open { pos = 'vsp' } end, { desc = 'Open [v]ertical terminal' })
-vim.keymap.set({ 'n', 't' }, '<A-v>', function() terminal.toggle { pos = 'vsp', id = 'vtoggleTerm' } end, { desc = 'Toggle [v]ertical terminal' })
-vim.keymap.set({ 'n', 't' }, '<A-h>', function() terminal.toggle { pos = 'sp', id = 'htoggleTerm' } end, { desc = 'Toggle [h]orizontal terminal' })
-vim.keymap.set({ 'n', 't' }, '<A-i>', function() terminal.toggle { pos = 'float', id = 'floatTerm' } end, { desc = 'Toggle [f]loating terminal' })
+keymap('t', '<C-x>', '<C-\\><C-N>', { desc = 'Escape terminal mode' })
+keymap('n', '<leader>h', function() terminal.open { pos = 'sp' } end, { desc = 'Open [h]orizontal terminal' })
+keymap('n', '<leader>v', function() terminal.open { pos = 'vsp' } end, { desc = 'Open [v]ertical terminal' })
+keymap({ 'n', 't' }, '<A-v>', function() terminal.toggle { pos = 'vsp', id = 'vtoggleTerm' } end, { desc = 'Toggle [v]ertical terminal' })
+keymap({ 'n', 't' }, '<A-h>', function() terminal.toggle { pos = 'sp', id = 'htoggleTerm' } end, { desc = 'Toggle [h]orizontal terminal' })
+keymap({ 'n', 't' }, '<A-i>', function() terminal.toggle { pos = 'float', id = 'floatTerm' } end, { desc = 'Toggle [f]loating terminal' })
 
 -- LSP
-vim.keymap.set('n', '<leader>ds', vim.diagnostic.setloclist, { desc = 'LSP diagnostic loclist' })
+keymap('n', '<leader>ds', vim.diagnostic.setloclist, { desc = 'LSP diagnostic loclist' })
 
 -- Dashboard
-vim.keymap.set('n', '<leader>da', function() require('nvim-dashboard').setup() end, { desc = 'Toggle Dashboard' })
+keymap('n', '<leader>da', function() require('nvim-dashboard').setup() end, { desc = 'Toggle Dashboard' })
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,51 +1,55 @@
+local opt = vim.opt
+local o = vim.o
+local g = vim.g
+
 -- Set leader key(s)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
+g.mapleader = ' '
+g.maplocalleader = '\\'
 
 -- Disable providers
-vim.g.loaded_node_provider = 0
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_ruby_provider = 0
+g.loaded_node_provider = 0
+g.loaded_python3_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
 
 -- Set scoped opts
-vim.opt.fillchars = { eob = ' ' } -- remove trailing ~ on buffers
-vim.opt.guicursor = 'n-v-c:block-blinkwait300-blinkon200-blinkoff150,i-ci:ver25-blinkwait300-blinkon200-blinkoff150' -- enable blinking cursor
-vim.opt.shortmess:append 'sI' -- disable nvim intro
-vim.opt.swapfile = false -- disable swap files
-vim.opt.termguicolors = true -- Ensure termguicolors is enabled if not already
-vim.opt.whichwrap:append '<>[]hl' -- go to previous/next line with h,l,left arrow and right arrow when cursor reaches EOL or BOL
+opt.fillchars = { eob = ' ' } -- remove trailing ~ on buffers
+opt.guicursor = 'n-v-c:block-blinkwait300-blinkon200-blinkoff150,i-ci:ver25-blinkwait300-blinkon200-blinkoff150' -- enable blinking cursor
+opt.shortmess:append 'sI' -- disable nvim intro
+opt.swapfile = false -- disable swap files
+opt.termguicolors = true -- Ensure termguicolors is enabled if not already
+opt.whichwrap:append '<>[]hl' -- go to previous/next line with h,l,left arrow and right arrow when cursor reaches EOL or BOL
 
 -- Set global opts
-vim.o.clipboard = 'unnamedplus' -- use system clipboard
-vim.o.cursorline = true -- highlight the current line
-vim.o.mouse = 'a' -- enable mouse support
-vim.o.splitbelow = true -- new horizontal splits below
-vim.o.splitright = true -- new vertical splits to the right
-vim.o.timeoutlen = 400 -- keymap timeout
-vim.o.undofile = true -- persistent undo
-vim.o.updatetime = 250 -- swap write & CursorHold delay
-vim.o.winborder = 'single'
+o.clipboard = 'unnamedplus' -- use system clipboard
+o.cursorline = true -- highlight the current line
+o.mouse = 'a' -- enable mouse support
+o.splitbelow = true -- new horizontal splits below
+o.splitright = true -- new vertical splits to the right
+o.timeoutlen = 400 -- keymap timeout
+o.undofile = true -- persistent undo
+o.updatetime = 250 -- swap write & CursorHold delay
+o.winborder = 'single'
 
 -- Statusline
-vim.o.laststatus = 3 -- global statusline
-vim.o.showmode = false -- don't show --INSERT--
-vim.o.statusline = "%!v:lua.require('nvim-statusline').setup()" -- invoke statusline
+o.laststatus = 3 -- global statusline
+o.showmode = false -- don't show --INSERT--
+o.statusline = "%!v:lua.require('nvim-statusline').setup()" -- invoke statusline
 
 -- Gutter
-vim.o.number = true -- print line number in front of each line
-vim.o.relativenumber = true -- show relative line numbers
-vim.o.numberwidth = 2 -- minimal number of columns for line number
-vim.o.ruler = false -- hide the line/column position
-vim.o.signcolumn = 'yes' -- always show sign column
+o.number = true -- print line number in front of each line
+o.relativenumber = true -- show relative line numbers
+o.numberwidth = 2 -- minimal number of columns for line number
+o.ruler = false -- hide the line/column position
+o.signcolumn = 'yes' -- always show sign column
 
 -- Spaces & Tabs
-vim.o.expandtab = true -- use spaces instead of tabs
-vim.o.shiftwidth = 2 -- spaces per indent
-vim.o.smartindent = true -- autoindent smartly
-vim.o.tabstop = 2 -- spaces per tab
-vim.o.softtabstop = 2 -- editing width of a tab
+o.expandtab = true -- use spaces instead of tabs
+o.shiftwidth = 2 -- spaces per indent
+o.smartindent = true -- autoindent smartly
+o.tabstop = 2 -- spaces per tab
+o.softtabstop = 2 -- editing width of a tab
 
 -- Case Sensitive Search
-vim.o.ignorecase = true -- ignore case in search
-vim.o.smartcase = true -- override ignorecase if uppercase in search
+o.ignorecase = true -- ignore case in search
+o.smartcase = true -- override ignorecase if uppercase in search
