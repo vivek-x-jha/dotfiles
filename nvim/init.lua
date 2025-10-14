3f0270a49ae9f44300ef9d15feb0ceb8fc5ba3aa
Vivek Jha <vivek.x.jha@gmail.com>
2025-10-14 16:27:34 -0700

    refactor(nvim): moved plugin loading into plugins and lsp into init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -49,43 +49,50 @@ vim.o.laststatus = 3 -- global statusline
 vim.o.showmode = false -- don't show --INSERT--
 vim.o.statusline = "%!v:lua.require('nvim-statusline').setup()" -- initialize statusline
 
--- Load plugins
-vim.pack.add {
-  { src = 'https://github.com/saghen/blink.cmp' },
-  { src = 'https://github.com/stevearc/conform.nvim' },
-  { src = 'https://github.com/ibhagwan/fzf-lua' },
-  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
-  { src = 'https://github.com/lukas-reineke/indent-blankline.nvim' },
-  { src = 'https://github.com/L3MON4D3/LuaSnip' }, -- blink snippets
-  { src = 'https://github.com/folke/noice.nvim' },
-  { src = 'https://github.com/MunifTanjim/nui.nvim' }, -- noice dependency
-  { src = 'https://github.com/windwp/nvim-autopairs' },
-  { src = 'https://github.com/vivek-x-jha/nvim-dashboard' },
-  { src = 'https://github.com/brenoprata10/nvim-highlight-colors' },
-  { src = 'https://github.com/rcarriga/nvim-notify' },
-  { src = 'https://github.com/vivek-x-jha/nvim-sourdiesel' },
-  { src = 'https://github.com/nvim-pack/nvim-spectre' },
-  { src = 'https://github.com/vivek-x-jha/nvim-statusline' },
-  { src = 'https://github.com/kylechui/nvim-surround' },
-  { src = 'https://github.com/vivek-x-jha/nvim-terminal' },
-  { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' }, -- noice syntax parsing
-  { src = 'https://github.com/nvim-tree/nvim-web-devicons' }, -- nvim-tree + statusline icons
-  { src = 'https://github.com/nvim-lua/plenary.nvim' }, -- spectre dependency
-  { src = 'https://github.com/christoomey/vim-tmux-navigator' },
+-- Configure diagnostics ui
+vim.diagnostic.config {
+  severity_sort = true,
+  float = { border = 'single' },
+  virtual_lines = { current_line = true },
+  virtual_text = { current_line = true, prefix = '#' },
+
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = 'E',
+      [vim.diagnostic.severity.WARN] = 'W',
+      [vim.diagnostic.severity.HINT] = 'H',
+      [vim.diagnostic.severity.INFO] = 'I',
+    },
+  },
 }
 
--- Set highlights
-vim.cmd.colorscheme 'sourdiesel' -- use shell theme colors
+--- @type string -- language server configs path: ~/.config/nvim/lsp/
+local server_path = vim.fs.joinpath(vim.fn.stdpath 'config', 'lsp')
+
+-- Ensure the directory exists
+assert(vim.fn.isdirectory(server_path) == 1, '[LSP ERROR] Failed to open: "' .. server_path .. '"')
+
+---@type string[] -- language server names
+local servers = {}
+
+for name, type in vim.fs.dir(server_path) do
+  --- @type boolean -- flag for valid lua file
+  local is_lua_file = type == 'file' and name:sub(-4) == '.lua'
+
+  if is_lua_file then table.insert(servers, name:sub(1, -5)) end
+end
 
 -- Initialize language servers
-require 'lsp'
+vim.lsp.enable(servers)
 
 -- Configure plugins
 require 'plugins'
 
 -- Set event triggers
 require 'autocmds'
+
+-- Set highlights
+vim.cmd.colorscheme 'sourdiesel' -- use shell theme colors
 
 -- Set keymaps - defer to next event loop to prevent unwanted remappings
 vim.schedule(function() require 'keymaps' end)
