316608fe3dc66c10d6930bce58e02275566b06f5
Vivek Jha <vivek.x.jha@gmail.com>
2025-11-10 15:01:44 -0800

    feat(nvim): added buffer local navigation for git hunks

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -16,4 +16,17 @@ require('gitsigns').setup {
     changedelete = { text = 'x' },
     untracked = { text = '?' },
   },
+
+  on_attach = function(bufnr)
+    local map = function(key, fn)
+      vim.keymap.set('n', key, function()
+        if vim.wo.diff then return key end
+        vim.schedule(package.loaded.gitsigns[fn])
+        return '<Ignore>'
+      end, { buffer = bufnr, expr = true, desc = 'Git: ' .. fn })
+    end
+
+    map(']c', 'next_hunk')
+    map('[c', 'prev_hunk')
+  end,
 }
