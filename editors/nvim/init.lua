5ccf95b705d62496f8b8cc59534bc7fa4b23c153
Vivek Jha <vivek.x.jha@gmail.com>
2026-04-24 13:30:18 -0700

    refactor(nvim): moved statusline call to top level init

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -10,6 +10,13 @@ require 'autocmds'
 -- Configure plugins
 require 'plugins'
 
+-- Initialize statusline
+_G.sourdiesel_statusline = function()
+  local ok, statusline = pcall(require, 'nvim-statusline')
+  return ok and statusline.setup() or ''
+end
+vim.o.statusline = '%!v:lua.sourdiesel_statusline()'
+
 -- Set highlights
 vim.cmd.colorscheme 'sourdiesel' -- use shell theme colors
 
