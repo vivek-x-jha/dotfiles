aaaa05e398b2e60b0aa725b00e08232de4ccd084
Vivek Jha <vivek.x.jha@gmail.com>
2026-06-15 15:25:15 -0700

    fix(nvim): scope bashls to bash files

--- editors/nvim/lsp/bashls.lua
+++ editors/nvim/lsp/bashls.lua
@@ -1,10 +1,12 @@
 -- https://github.com/bash-lsp/bash-language-server/tree/main?tab=readme-ov-file#neovim
 return {
   cmd = { 'bash-language-server', 'start' },
-  filetypes = { 'sh', 'zsh' },
+  -- bashls is only useful for Bash-compatible files here; Zsh buffers get
+  -- their own syntax/filetype handling and should not inherit Bash diagnostics.
+  filetypes = { 'bash', 'sh' },
   settings = {
     bashIde = {
-      globPattern = '*@(.sh|.inc|.bash|.command|profile|.bashrc|.bash_profile|.zshrc|.zshenv|.zprofile)',
+      globPattern = '*@(.sh|.inc|.bash|.command|profile|.bashrc|.bash_profile)',
     },
   },
 }
